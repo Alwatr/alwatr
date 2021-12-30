e50b3106f428a2a11cc0d960970017d564caa017
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2021-12-30 19:12:21 +0330

    feat: build demo

---
+++ demo/src/util/index.ts
@@ -0,0 +1,3 @@
+import { persianNumberToEnglishNumber } from "@one/util/persian-number";
+
+console.log(persianNumberToEnglishNumber("۱۲۳۴"));
---
+++ package/util/src/persian-number.ts
@@ -0,0 +1,36 @@
+const persianNumbers = [
+  /۰/g,
+  /۱/g,
+  /۲/g,
+  /۳/g,
+  /۴/g,
+  /۵/g,
+  /۶/g,
+  /۷/g,
+  /۸/g,
+  /۹/g,
+];
+
+const arabicNumbers = [
+  /٠/g,
+  /١/g,
+  /٢/g,
+  /٣/g,
+  /٤/g,
+  /٥/g,
+  /٦/g,
+  /٧/g,
+  /٨/g,
+  /٩/g,
+];
+
+export function persianNumberToEnglishNumber(string: string): string {
+  if (typeof string === "string") {
+    for (let number = 0; number < 10; number++) {
+      string = string
+        .replace(persianNumbers[number], number.toString())
+        .replace(arabicNumbers[number], number.toString());
+    }
+  }
+  return string;
+}
