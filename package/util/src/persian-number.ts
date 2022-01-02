53b4c4cc459956cf2e4b77ee7cfef9733393b182
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-01-02 19:13:15 +0330

    style(eslint)

--- package/util/src/persian-number.ts
+++ package/util/src/persian-number.ts
@@ -24,9 +24,15 @@ const arabicNumbers = [
   /٩/g,
 ];
 
-export function persianNumberToEnglishNumber(string: string): string {
-  if (typeof string === "string") {
-    for (let number = 0; number < 10; number++) {
+/**
+ * persianNumberToEnglishNumber
+ */
+export function persianNumberToEnglishNumber(string: string): string
+{
+  if (typeof string === 'string')
+  {
+    for (let number = 0; number < 10; number++)
+    {
       string = string
         .replace(persianNumbers[number], number.toString())
         .replace(arabicNumbers[number], number.toString());
