2b4261050b855bddd2f594868993305d37086be8
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-01-07 22:41:58 +0330

    feat(server): install and config web dev server

---
+++ dev-server.mjs
@@ -0,0 +1,11 @@
+export default {
+  open: false,
+  nodeResolve: true,
+  watch: true,
+  // appIndex: './',
+  // in a monorepo you need to set set the root dir to resolve modules
+  // rootDir: '.',
+  // debug: false,
+  // hostname: "localhost",
+  // port:"8080",
+};
