711268e17dea8ada9c901ef1e9d605b3212abd97
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-02-24 05:28:53 +0330

    feat(logger): vatr logger package

---
+++ package/logger/src/logger.ts
@@ -0,0 +1,53 @@
+export type LoggerType = (message: string, ...restParam: Array<unknown>) => void;
+export type LogLevelType = 'debug' | 'error' | 'info' | 'log' | 'trace' | 'warn';
+
+let colorIndex = 0;
+const colorList = [
+  '#f05561',
+  '#35b997',
+  '#ee224a',
+  '#91c13e',
+  '#22af4b',
+  '#f0e995',
+  '#0fe995',
+  '#0f89ca',
+  '#08b9a5',
+  '#fee851',
+  '#ee573d',
+  '#f9df30',
+  '#1da2dc',
+  '#f05123',
+  '#ee2524',
+];
+
+const getNextColor = (): string => {
+  const color = colorList[colorIndex];
+  colorIndex++;
+  if (colorIndex >= colorList.length) {
+    colorIndex = 0;
+  }
+  return color;
+};
+
+/**
+ * Create a logger function for fancy console debug with custom scope.
+ *
+ * @property {boolean} force - if set to true logger will work even if its not in debug mode.
+ * @example
+ * const log = createLogger('my scope', 'log', true);
+ * log('my log message :)');
+ */
+export function createLogger(scope: string, level: LogLevelType = 'debug', force?: boolean): LoggerType {
+  const color = getNextColor();
+  return (message: string, ...restParam: Array<unknown>) => {
+    if (!(force === true || window.localStorage?.getItem('DEBUG') != null)) return;
+    // first args must be separated as keyPattern for fix issue of `this._log('a=%s', a)`
+    console[level](
+        `%c%s%c  ${message}`,
+        `color: ${color}; font-size: 1.2em;`,
+        scope,
+        'color: inherit;font-size: 1em',
+        ...restParam,
+    );
+  };
+}
