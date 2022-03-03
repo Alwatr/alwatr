802405fb17846be9fe51a41122c871b5aca9cf6b
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-03-04 01:45:57 +0330

    feat(router): initialRouter with trigers options

--- package/router/src/router.ts
+++ package/router/src/router.ts
@@ -1,19 +1,19 @@
 import {createLogger, vatrRegisteredList} from '@vatr/logger';
-// import type {InitOptions} from './type';
+import {clickTrigger} from './trigger-click';
+import type {InitOptions} from './type';
 
 export const log = createLogger('vatr/router');
 // export const error = createLogger('vatr/router', 'error', true);
 
 vatrRegisteredList.push({
-  name: '@vatr/signal',
+  name: '@vatr/router',
   version: '{{VATR_VERSION}}',
 });
 
 /**
- * Initial and config router
+ * Initial and config Vatr Router
  */
-// export function initRouter(options: InitOptions) {
-//   if (options.globalClickTrigger) {
-
-//   }
-// }
+export function initialRouter(options?: InitOptions) {
+  log('initialRouter: %o', options);
+  clickTrigger.enable = options?.clickTrigger ?? true;
+}
--- package/router/src/type.ts
+++ package/router/src/type.ts
@@ -1,6 +1,27 @@
+import '@vatr/signal/type';
+
+declare global {
+  interface VatrSignals {
+    'vatr-router-go': unknown;
+  }
+
+  interface VatrRequestSignals {
+    'vatr-router-change': {
+      pathname: string;
+      search: string;
+      hash: string;
+    };
+  }
+}
 export interface InitOptions {
   /**
-   * Global click trigger for catch all links click events to dispatch inside routing signal.
+   * A navigation trigger for Vatr Router that translated clicks on `<a>` links into navigation signal.
+   *
+   * Only regular clicks on in-app links are translated.
+   * Only primary mouse button, no modifier keys, the target href is within the app's URL space.
+   *
+   * @default true
    */
-  globalClickTrigger?: boolean;
+  clickTrigger?: boolean;
+  popstateTrigger?: boolean;
 }
