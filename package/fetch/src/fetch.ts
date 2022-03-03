dd2d1418d090387e1afa8277f78ae88ace17fd17
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-03-03 05:56:13 +0330

    feat(fetch): FetchOptions type

--- package/fetch/src/fetch.ts
+++ package/fetch/src/fetch.ts
@@ -4,13 +4,21 @@ const log = createLogger('vatr/signal');
 // export const error = createLogger('vatr/signal', 'error', true);
 
 vatrRegisteredList.push({
-  name: '@vatr/signal',
+  name: '@vatr/fetch',
   version: '{{VATR_VERSION}}',
 });
+
+export interface FetchOptions extends RequestInit
+{
+  timeout?: number;
+  jsonify?: boolean;
+  bodyObject?: Record<string | number, unknown>;
+  queryParamList?: Record<string, string | number | boolean>;
+}
 
 /**
  * Enhanced fetch function.
  */
-export function fetch <T>(url: string, options?: unknown): Promise<T> {
+export function fetch <ResponseType>(url: string, options?: FetchOptions): Promise<ResponseType> {
   log('fetch', url, options);
 }
