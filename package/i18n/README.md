88a6c28ad038ed02ca45128378e669f6d9c6949d
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-03-06 03:05:09 +0330

    feat(i18n): start new package for i18n/l10n

---
+++ package/i18n/README.md
@@ -0,0 +1,11 @@
+# @vatr/fetch
+
+vatr i18n
+
+## Example usage
+
+```js
+import {...} from 'https://esm.run/@vatr/i18n';
+
+...
+```
---
+++ package/i18n/src/core.ts
@@ -0,0 +1,9 @@
+import {createLogger, vatrRegisteredList} from '@vatr/logger';
+
+export const log = createLogger('vatr/i18n');
+// const error = createLogger('vatr/i18n', 'error', true);
+
+vatrRegisteredList.push({
+  name: '@vatr/i18n',
+  version: '{{VATR_VERSION}}',
+});
