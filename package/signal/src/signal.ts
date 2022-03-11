6ece7b560368af3434516c46379ad729997b156b
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-03-11 04:32:45 +0330

    refactor(signal): new logger

--- package/signal/src/signal.ts
+++ package/signal/src/signal.ts
@@ -1,4 +1,5 @@
-import {log,
+import {
+  logger,
   _getSignalObject,
   _callListeners,
   _removeSignalListener,
@@ -24,7 +25,7 @@ export function addSignalListener<SignalName extends keyof VatrSignals>(
     signalCallback: ListenerCallback<SignalName>,
     options?: ListenerOptions,
 ): symbol {
-  log('addSignalListener(%s, %o)', signalName, options);
+  logger.logMethodArgs('addSignalListener', {signalName, options});
 
   const signal = _getSignalObject(signalName);
   const listener: ListenerObject<SignalName> = {
@@ -39,17 +40,19 @@ export function addSignalListener<SignalName extends keyof VatrSignals>(
   // Run callback for old dispatch signal
   if (signal.value !== undefined) {
     if (options?.receivePrevious === 'Immediate') {
-      log('addSignalListener(%s): run callback(immediately)', signalName);
+      logger.incident('addSignalListener', 'call_signal_callback', 'run callback with previous signal value!',
+          {signalName, mode: 'Immediate'});
       try {
         signalCallback(signal.value);
       } catch (err) {
-        console.error('addSignalListener(%s): signalCallback error! %o', signalName, err);
+        logger.error('addSignalListener', 'call_signal_callback_failed', (err as Error).stack || err, {signalName});
       }
       callbackCalled = true;
     } else if (options?.receivePrevious === true) {
       requestAnimationFrame(() => {
         if (signal.value !== undefined) {
-          log('addSignalListener(%s): run callback(delay)', signalName);
+          logger.incident('addSignalListener', 'call_signal_callback', 'run callback with previous signal value!',
+              {signalName, mode: 'Delay'});
           signalCallback(signal.value);
         }
       });
@@ -80,7 +83,8 @@ export function removeSignalListener<SignalName extends keyof VatrSignals>(
     signalName: SignalName,
     listenerId: symbol,
 ): void {
-  log('removeSignalListener(%s)', signalName);
+  logger.logMethodArgs('removeSignalListener', signalName);
+
   const signal = _getSignalObject(signalName);
   _removeSignalListener(signal, listenerId);
 }
@@ -96,7 +100,7 @@ export function dispatchSignal<SignalName extends keyof VatrSignals>(
     value: VatrSignals[SignalName],
     options?: DispatchOptions,
 ): void {
-  log('dispatchSignal(%s, %o, %o)', signalName, value, options);
+  logger.logMethodArgs('dispatchSignal', {signalName, value, options});
 
   const signal = _getSignalObject(signalName);
   // set value before check signal.debounced for act like throttle (call listeners with last dispatch value).
@@ -130,7 +134,8 @@ export function requestSignal<SignalName extends keyof VatrRequestSignals>(
     signalName: SignalName,
     requestParam: VatrRequestSignals[SignalName],
 ): Promise<VatrSignals[SignalName]> {
-  log('requestSignal: %s', signalName);
+  logger.logMethodArgs('requestSignal', {signalName, requestParam});
+
   dispatchSignal(
       `request-${signalName}` as unknown as SignalName,
       requestParam as unknown as VatrSignals[SignalName], // mastmalize to avoid type error
@@ -157,12 +162,13 @@ export function setSignalProvider<SignalName extends keyof VatrRequestSignals>(
     signalProvider: SignalProvider<SignalName>,
     options?: SignalProviderOptions,
 ): symbol {
-  log('setSignalProvider(%s)', signalName);
+  logger.logMethodArgs('setSignalProvider', {signalName, options});
+
   // @TODO: refactor with removeSignalProvider
   const signal = _getSignalObject(`request-${signalName}` as unknown as SignalName);
   if (signal.listenerList.length > 0) {
-    log('setSignalProvider(%s): WARNING! another provider defined and will removed!',
-        signalName, signal.listenerList.length);
+    logger.accident('setSignalProvider', 'signal_provider_already_set', 'another provider defined and will removed'
+        , {signalName});
     signal.listenerList = [];
   }
 
@@ -198,7 +204,7 @@ export async function waitForSignal<SignalName extends keyof VatrSignals>(
     signalName: SignalName,
     receivePrevious?: boolean,
 ): Promise<VatrSignals[SignalName]> {
-  log('waitForSignal(%s)', signalName);
+  logger.logMethodArgs('waitForSignal', {signalName, receivePrevious});
 
   return new Promise((resolve) => {
     addSignalListener(signalName, resolve, {
@@ -217,7 +223,7 @@ export async function waitForSignal<SignalName extends keyof VatrSignals>(
  */
 export function hasSignalDispatchedBefore<SignalName extends keyof VatrSignals>(signalName: SignalName): boolean {
   const dispatched = 'value' in _getSignalObject(signalName);
-  log('hasSignalDispatchedBefore(%s) => %s', signalName, dispatched);
+  logger.logMethodFull('hasSignalDispatchedBefore', signalName, dispatched);
   return dispatched;
 }
 
@@ -231,7 +237,7 @@ export function hasSignalDispatchedBefore<SignalName extends keyof VatrSignals>(
  * hasSignalDispatchedBefore('content-change'); // false
  */
 export function expireSignal<SignalName extends keyof VatrSignals>(signalName: SignalName): void {
-  log('expireSignal(%s)', signalName);
+  logger.logMethodArgs('expireSignal', signalName);
   delete _getSignalObject(signalName).value;
 }
 
