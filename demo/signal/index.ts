fc3f9fdd8c76c6bc5117f3ee894480500f81cc80
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-03-13 02:31:43 +0330

    feat(signal): demo as test

---
+++ demo/signal/index.ts
@@ -0,0 +1,24 @@
+import {SignalInterface} from '@alwatr/signal';
+
+const signal = new SignalInterface('easter-egg');
+
+signal.setProvider((param) => {
+  const newValue = param * 10;
+  console.info('2. signal provider called', {param, newValue});
+  return newValue;
+});
+
+document.getElementById('requestButton')?.addEventListener('click', async () => {
+  console.info('1. request param: 1');
+  const value = await signal.request(1);
+  console.info('3. new signal value', {value});
+});
+
+document.getElementById('requestButton2')?.addEventListener('click', async () => {
+  console.info('1. request');
+  const value1 = await signal.request(1);
+  console.info('3. 2x request');
+  const value2p = signal.request(2);
+  const value3p = signal.request(3);
+  console.info('4. new signal value', {value1, value2: await value2p, value3: await value3p});
+});
