d07e0eaf7781b8c35f7f47c90fdbbbc7e229a060
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-03-14 04:08:55 +0330

    fix(router): export RequestRouteParam type

--- package/router/src/router.ts
+++ package/router/src/router.ts
@@ -4,7 +4,7 @@ import {clickTrigger} from './trigger-click';
 import {popstateTrigger} from './trigger-popstate';
 import type {InitOptions, Route, RoutesConfig} from './type';
 
-export type {Route, RoutesConfig} from './type';
+export type {Route, RequestRouteParam, RoutesConfig} from './type';
 
 /**
  * Initial and config the Router.
