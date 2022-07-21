2b4aab655bc0707761587a7439de98bbd4ad0e08
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-07-21 15:23:31 +0430

    feat(logger): node coloring support

--- demo/logger/index.ts
+++ demo/logger/index.ts
@@ -1,34 +1,37 @@
 import {createLogger} from '@alwatr/logger';
 
-const logger = createLogger('logger/demo', 'green');
+const logger1 = createLogger('logger/demo1');
+const logger2 = createLogger('logger/demo2');
 
 console.debug('--- logger.logProperty ---');
-logger.logProperty('name', 'ali');
-logger.logProperty('options', {a: 1, b: 2});
+logger1.logProperty('name', 'ali');
+logger2.logProperty('options', {a: 1, b: 2});
 
 console.debug('--- logger.logMethod ---');
-logger.logMethod('myMethod');
+logger1.logMethod('myMethod1');
+logger2.logMethod('myMethod2');
 
 console.debug('--- logger.logMethodArgs ---');
-logger.logMethodArgs('myMethod', {a: 1, b: 2});
+logger1.logMethodArgs('myMethod1', {a: 1, b: 2});
+logger2.logMethodArgs('myMethod2', {a: 1, b: 2});
 
 console.debug('--- logger.logMethodFull ---');
-logger.logMethodFull('add', {a: 1, b: 2}, 3);
+logger1.logMethodFull('add', {a: 1, b: 2}, 3);
 
 console.debug('--- logger.incident ---');
-logger.incident('myMethod', 'abort_signal', 'Aborted signal received', {url: '/test.json'});
+logger1.incident('myMethod', 'abort_signal', 'Aborted signal received', {url: '/test.json'});
 
 console.debug('--- logger.accident ---');
-logger.accident('myMethod', 'file_not_found', 'Url requested return 404 not found', {
+logger2.accident('myMethod', 'file_not_found', 'Url requested return 404 not found', {
   url: '/test.json',
 });
 
 console.debug('--- logger.logOther ---');
-logger.logOther('foo:', 'bar', {a: 1});
+logger1.logOther('foo:', 'bar', {a: 1});
 
 console.debug('--- logger.error ---');
 try {
   throw new Error('my_error_message');
 } catch (err) {
-  logger.error('myMethod', 'error_code', (err as Error).stack || err, {a: 1, b: 2});
+  logger1.error('myMethod', 'error_code', (err as Error).stack || err, {a: 1, b: 2});
 }
