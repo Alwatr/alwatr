2b52160c3656836776574678a7f7ffbac026868e
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-08-03 23:12:49 +0430

    refactor(storage): rename

--- packages/service/storage/src/index.ts
+++ packages/service/storage/src/index.ts
@@ -1,5 +1,5 @@
 import './route/home.js';
-import './route/update-doc.js';
+import './route/update.js';
 import {logger} from './lib/config.js';
 
 logger.logOther('..:: Alwatr Nanoservice Starter Kit ::..');
