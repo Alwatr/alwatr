6e1536ff8166152237ea07c67a2c3dd60fb22f4e
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-08-17 20:22:24 +0430

    feat(php-apache): classic php with apache

---
+++ packages/classic-cloud/php-apache/README.md
@@ -0,0 +1,5 @@
+# @alwatr/php-apache container
+
+## Supper Classic PHP Container With Apache
+
+**PLEASE ALWAYS USE PHP WITH NGINX IN FPM MODE, THIS PROJECT IS JUST FOR EDUCATION PURPOSE.**
---
+++ packages/classic-cloud/php-apache/_up.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+trap "echo '❌ Error'" ERR
+
+TIMEFORMAT="done in %Rs"
+thisPath="$(dirname "$0")"
+cd $thisPath
+ls -lahF
+
+echoStep () {
+  echo "🔹 $1"
+}
+
+echoStep "Preparing..."
+
+[ ! -d _data ] && mkdir _data
+
+time docker-compose pull
+time docker-compose build --pull
+
+echoStep "Starting..."
+
+time docker-compose up --detach --remove-orphans --force-recreate
+
+echoStep "Fix permitions..."
+
+time docker-compose exec php "fix-permition.sh" || true
+
+echoStep "Done"
+
+docker-compose logs --tail=300 --follow || true
