88f1fce6a3e2fae287290acaf52f8b31f3c7269c
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-09-23 10:43:40 +0330

    refactor(math): rewrite UnicodeDigits to improve performance

--- demo/math/translate-unicode-digits.ts
+++ demo/math/translate-unicode-digits.ts
@@ -1,4 +1,6 @@
-import {translateUnicodeDigits} from '@alwatr/math';
+import {UnicodeDigits} from '@alwatr/math';
+
+const unicodeDigits = new UnicodeDigits('common', 'fa');
 
 const list = [
   '0123456789',
@@ -14,5 +16,15 @@ const list = [
 ].join('\n');
 
 console.log(list);
+console.log('-----');
 
-console.log(translateUnicodeDigits(list, 'fa'));
+console.log(unicodeDigits.translate(list));
+
+const start = Date.now();
+const count = 20_000;
+
+for (let i = count; i > 0; i--) {
+  unicodeDigits.translate(list);
+}
+
+console.log(count / (Date.now() - start) * 1000);
