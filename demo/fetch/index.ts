47dd2391cd1d760cfec8b0f54a249d05b83b4c33
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-10-21 23:05:39 +0330

    feat(fetch): cache strategy demo

--- demo/fetch/index.ts
+++ demo/fetch/index.ts
@@ -1,5 +1,7 @@
 import {fetch} from '@alwatr/fetch';
 
+import type {CacheStrategy} from '@alwatr/fetch';
+
 const buttons = document.querySelectorAll('button') as NodeListOf<HTMLButtonElement>;
 
 for (const button of buttons) {
@@ -11,7 +13,11 @@ for (const button of buttons) {
 
       button.classList.add('loading');
       try {
-        const response = await fetch(url);
+        const response = await fetch({
+          url,
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          cacheStrategy: document.querySelector<HTMLSelectElement>('#cacheSelect')!.value as CacheStrategy,
+        });
         console.log('Demo response: %o', {url, response});
       }
       catch (error) {
