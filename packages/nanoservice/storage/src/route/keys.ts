46fb9708c4a5438d1ef5179e3cba79cad4d004b0
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-11-14 00:19:37 +0330

    feat(service/storage): add keys route

---
+++ packages/nanoservice/storage/src/route/keys.ts
@@ -0,0 +1,24 @@
+import {config, logger} from '../lib/config.js';
+import {nanoServer} from '../lib/nano-server.js';
+import {storageProvider} from '../lib/storage-provider.js';
+
+import type {AlwatrConnection} from '@alwatr/nano-server';
+
+nanoServer.route('GET', '/keys', getStorageKeys);
+
+async function getStorageKeys(connection: AlwatrConnection): Promise<void> {
+  logger.logMethod('getStorageKeys');
+
+  const token = connection.requireToken(config.token);
+  if (token == null) return;
+
+  const params = connection.requireQueryParams<{storage: string, id: string}>(['storage']);
+  if (params == null) return;
+
+  const storage = storageProvider.get({name: params.storage});
+
+  connection.reply({
+    ok: true,
+    data: {keys: storage.keys},
+  });
+}
