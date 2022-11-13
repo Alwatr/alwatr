b6454a5bf111c2457644b19af6f5c33f9cccf3d1
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-11-14 00:15:25 +0330

    feat(storage): add data method

--- packages/core/storage/src/storage.ts
+++ packages/core/storage/src/storage.ts
@@ -101,6 +101,13 @@ export class AlwatrStorage<DocumentType extends DocumentObject> {
   protected _keys: Array<string> | null = null;
 
   /**
+   * All document in array.
+   */
+  get data(): Record<string, DocumentType | undefined> {
+    return this._storage.data;
+  }
+
+  /**
    * All document ids in array.
    */
   get keys(): Array<string> {
