f1141046705b5c928d7c73423868a55a451dd06e
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-11-15 18:29:55 +0330

    refactor(ns/storage): rename route files

--- packages/nanoservice/storage/src/index.ts
+++ packages/nanoservice/storage/src/index.ts
@@ -1,8 +1,8 @@
 import './route/get.js';
-import './route/update.js';
-import './route/remove.js';
+import './route/patch.js';
+import './route/delete.js';
 import './route/keys.js';
-import './route/get-all.js';
+import './route/all.js';
 import {logger} from './lib/config.js';
 
 logger.logOther('..:: Alwatr Storage Nanoservice API ::..');
