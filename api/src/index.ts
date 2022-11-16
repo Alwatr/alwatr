f72f0ac9221ec197cb5923a85e0f8016a75ff411
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-11-16 18:17:17 +0330

    feat(crawler): add route in index

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,6 @@
-import './route/echo.js';
+import './route/add.js';
+import './route/delete.js';
+import './route/current.js';
 import {logger} from './lib/config.js';
 
 logger.logOther('..:: Flight Find API ::..');
