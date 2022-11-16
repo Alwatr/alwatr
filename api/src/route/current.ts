6dd73aa26efb7cc30c6a2cbd229c72ff2ec684b1
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-11-16 18:16:23 +0330

    feat(crawler): add delete

---
+++ api/src/route/current.ts
@@ -0,0 +1,41 @@
+import {logger} from '../lib/config.js';
+import {nanoServer} from '../lib/nano-server.js';
+
+import type {AlwatrConnection} from '@alwatr/nano-server';
+import {stroage} from 'src/lib/storage.js';
+
+// add job API
+nanoServer.route('GET', '/current', handler);
+
+async function handler(connection: AlwatrConnection): Promise<void> {
+  logger.logMethod('handler');
+
+  try {
+    const allJob = await stroage.getAll();
+    let allJobResponse: any = {};
+    for (const job in allJob) {
+      allJobResponse[job] = {
+        origin: allJob[job].origin,
+        dest: allJob[job].dest,
+        date: allJob[job].date,
+      };
+    }
+    connection.reply({
+      ok: true,
+      data: {
+        app: 'Job API',
+        current: allJobResponse,
+      },
+    });
+  } catch {
+    connection.reply({
+      ok: false,
+      errorCode: 'internal_error',
+      statusCode: 500,
+      data: {
+        app: 'Job API',
+        message: 'Internal server error',
+      },
+    });
+  }
+}
