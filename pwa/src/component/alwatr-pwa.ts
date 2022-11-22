6b115f1a4f60b1a9a1b2c20842c899e1bb1566d5
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-11-22 19:34:49 +0330

    feat(ionic): define components

--- pwa/src/component/alwatr-pwa.ts
+++ pwa/src/component/alwatr-pwa.ts
@@ -1,10 +1,14 @@
 import {AlwatrElement} from '@alwatr/element';
 import {router} from '@alwatr/router';
+import {initialize} from '@ionic/core/components';
 import {css, html} from 'lit';
-import {customElement} from 'lit/decorators/custom-element.js';
-import {cache} from 'lit/directives/cache.js';
+import {customElement} from 'lit/decorators.js';
+
+import ionNormalize from '../style/ionic.normalize';
+import ionTheming from '../style/ionic.theming';
 
 import './page-flight-finder';
+import './ionic-components';
 
 import type {RoutesConfig} from '@alwatr/router';
 import type {TemplateResult} from 'lit';
@@ -15,59 +19,23 @@ declare global {
   }
 }
 
+initialize();
+
 /**
  * alwatr-pwa PWA Root Element
  */
 @customElement('alwatr-pwa')
 export class AlwatrPWA extends AlwatrElement {
   static override styles = [
+    ionNormalize,
+    ionTheming,
     css`
-      :host {
-        inset: 0;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        display: flex;
-        position: absolute;
-        flex-direction: column;
-        justify-content: space-between;
-        contain: layout size style;
-        overflow: hidden;
-        z-index: 0;
-      }
-
       .page-container {
         position: relative;
         flex-grow: 1;
         flex-shrink: 1;
         flex-basis: 0%;
         contain: size layout style;
-      }
-
-      .page {
-        inset: 0;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        display: flex;
-        position: absolute;
-        flex-direction: column;
-        justify-content: space-between;
-        contain: layout size style;
-        overflow: hidden;
-        z-index: 0;
-      }
-
-      /* This will be displayed only on lazy loading. */
-      [unresolved]::after {
-        content: '...';
-        display: block;
-        font-size: 2em;
-        padding-top: 30vh;
-        letter-spacing: 3px;
-        text-align: center;
       }
     `,
   ];
@@ -87,12 +55,12 @@ export class AlwatrPWA extends AlwatrElement {
     map: (route) => route.sectionList[0]?.toString(),
     list: {
       home: {
-        render: () => html`<page-home class="page"></page-home>`,
+        render: () => html`<page-flight-finder class="ion-page"></page-flight-finder>`,
       },
     },
   };
 
   override render(): TemplateResult {
-    return html`<div class="page-container">${cache(router.outlet(this._routes))}</div>`;
+    return html` <main class="page-container">${router.outlet(this._routes)}</main>`;
   }
 }
