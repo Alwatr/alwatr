272440cf9311169a8cf69cbab0e273469585cc26
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-11-24 15:40:25 +0330

    feat(fakedata): job list

--- pwa/src/component/page-flight-finder.ts
+++ pwa/src/component/page-flight-finder.ts
@@ -50,12 +50,46 @@ export class PageFlightFinder extends AlwatrElement {
     `,
   ];
 
-  @state() private __jobList: Array<Job> = [];
+  @state() private __jobList: Array<Job> = [
+    {
+      _id: '2',
+      filter: {
+        dest: 'MHD',
+        origin: 'THR',
+        dayPart: ['afternoon'],
+        date: '1401/09/10',
+        maxPrice: 389000,
+      },
+      resultList: [],
+    },
+    {
+      _id: '3',
+      filter: {
+        dest: 'THR',
+        origin: 'MHD',
+        dayPart: ['earlyMorning'],
+        date: '1401/09/12',
+        maxPrice: 30000,
+      },
+      resultList: [
+        {
+          price: 10000,
+          time: 0,
+          seatCount: 0,
+        },
+        {
+          price: 12000,
+          time: 0,
+          seatCount: 0,
+        },
+      ],
+    },
+  ];
 
   static jobListSignal = new SignalInterface('job-list');
   static jobAddSignal = new SignalInterface('job-add');
   static cityListTemplate = Object.keys(cityList).map(
-      (city) => html`<ion-select-option value=${city}>${cityList[city]}</ion-select-option>`,
+      (city) => html`<ion-select-option value=${city}>${city} - ${cityList[city]}</ion-select-option>`,
   );
   private __newJob: Partial<JobFilter> = {};
 
@@ -120,16 +154,16 @@ export class PageFlightFinder extends AlwatrElement {
             <ion-item fill="solid">
               <ion-label position="floating">تاریخ</ion-label>
               <ion-select name="date" @ionChange=${this.__inputChanged} interface="popover">
-                <ion-select-option value="data1">۱۴۰۱/۰۹/۲۴</ion-select-option>
-                <ion-select-option value="data2">۱۴۰۱/۰۹/۲۵</ion-select-option>
-                <ion-select-option value="data3">۱۴۰۱/۰۹/۲۶</ion-select-option>
-                <ion-select-option value="data4">۱۴۰۱/۰۹/۲۷</ion-select-option>
-                <ion-select-option value="data5">۱۴۰۱/۰۹/۲۸</ion-select-option>
-                <ion-select-option value="data6">۱۴۰۱/۰۹/۲۹</ion-select-option>
-                <ion-select-option value="data7">۱۴۰۱/۰۹/۳۰</ion-select-option>
-                <ion-select-option value="data8">۱۴۰۱/۰۹/۳۱</ion-select-option>
-                <ion-select-option value="data9">۱۴۰۱/۱۰/۰۱</ion-select-option>
-                <ion-select-option value="data10">۱۴۰۱/۱۰/۰۲</ion-select-option>
+                <ion-select-option value="1401/09/24">۱۴۰۱/۰۹/۲۴</ion-select-option>
+                <ion-select-option value="1401/09/25">۱۴۰۱/۰۹/۲۵</ion-select-option>
+                <ion-select-option value="1401/09/26">۱۴۰۱/۰۹/۲۶</ion-select-option>
+                <ion-select-option value="1401/09/27">۱۴۰۱/۰۹/۲۷</ion-select-option>
+                <ion-select-option value="1401/09/28">۱۴۰۱/۰۹/۲۸</ion-select-option>
+                <ion-select-option value="1401/09/29">۱۴۰۱/۰۹/۲۹</ion-select-option>
+                <ion-select-option value="1401/09/30">۱۴۰۱/۰۹/۳۰</ion-select-option>
+                <ion-select-option value="1401/10/01">۱۴۰۱/۱۰/۰۱</ion-select-option>
+                <ion-select-option value="1401/10/02">۱۴۰۱/۱۰/۰۲</ion-select-option>
+                <ion-select-option value="1401/10/03">۱۴۰۱/۱۰/۰۳</ion-select-option>
               </ion-select>
             </ion-item>
             <ion-item fill="solid">
