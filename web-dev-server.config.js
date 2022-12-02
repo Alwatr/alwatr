98cbb53ca98bd8905d76cd2e419b5fb1a20b53f1
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-12-02 19:20:49 +0330

    fix: web-dev-server

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,13 +1,11 @@
 // https://modern-web.dev/docs/dev-server/cli-and-configuration/#configuration-file
 export default {
-  hostname: '0.0.0.0',
   port: 8080,
   open: true,
   watch: true,
-  appIndex: 'index.html',
+  // appIndex: 'index.html',
   nodeResolve: {
-    exportConditions: ['development'],
-    dedupe: true,
+    exportConditions: ['development']
   },
   esbuildTarget: 'auto',
   // in a monorepo you need to set set the root dir to resolve modules
