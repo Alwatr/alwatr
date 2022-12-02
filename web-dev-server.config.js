12577ecadad97cf776dc75f6c920bacb4908fb57
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-12-02 18:37:23 +0330

    chore: lit best practices

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,14 +1,18 @@
 // https://modern-web.dev/docs/dev-server/cli-and-configuration/#configuration-file
 export default {
-  open: false,
-  nodeResolve: true,
-  watch: true,
-  // appIndex: './',
-  // in a monorepo you need to set set the root dir to resolve modules
-  // rootDir: '.',
-  // debug: false,
   hostname: '0.0.0.0',
-  port: 8000,
+  port: 8080,
+  open: true,
+  watch: true,
+  appIndex: 'index.html',
+  nodeResolve: {
+    exportConditions: ['development'],
+    dedupe: true,
+  },
+  esbuildTarget: 'auto',
+  // in a monorepo you need to set set the root dir to resolve modules
+  rootDir: '.',
+  // debug: false,
   preserveSymlinks: true,
   plugins: [],
   middleware: [],
