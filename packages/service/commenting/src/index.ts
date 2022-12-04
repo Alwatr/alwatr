705d7b99c2cd7369d496051538f09de35660b2df
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-12-04 15:19:37 +0330

    feat(service/commenting): add base nano server

---
+++ packages/service/commenting/src/index.ts
@@ -0,0 +1,4 @@
+import './route/home.js'
+import {logger} from './lib/config.js';
+
+logger.logOther('..:: Alwatr Storage Nanoservice API ::..');
---
+++ packages/service/commenting/src/lib/config.ts
@@ -0,0 +1,12 @@
+import {createLogger} from '@alwatr/logger';
+
+export const logger = createLogger('storage-server');
+
+export const config = {
+  host: process.env.HOST ?? '0.0.0.0',
+  port: process.env.PORT != null ? +process.env.PORT : 8000,
+  storagePath: process.env.STORAGE_PATH ?? 'db',
+  token: process.env.TOKEN ?? 'YOUR_SECRET_TOKEN',
+};
+
+logger.logProperty('config', {...config, token: '***'});
