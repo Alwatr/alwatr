9f5924008d7b567af1c990450da2450af887404a
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-12-10 15:51:40 +0330

    feat(services/telegram-notifier): dynamic storage path

--- services/telegram-notifier/src/lib/storage.ts
+++ services/telegram-notifier/src/lib/storage.ts
@@ -1,5 +1,6 @@
 import {AlwatrStorageEngine} from '@alwatr/storage-engine';
 
+import {config} from './config';
 import {MemberList} from './type';
 
-export const storage = new AlwatrStorageEngine<MemberList>({name: 'notifier-storage', path: '_data'});
+export const storage = new AlwatrStorageEngine<MemberList>({name: 'notifier-storage', path: config.storage.path});
