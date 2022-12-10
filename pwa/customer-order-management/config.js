12540d2ed51d4b57d819941f64ba0c0b67087caa
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-12-10 15:19:49 +0330

    refactor(com-pwa): folder structure

--- pwa/customer-order-management/config.js
+++
@@ -1,4 +0,0 @@
-window.appConfig = {
-  api: 'http://127.0.0.1:8000',
-  token: 'YOUR_SECRET_TOKEN',
-};
--- pwa/customer-order-management/workbox-config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('workbox-build').GenerateSWOptions} */
-export const workboxConfig = {
-  swDest: 'dist/sw.js',
-  globDirectory: 'dist/',
-  // skipWaiting: true,
-  globPatterns: ['**/*.{js,css,html,json,png,svg,ico,webp}'],
-}
