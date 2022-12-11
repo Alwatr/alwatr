e10a362167916c3aa169b0939da9247b15e26f29
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-12-11 14:03:56 +0330

    chore: improve clean command

--- services/telegram-notifier/esbuild.config.js
+++ services/telegram-notifier/esbuild.config.js
@@ -1,15 +1,15 @@
-import {build} from "esbuild"
+import {build} from 'esbuild';
 
 build({
   entryPoints: ['./src/index.ts'],
   bundle: true,
-  logLevel: "info",
+  logLevel: 'debug',
   minify: true,
-  format: "esm",
-  target: "esnext",
-  platform: "node",
-  outfile: 'dist/index.js',
+  format: 'esm',
+  target: 'esnext',
+  platform: 'node',
+  outdir: 'dist',
   banner: {
-    js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+    js: 'import { createRequire } from \'module\';const require = createRequire(import.meta.url);',
   },
-})
+});
