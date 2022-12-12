dbd937aab6680b7e01227c07776900349d9a1000
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-12-12 12:23:30 +0330

    refactor(telegram-notifier): external esbuild

---
+++ services/telegram-notifier/esbuild.mjs
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import {build} from 'esbuild';
+
+build({
+  entryPoints: ['./src/index.ts'],
+
+  logLevel: 'debug',
+  platform: 'node',
+  target: 'node19',
+  format: 'esm',
+
+  bundle: true,
+  treeShaking: true,
+  minify: true,
+  sourcemap: true,
+
+  outdir: 'dist',
+  outExtension: {
+    '.js': '.mjs',
+  },
+
+  // banner: {
+  //   js: 'import { createRequire } from \'module\';const require = createRequire(import.meta.url);',
+  // },
+});
