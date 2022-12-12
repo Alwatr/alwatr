413b5fc3753304fe3c7e3e4ff574040fe3395ae8
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-12-12 14:35:57 +0330

    fix(telegram-notifier): lanch fail

--- services/telegram-notifier/src/bot/launch.ts
+++ services/telegram-notifier/src/bot/launch.ts
@@ -4,7 +4,12 @@ import {sendMessage} from './send-message.js';
 
 export async function launchBot(): Promise<void> {
   logger.logMethod('launchBot');
-  await bot.launch();
+  try {
+    await bot.launch();
+  } catch (err) {
+    logger.error('launchBot', 'launch_bot_failed', (err as Error).stack || err);
+    throw new Error('launch_bot_failed');
+  }
   logger.logProperty('botInfo', bot.botInfo);
   sendMessage(config.telegramBot.debugNotifyToken, '⚡️ Bot launched');
 }
