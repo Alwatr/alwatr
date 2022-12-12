b1fd3fecf5f11c29f1edde1197d0b3cea5935cb0
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-12-12 15:51:06 +0330

    refactor(telegram-notifier): launchBot

--- services/telegram-notifier/src/bot/launch.ts
+++ services/telegram-notifier/src/bot/launch.ts
@@ -5,12 +5,16 @@ import {sendMessage} from './send-message.js';
 export async function launchBot(): Promise<void> {
   logger.logMethod('launchBot');
   try {
-    await bot.launch();
+    bot.botInfo ??= await bot.telegram.getMe();
+    logger.logProperty('botInfo', bot.botInfo);
+
+    bot.launch().catch((err) => {
+      logger.error('launchBot', 'launch_bot_failed', (err as Error).stack || err);
+    });
+
+    await sendMessage(config.telegramBot.debugNotifyToken, '⚡️ Bot launched');
   }
   catch (err) {
     logger.error('launchBot', 'launch_bot_failed', (err as Error).stack || err);
-    throw new Error('launch_bot_failed');
   }
-  logger.logProperty('botInfo', bot.botInfo);
-  sendMessage(config.telegramBot.debugNotifyToken, '⚡️ Bot launched');
 }
