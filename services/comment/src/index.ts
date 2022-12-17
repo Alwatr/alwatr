d7bef2e96894501aafe3fca2ae78889bcfbbdb3d
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-12-17 22:56:20 +0330

    refactor(comment)!: rename route /all to  /storage

--- services/comment/src/index.ts
+++ services/comment/src/index.ts
@@ -1,6 +1,6 @@
 import './route/home.js';
 import './route/patch.js';
-import './route/all.js';
+import './route/storage.js';
 import {logger} from './config.js';
 
 logger.logOther('..:: Alwatr Comment Nanoservice API ::..');
