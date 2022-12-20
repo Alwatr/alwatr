d4ff63cf966195e1a01c4161d248ae90c8a88531
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-12-20 23:32:24 +0330

    refactor(ff-pwa): updateTimer

--- uniquely/flight-finder-pwa/src/component/page-flight-finder.ts
+++ uniquely/flight-finder-pwa/src/component/page-flight-finder.ts
@@ -3,12 +3,11 @@ import {l10n} from '@alwatr/i18n';
 import {SignalInterface} from '@alwatr/signal';
 import {modalController} from '@ionic/core';
 import {css, html} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, query} from 'lit/decorators.js';
 import {map} from 'lit/directives/map.js';
 
 import ionNormalize from '../style/ionic.normalize.js';
 import ionTheming from '../style/ionic.theming.js';
-import {getHumanTime} from '../util/human-time.js';
 
 import './job-item.js';
 import './job-add-form.js';
@@ -51,12 +50,25 @@ export class PageFlightFinder extends AlwatrElement {
     `,
   ];
 
-  private __jobList: Array<Job> = [];
-  private __relativeTime = '';
 
   static jobDataSignal = new SignalInterface('job-data');
   static jobAddSignal = new SignalInterface('job-add');
   static relativeTimeFormatter = new Intl.RelativeTimeFormat('fa-IR', {style: 'narrow'});
+
+  private __jobList?: Array<Job>;
+  private __lastUpdate = 0;
+  @query('#timer') protected _timer?: HTMLSpanElement;
+
+  constructor() {
+    super();
+    this.__updateTimer = this.__updateTimer.bind(this);
+  }
+
+  private __updateTimer(): void {
+    const timer = this._timer;
+    if (timer == null || this.__lastUpdate === 0) return;
+    timer.innerText = l10n.formatNumber(Math.round((Date.now() - this.__lastUpdate) / 1000));
+  }
 
   override connectedCallback(): void {
     super.connectedCallback();
@@ -67,16 +79,11 @@ export class PageFlightFinder extends AlwatrElement {
 
     PageFlightFinder.jobDataSignal.addListener((jobList) => {
       this.__jobList = Object.values(jobList.data);
-
-      const now = new Date().getTime();
-      const relativeTimeObject = getHumanTime(now - (jobList.meta?.lastUpdated ?? now));
-
-      this.__relativeTime = PageFlightFinder.relativeTimeFormatter.format(
-          -Math.floor(relativeTimeObject.humanTime),
-          relativeTimeObject.units,
-      );
+      this.__lastUpdate = Date.now();
       this.requestUpdate();
     });
+
+    setInterval(this.__updateTimer, 1000);
   }
   override render(): TemplateResult {
     return html`
@@ -105,7 +112,7 @@ export class PageFlightFinder extends AlwatrElement {
       <ion-card class="job__list">
         <ion-card-header>
           <ion-card-title>${l10n.localize('search_list')}</ion-card-title>
-          <ion-card-subtitle>${this.__relativeTime}</ion-card-subtitle>
+          <ion-card-subtitle><span id="timer">-</span> ${l10n.localize('seconds_ago')}</ion-card-subtitle>
         </ion-card-header>
 
         <ion-list lines="full">
