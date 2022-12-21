b6af7ef59b33accf901dda07c02f24399506e132
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-12-21 11:23:34 +0330

    feat(uniquely/com-api): types

--- uniquely/com-api/src/lib/type.ts
+++ uniquely/com-api/src/lib/type.ts
@@ -1,3 +1,31 @@
 import type {AlwatrDocumentObject} from '@alwatr/storage-client';
 
-export type Order = AlwatrDocumentObject
+export type Order = AlwatrDocumentObject & {
+  user: User;
+  detail: OrderDetail;
+  itemList: Array<ProductValue>
+}
+
+export type User = AlwatrDocumentObject & {
+  name: string;
+  phoneNumber: string;
+  id: string;
+  nationalCode: number;
+}
+
+export type OrderDetail = {
+  description: string;
+  reciver: User;
+}
+
+export type Product = AlwatrDocumentObject & {
+  id: string;
+  name: string;
+  description: string;
+  type: string;
+  price: number;
+}
+
+export type ProductValue = Product & {
+  value: number;
+}
