94279c21891fc710f48642b1c8debbf02b2436d3
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-12-21 17:34:56 +0330

    fix: inline home route

--- services/starter/src/route/home.ts
+++ services/starter/src/route/home.ts
@@ -1,11 +1,10 @@
 import {nanoServer} from '../lib/nano-server.js';
 
-nanoServer.route('GET', '/', () => {
-  return {
-    ok: true,
-    data: {
-      app: 'Alwatr Nanoservice Starter Kit',
-      message: 'Hello ;)',
-    },
-  };
-});
+nanoServer.route('GET', '/', () => ({
+  ok: true,
+  data: {
+    app: 'Alwatr Nanoservice Starter Kit',
+    message: 'Hello ;)',
+  },
+}
+));
--- services/telegram-notifier/src/route/home.ts
+++ services/telegram-notifier/src/route/home.ts
@@ -1,11 +1,9 @@
 import {nanoServer} from '../lib/nano-server.js';
 
-nanoServer.route('GET', '/', () => {
-  return {
-    ok: true,
-    data: {
-      app: 'Alwatr Telegram Notify Microservice',
-      message: 'Hello ;)',
-    },
-  };
-});
+nanoServer.route('GET', '/', () => ({
+  ok: true,
+  data: {
+    app: 'Alwatr Telegram Notify Microservice',
+    message: 'Hello ;)',
+  },
+}));
