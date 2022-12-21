3b144839bc6c1760bc9feb29e5f48fb0836a5562
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-12-21 11:12:18 +0330

    chore(uniquely/com-api): remove types

--- uniquely/com-api/src/lib/type.ts
+++ uniquely/com-api/src/lib/type.ts
@@ -1,28 +1,3 @@
 import type {AlwatrDocumentObject} from '@alwatr/storage-client';
 
-export interface Job extends AlwatrDocumentObject {
-  detail: JobDetail;
-  resultList: Array<JobResult>;
-}
-
-export type dayParts = 'earlyMorning' | 'morning' | 'midday' | 'afternoon' | 'evening' | 'night';
-
-export interface JobDetail extends Record<string, unknown> {
-  origin: string;
-  destination: string;
-  date: string;
-  seatCount: number;
-  maxPrice: number | null;
-  dayPart: Array<dayParts>;
-  description: string;
-}
-
-export interface JobResult extends Record<string, unknown> {
-  price: number;
-  time: string;
-  seatCount: number;
-  airline: string,
-  airplane: string,
-  flightId: string,
-  arrivalTime: string,
-}
+export type Order = AlwatrDocumentObject
