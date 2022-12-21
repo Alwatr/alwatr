469c36aec2fd991b30c63753f6d6a13a8d063bfb
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-12-21 11:29:15 +0330

    feat(uniquely/com-api): improve types

--- uniquely/com-api/src/lib/type.ts
+++ uniquely/com-api/src/lib/type.ts
@@ -3,29 +3,30 @@ import type {AlwatrDocumentObject} from '@alwatr/storage-client';
 export type Order = AlwatrDocumentObject & {
   user: User;
   detail: OrderDetail;
-  itemList: Array<ProductValue>
-}
+  itemList: Array<ProductValue>;
+};
 
 export type User = AlwatrDocumentObject & {
   name: string;
   phoneNumber: string;
-  id: string;
+};
+
+export type Reciver = User & {
   nationalCode: number;
 }
 
 export type OrderDetail = {
   description: string;
-  reciver: User;
-}
+  reciver: Reciver;
+};
 
 export type Product = AlwatrDocumentObject & {
-  id: string;
   name: string;
   description: string;
   type: string;
   price: number;
-}
+};
 
 export type ProductValue = Product & {
   value: number;
-}
+};
