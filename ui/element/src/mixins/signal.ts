2f3c87141e162e8f600246bf863fcfe93f41f53b
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-12-26 00:10:34 +0330

    fix(element): signal mixing safety check

--- ui/element/src/mixins/signal.ts
+++ ui/element/src/mixins/signal.ts
@@ -1,7 +1,6 @@
 import {LitElement} from 'lit';
 
 import type {Constructor} from '../type.js';
-import type {ListenerInterface} from '@alwatr/signal';
 
 export declare class SignalMixinInterface extends LitElement {
   protected _signalListenerList: Array<unknown>;
@@ -11,12 +10,16 @@ export function SignalMixin<ClassType extends Constructor<LitElement>>(
     superClass: ClassType,
 ): Constructor<SignalMixinInterface> & ClassType {
   class SignalMixinClass extends superClass {
-    protected _signalListenerList: Array<unknown> = [];
+    protected _signalListenerList: Array<Record<string, unknown>> = [];
 
     override disconnectedCallback(): void {
       super.disconnectedCallback();
 
-      this._signalListenerList.forEach((listener) => (listener as ListenerInterface<keyof AlwatrSignals>).remove());
+      for (const listener of this._signalListenerList) {
+        if (typeof listener.remove === 'function') {
+          listener.remove();
+        }
+      }
     }
   }
 
