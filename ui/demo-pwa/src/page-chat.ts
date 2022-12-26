0f5bfd16b7c7973375c97f0a8b03d1e0b39bf148
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-12-26 23:35:37 +0330

    feat(ui/demo-pwa): use `alwatr-chat-text-input`

--- ui/demo-pwa/src/page-chat.ts
+++ ui/demo-pwa/src/page-chat.ts
@@ -3,6 +3,7 @@ import {customElement, AlwatrSmartElement, css, html} from '@alwatr/element';
 import '@alwatr/ui-kit/chat/chat-avatar.js';
 import '@alwatr/ui-kit/chat/chat-bubble.js';
 import '@alwatr/ui-kit/chat/chat-message.js';
+import '@alwatr/ui-kit/chat/chat-text-input.js';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -28,6 +29,11 @@ export class AlwatrPageHome extends AlwatrSmartElement {
       padding: 1.5rem;
       color: var(--md-sys-color-on-surface-variant);
       background-color: var(--md-sys-color-surface-variant);
+    }
+    p.on-primary {
+      color: var(--md-sys-color-on-surface);
+      background-color: var(--md-sys-color-surface);
+      border: 1px solid var(--md-sys-color-on-surface);
     }
 
     .section-name {
@@ -55,7 +61,7 @@ export class AlwatrPageHome extends AlwatrSmartElement {
         <span class="section-name">alwatr-chat-bubble</span>
         <alwatr-chat-bubble .message=${'سلام'}></alwatr-chat-bubble>
         <alwatr-chat-bubble .message=${'چطوری؟'} end-side></alwatr-chat-bubble>
-        <br/><br/>
+        <br /><br />
         <alwatr-chat-bubble
           .message=${'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
           ' Veritatis quia nemo eaque laboriosam unde consequatur!'}
@@ -65,14 +71,18 @@ export class AlwatrPageHome extends AlwatrSmartElement {
       <p>
         <span class="section-name">alwatr-chat-message</span>
         <alwatr-chat-message .user=${'1'} .message=${'سلام'}></alwatr-chat-message>
-        <br/>
+        <br />
         <alwatr-chat-message .user=${'2'} .message=${'چطوری؟'} end-side></alwatr-chat-message>
-        <br/>
+        <br />
         <alwatr-chat-message
           .user=${'3'}
           .message=${'Lorem ipsum dolor sit amet consectetur adipisicing elit.' +
           ' Veritatis quia nemo eaque laboriosam unde consequatur!'}
         ></alwatr-chat-message>
+      </p>
+      <p class="on-primary">
+        <span class="section-name">alwatr-chat-text-input</span>
+        <alwatr-chat-text-input></alwatr-chat-text-input>
       </p>
     `;
   }
