923aeb942eacd45d5b9a12bfda320f82399e2d6d
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-12-27 18:12:45 +0330

    feat(ui-kit): elevated, filled, outlined card components

---
+++ ui/ui-kit/src/card/filled-card.ts
@@ -0,0 +1,25 @@
+import {css, customElement} from '@alwatr/element';
+
+import {AlwatrCard} from './card.js';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'alwatr-filled-card': AlwatrFilledCard;
+  }
+}
+
+/**
+ * Alwatr filled card element.
+ */
+@customElement('alwatr-filled-card')
+export class AlwatrFilledCard extends AlwatrCard {
+  static override styles = [
+    ...AlwatrCard.styles,
+    css`
+      :host {
+        color: var(--md-sys-color-on-primary-container);
+        background-color: var(--md-sys-color-primary-container);
+      }
+    `,
+  ];
+}
