3ddae05cd0bcebb855327f0f449d3ee7c7e576cd
Ali Mihandoost <ali.mihandoost@gmail.com>
2022-12-27 22:42:49 +0330

    feat(chat-list): new element for list of chats

--- ui/demo-pwa/src/page-chat.ts
+++ ui/demo-pwa/src/page-chat.ts
@@ -3,8 +3,9 @@ import {customElement, AlwatrSmartElement, css, html, map} from '@alwatr/element
 import '@alwatr/ui-kit/chat/chat-avatar.js';
 import '@alwatr/ui-kit/chat/chat-bubble.js';
 import '@alwatr/ui-kit/chat/chat-message.js';
+import '@alwatr/ui-kit/chat/chat-list.js';
 
-import type {ChatMessage} from '@alwatr/ui-kit/chat/chat-message.js';
+import type {ChatStorage} from '@alwatr/ui-kit/chat/chat-list.js';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -12,64 +13,83 @@ declare global {
   }
 }
 
-const me = 'user-1';
+const currentUser = 'user-1';
 
-const messageList: Array<ChatMessage> = [
-  {
-    from: 'user-1',
-    type: 'text',
-    text: 'سلام',
+const chatStorage: ChatStorage = {
+  ok: true,
+  meta: {
+    formatVersion: 4,
+    lastAutoId: 0,
+    lastUpdated: Date.now(),
+    reversion: 100,
   },
-  {
-    from: 'user-2',
-    type: 'text',
-    text: 'با تمام جهل و مستی تصمیم گرفته‌ام دفترچه رزوگار را با پاک کنِ مهر و عطوفت پاک کنم\n' +
-      'و از اول با نام تو روزگار را آغاز کنم.',
+  data: {
+    '1': {
+      id: '1',
+      from: 'user-1',
+      type: 'text',
+      text: 'سلام',
+    },
+    '2': {
+      id: '2',
+      from: 'user-2',
+      type: 'text',
+      text: 'با تمام جهل و مستی تصمیم گرفته‌ام دفترچه رزوگار را با پاک کنِ مهر و عطوفت پاک کنم\n' +
+        'و از اول با نام تو روزگار را آغاز کنم.',
+    },
+    '3': {
+      id: '3',
+      from: 'user-1',
+      type: 'text',
+      text: 'هنوز در نخستین صفحات آن مانده‌ام و مطلبی برای نوشتن ندارم.تا پایان نوشتن انتظارت می‌کشم.',
+    },
+    '4': {
+      id: '4',
+      from: 'user-4',
+      type: 'text',
+      text: 'دیوانه مسلمانی که در روزهای انتظار هزار بار به دیوانگی‌اش ایمان می‌آورد….',
+    },
+    '5': {
+      id: '5',
+      from: 'user-1',
+      type: 'text',
+      text: 'کودک که بودم پدرم جمعه ها صبح درب خانه را آب و جارو می کرد تا  اگر مولا از آن حوالی عبور کند …',
+    },
+    '6': {
+      id: '6',
+      from: 'user-2',
+      type: 'text',
+      text: 'و نو روز هر باره این حس را در من زنده می کند',
+    },
+    '7': {
+      id: '7',
+      from: 'user-2',
+      type: 'text',
+      text: 'مردمی را می بینم که سراسر شوق و شورند ، خانه تکانی می کنند و لباس های نو برتن…',
+    },
+    '8': {
+      id: '8',
+      from: 'user-2',
+      type: 'text',
+      text: 'اما برای چه ؟ برای که ؟\nاینان منتظرند تا  بهار شود ؟',
+    },
+    '9': {
+      id: '9',
+      from: 'user-4',
+      type: 'text',
+      text: 'سالهاست می اندیشم که هنگام بهار مگر چه می شود که اینگونه به هم می ریزیم' +
+        '،مهربان می شویم، به سراغ هم می رویم و از همه مهمتر منتظر می شویم…',
+    },
+    '10': {
+      id: '10',
+      from: 'user-4',
+      type: 'text',
+      text: 'انتظار...',
+    },
   },
-  {
-    from: 'user-1',
-    type: 'text',
-    text: 'هنوز در نخستین صفحات آن مانده‌ام و مطلبی برای نوشتن ندارم.تا پایان نوشتن انتظارت می‌کشم.',
-  },
-  {
-    from: 'user-4',
-    type: 'text',
-    text: 'دیوانه مسلمانی که در روزهای انتظار هزار بار به دیوانگی‌اش ایمان می‌آورد….',
-  },
-  {
-    from: 'user-1',
-    type: 'text',
-    text: 'کودک که بودم پدرم جمعه ها صبح درب خانه را آب و جارو می کرد تا  اگر مولا از آن حوالی عبور کند …',
-  },
-  {
-    from: 'user-2',
-    type: 'text',
-    text: 'و نو روز هر باره این حس را در من زنده می کند',
-  },
-  {
-    from: 'user-2',
-    type: 'text',
-    text: 'مردمی را می بینم که سراسر شوق و شورند ، خانه تکانی می کنند و لباس های نو برتن…',
-  },
-  {
-    from: 'user-2',
-    type: 'text',
-    text: 'اما برای چه ؟ برای که ؟\nاینان منتظرند تا  بهار شود ؟',
-  },
-  {
-    from: 'user-4',
-    type: 'text',
-    text: 'سالهاست می اندیشم که هنگام بهار مگر چه می شود که اینگونه به هم می ریزیم' +
-      '،مهربان می شویم، به سراغ هم می رویم و از همه مهمتر منتظر می شویم…',
-  },
-  {
-    from: 'user-4',
-    type: 'text',
-    text: 'انتظار...',
-  },
-];
+};
 
-const messageListShort = messageList.splice(0, 5);
+const messageListShort = Object.values(chatStorage.data).splice(0, 5);
 
 /**
  * Alwatr Demo Home Page
@@ -122,18 +142,18 @@ export class AlwatrPageHome extends AlwatrSmartElement {
       <p>
         <span class="section-name">alwatr-chat-bubble</span>
         ${map(messageListShort, (message) => html`
-          <alwatr-chat-bubble .text=${message.text} ?end-side=${message.from !== me}></alwatr-chat-bubble>
+          <alwatr-chat-bubble .text=${message.text} ?end-side=${message.from !== currentUser}></alwatr-chat-bubble>
         `)}
       </p>
 
       <p>
         <span class="section-name">alwatr-chat-message</span>
         ${map(messageListShort, (message) => html`
-          <alwatr-chat-message .message=${message} ?end-side=${message.from !== me}></alwatr-chat-message>
+          <alwatr-chat-message .message=${message} ?end-side=${message.from !== currentUser}></alwatr-chat-message>
         `)}
       </p>
 
-      <alwatr-chat-list .list=${messageList}></alwatr-chat-list>
+      <alwatr-chat-list .storage=${chatStorage} .currentUser=${currentUser}></alwatr-chat-list>
     `;
   }
 }
