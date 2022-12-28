2de384297b70b9818ec8d62ce8d31be4822d95a4
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-12-28 23:06:32 +0330

    feat(type): add types

---
+++ core/type/src/storage.ts
@@ -0,0 +1,23 @@
+import type {AlwatrServiceResponseSuccessWithMeta} from './service.js';
+
+export type AlwatrDocumentObject = {
+  id: string;
+  meta?: {
+    rev: number;
+    created: number;
+    updated: number;
+  };
+};
+
+export type AlwatrDocumentMeta = {
+  formatVersion: number;
+  reversion: number;
+  lastUpdated: number;
+  lastAutoId: number;
+};
+
+
+export type AlwatrDocumentStorage<T extends AlwatrDocumentObject> = Omit<
+  AlwatrServiceResponseSuccessWithMeta<Record<string, T>, AlwatrDocumentMeta>,
+  'statusCode' | 'errorCode'
+>;
---
+++ core/type/src/type.ts
@@ -0,0 +1,5 @@
+export * from './storage.js';
+export * from './service.js';
+
+// FIXME:
+export type MaybePromise<T> = T | Promise<T>
