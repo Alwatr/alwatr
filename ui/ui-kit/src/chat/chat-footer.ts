5151d1c50c1fd63e1bee479220a4363a9ab800e3
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-12-29 19:49:45 +0330

    feat(ui-kit/chat-footer): new component

--- ui/ui-kit/src/chat/chat-footer.ts
+++ ui/ui-kit/src/chat/chat-footer.ts
@@ -1,5 +1,8 @@
 import {AlwatrDummyElement, css, customElement, html} from '@alwatr/element';
 
+import './chat-text-input.js';
+import '../icon-button/standard-icon-button.js';
+
 declare global {
   interface HTMLElementTagNameMap {
     'alwatr-chat-footer': AlwatrChatFooter;
@@ -14,19 +17,27 @@ export class AlwatrChatFooter extends AlwatrDummyElement {
   static override styles = css`
     :host {
       display: flex;
-      width: 100%;
 
       color: var(--md-sys-color-on-surface);
       background-color: var(--md-sys-color-surface);
 
+      gap: var(--md-sys-spacing-track-1);
       padding: var(--md-sys-spacing-track-2);
 
-      border-top-left-radius: var(--md-sys-shape-corner-extra-large-top-top-left);
-      border-top-right-radius: var(--md-sys-shape-corner-extra-large-top-top-right);
+      border-top-left-radius: var(--md-sys-shape-corner-large-top-top-left);
+      border-top-right-radius: var(--md-sys-shape-corner-large-top-top-right);
+    }
+
+    alwatr-standard-icon-button {
+      width: var(--md-sys-spacing-track-6);
+      height: var(--md-sys-spacing-track-6);
     }
   `;
 
   override render(): unknown {
-    return html``;
+    return html`
+      <alwatr-standard-icon-button .icon=${'happy-outline'}></alwatr-standard-icon-button>
+      <alwatr-chat-text-input></alwatr-chat-text-input>
+    `;
   }
 }
