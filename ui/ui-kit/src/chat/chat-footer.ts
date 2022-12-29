38e3bbc5d35242466cd4e33bf4bcacd5c6a87368
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-12-29 19:14:18 +0330

    fix(ui-kit/chat-footer): styles

--- ui/ui-kit/src/chat/chat-footer.ts
+++ ui/ui-kit/src/chat/chat-footer.ts
@@ -14,11 +14,15 @@ export class AlwatrChatFooter extends AlwatrDummyElement {
   static override styles = css`
     :host {
       display: flex;
-      border-top-left-radius: var(--md-sys-shape-corner-extra-large-top-top-left);
-      border-top-right-radius: var(--md-sys-shape-corner-extra-large-top-top-right);
+      width: 100%;
+
       color: var(--md-sys-color-on-surface);
       background-color: var(--md-sys-color-surface);
+
       padding: var(--md-sys-spacing-track-2);
+
+      border-top-left-radius: var(--md-sys-shape-corner-extra-large-top-top-left);
+      border-top-right-radius: var(--md-sys-shape-corner-extra-large-top-top-right);
     }
   `;
 
