383fd3502b290a9cc0557647dbf66ba994537c38
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-01 12:29:25 +0330

    refactor(type): global alwatr type

--- core/type/src/alwatr.ts
+++ core/type/src/alwatr.ts
@@ -1,34 +1,23 @@
 /* eslint-disable no-var */
-export interface GlobalAlwatr {
+export type AlwatrPackageInfo = {
+  name: string;
   version: string;
-  registeredList: Array<{
-    name: string;
-    version: string;
-  }>;
+}
+
+export type GlobalAlwatr = {
+  registeredList: Array<AlwatrPackageInfo>;
 }
 
 declare global {
   var Alwatr: GlobalAlwatr;
-  var ALWATR_DEBUG: string | undefined;
-
-  /**
-   * @TODO: Make an issue to TS, WTF is this way! any better solution for more args in bind?!
-   */
-  interface CallableFunction {
-    bind<T, A0, A1, A2, A3, A4, A extends unknown[], R>(
-      this: (this: T, arg0: A0, arg1: A1, arg2: A2, arg3: A3, arg4: A4, ...args: A) => R,
-      thisArg: T,
-      arg0: A0,
-      arg1: A1,
-      arg2: A2,
-      arg3: A3,
-      arg4: A4
-    ): (...args: A) => R;
-  }
 }
 
-/**
- * Define `globalThis.Alwatr.registeredList`
- */
-export const alwatrRegisteredList = globalThis.Alwatr?.registeredList || [];
-globalThis.Alwatr ??= {version: '0.26.0', registeredList: alwatrRegisteredList};
+if (globalThis.Alwatr == null) {
+  globalThis.Alwatr = {
+    registeredList: [],
+  };
+}
+
+if (Alwatr.registeredList == null) {
+  Alwatr.registeredList = [];
+}
