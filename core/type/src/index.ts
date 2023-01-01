f16d97db5faaa8bfcb907bdd8e1f9646c40087b9
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-01 12:29:47 +0330

    refactor(type): ts type helper

--- core/type/src/index.ts
+++ core/type/src/index.ts
@@ -4,8 +4,7 @@ export * from './storage.js';
 export * from './alwatr.js';
 export * from './math.js';
 
-// FIXME:
-export type MaybePromise<T> = T | Promise<T>
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type Constructor<ClassType = Record<string, unknown>> = new (...args: any[]) => ClassType;
+Alwatr.registeredList.push({
+  name: '@alwatr/type',
+  version: '{{ALWATR_VERSION}}',
+});
