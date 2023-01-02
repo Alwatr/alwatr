cd2f7ecb994126e64202d99ba68a0e621fdc20cd
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-01-02 10:52:23 +0330

    feat(com-api): order entity route

--- uniquely/com-api/src/index.ts
+++ uniquely/com-api/src/index.ts
@@ -3,5 +3,6 @@ import {logger} from './config.js';
 import './route/home.js';
 import './route/product/get.js';
 import './route/product/put.js';
+import './route/order/put.js';
 
 logger.logOther('..:: Customer Order Management API ::..');
