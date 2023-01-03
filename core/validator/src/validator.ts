60829a6280fdc3138f8702cee776e0dae11f548f
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-03 16:08:09 +0330

    feat(validator): additionalProperties

--- core/validator/src/validator.ts
+++ core/validator/src/validator.ts
@@ -7,11 +7,13 @@ export {JsonSchema};
 export function validator<T extends ValidType>(
     validSchema: JsonSchema,
     targetObject: Record<string, unknown>,
+    additionalProperties = false,
     path = '.',
 ): T {
   if (typeof targetObject !== 'object' || targetObject == null) {
     throw new Error('invalid_type', {
       cause: {
+        message: 'targetObject is not a function or null',
         itemPath: path,
         itemSchema: 'JsonSchema',
         itemValue: String(targetObject),
@@ -19,32 +21,36 @@ export function validator<T extends ValidType>(
     });
   }
 
-  for (const itemName in targetObject) {
-    if (!Object.prototype.hasOwnProperty.call(targetObject, itemName)) continue;
+  if (
+    additionalProperties === false &&
+    Object.keys(validSchema).sort().join() !== Object.keys(targetObject).sort().join()
+  ) {
+    throw new Error('invalid_type', {
+      cause: {
+        message: 'Object.keys(validSchema) !== Object.keys(targetObject)',
+        itemPath: path,
+        itemSchema: String(validSchema),
+        itemValue: String(targetObject),
+      },
+    });
+  }
+
+  for (const itemName in validSchema) {
+    if (!Object.prototype.hasOwnProperty.call(validSchema, itemName)) continue;
 
     const itemPath = `${path}/${itemName}`;
     const itemSchema = validSchema[itemName];
     const itemValue = targetObject[itemName] as string | number | boolean | Record<string, unknown>;
 
-    if (itemSchema == null) {
-      throw new Error('invalid_type', {
-        cause: {
-          itemPath,
-          itemSchema: 'undefined',
-          itemValue: String(itemValue),
-        },
-      });
-    }
-
-    else if (typeof itemSchema === 'object') {
+    if (typeof itemSchema === 'object' && itemSchema != null) {
       // nested object
       targetObject[itemName] = validator<ValidType>(
           itemSchema,
-          itemValue as Record<string, unknown>,
-          itemPath,
+        itemValue as Record<string, unknown>,
+        additionalProperties,
+        itemPath,
       );
     }
-
     else if (itemSchema === Boolean) {
       const strValue = String(itemValue).toLowerCase();
       if (strValue === 'true') {
@@ -56,6 +62,7 @@ export function validator<T extends ValidType>(
       else {
         throw new Error('invalid_type', {
           cause: {
+            message: 'invalid type',
             itemPath,
             itemSchema: 'Boolean',
             itemValue: String(itemValue),
@@ -63,7 +70,6 @@ export function validator<T extends ValidType>(
         });
       }
     }
-
     else if (itemSchema === Number) {
       if (isNumber(itemValue)) {
         targetObject[itemName] = +itemValue;
@@ -71,6 +77,7 @@ export function validator<T extends ValidType>(
       else {
         throw new Error('invalid_type', {
           cause: {
+            message: 'invalid type',
             itemPath,
             itemSchema: 'Number',
             itemValue: String(itemValue),
@@ -78,14 +85,11 @@ export function validator<T extends ValidType>(
         });
       }
     }
-
     else if (itemSchema === String) {
-      if (typeof itemValue === 'string') {
-        targetObject[itemName] = itemValue;
-      }
-      else {
+      if (typeof itemValue !== 'string') {
         throw new Error('invalid_type', {
           cause: {
+            message: 'invalid type',
             itemPath,
             itemSchema: 'String',
             itemValue: String(itemValue),
