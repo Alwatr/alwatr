a0d824202d91d75678945d72497965a55c4deba7
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-08 15:39:12 +0330

    feat(type): user

--- core/type/src/user.ts
+++ core/type/src/user.ts
@@ -1,6 +1,21 @@
 import type {AlwatrDocumentObject} from './storage.js';
 
+export const genderCS = ['male', 'female'] as const;
+export type Gender = typeof genderCS[number];
+
 export type User = AlwatrDocumentObject & {
-  name: string;
-  phoneNumber: string;
+  /**
+   * User global unique id (verifiable)
+   */
+  id: string;
+
+  /**
+   * User full name
+   */
+  fullName: string;
+
+  gender?: Gender;
+
+  email?: string;
+  phoneNumber?: string;
 };
