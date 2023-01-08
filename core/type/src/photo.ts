7e38283113606a0a01c705caeb1f773a75dfd97c
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-08 15:39:21 +0330

    feat(type): photo

--- core/type/src/photo.ts
+++ core/type/src/photo.ts
@@ -1,8 +1,15 @@
 import {AlwatrDocumentObject} from './storage.js';
 
 export type Photo = AlwatrDocumentObject & {
-  fileName: string;
-  meta: {
-    description: string;
-  };
+  /**
+   * Primary Photo ID
+   *
+   * like full relative path (include extension) to image CDN (temporary)
+   */
+  id: string; // path/file-name.png
+
+  /**
+   * Photo extra meta information for future maintenances
+   */
+  meta: Record<string, string | number>; // meta: {order: 1233, customer: 1334}
 }
