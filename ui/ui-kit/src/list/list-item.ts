bbc7ee4fdcc69cb83b6c135cb7a3afb8f7d02f5d
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-01-09 11:39:21 +0330

    feat(ui-kit/list/item): new components

--- ui/ui-kit/src/list/list-item.ts
+++ ui/ui-kit/src/list/list-item.ts
@@ -1,4 +1,4 @@
-import {customElement, AlwatrDummyElement, css, html, DirectionMixin} from '@alwatr/element';
+import {customElement, AlwatrDummyElement, css, html, DirectionMixin, property} from '@alwatr/element';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -14,14 +14,54 @@ export class AlwatrListItem extends DirectionMixin(AlwatrDummyElement) {
   static override styles = css`
     :host {
       display: block;
-      padding: var(--sys-spacing-track);
+      padding: var(--sys-spacing-track) calc(2 * var(--sys-spacing-track));
       user-select: none;
+
+      color: var(--sys-color-on-surface);
+      background-color: var(--sys-color-surface);
+    }
+
+    :host([dir='ltr']) {
+      padding-right: calc(3 * var(--sys-spacing-track));
+    }
+
+    :host([dir='rtl']) {
+      padding-left: calc(3 * var(--sys-spacing-track));
     }
   `;
 
+  @property()
+    headline?: string;
+
+  @property()
+    description?: string;
+
+  @property()
+    icon?: string;
+
+  @property()
+    actionIcon?: string;
+
+  @property({type: Boolean})
+    checkmark = false;
+
+  @property({type: Boolean, reflect: true})
+    chips = false;
+
+  @property({type: Boolean, reflect: true})
+    selected = false;
+
+  @property({type: Boolean, reflect: true})
+    disabled = false;
+
+  @property({type: Boolean, reflect: true})
+    selectable = false;
+
+  @property({type: Boolean, reflect: true})
+    highlighted = false;
+
   override render(): unknown {
     super.render();
-    return html`
-    `;
+    return html``;
   }
 }
