481031c93b0e49f7a541087da1fd283c5bfada36
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-10 17:43:39 +0330

    fix(card): review styles

--- ui/ui-kit/src/card/card.ts
+++ ui/ui-kit/src/card/card.ts
@@ -1,20 +1,22 @@
-import {AlwatrDummyElement, css, html, TemplateResult} from '@alwatr/element';
+import {AlwatrDummyElement, css, html} from '@alwatr/element';
 
+import type {CSSResultGroup} from '@alwatr/element';
+
+/**
+ * Alwatr Card Base Element
+ */
 export class AlwatrCard extends AlwatrDummyElement {
-  static override styles = [
-    css`
-      :host {
-        display: flex;
-        flex-direction: column;
-        border-radius: var(--sys-shape-corner-medium-default-size);
-        color: var(--sys-color-on-surface);
+  static override styles: CSSResultGroup = css`
+    :host {
+      --elevation-level: 0;
+      border-radius: var(--sys-shape-corner-medium-default-size);
+      color: var(--sys-color-on-surface);
+      background-color: var(--sys-color-surface);
+    }
+  `;
 
-        --elevation-level: 0;
-      }
-    `,
-  ];
-
-  override render(): TemplateResult {
-    return html` <slot></slot> `;
+  override render(): unknown {
+    super.render();
+    return html`<slot></slot>`;
   }
 }
