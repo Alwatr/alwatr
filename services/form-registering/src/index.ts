9161b22a006550e6ef6938d2e6ae47d9eddf9a00
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-01-15 19:47:45 +0330

    refactor(form-registering): post method for setForm

--- services/form-registering/src/index.ts
+++ services/form-registering/src/index.ts
@@ -1,7 +1,7 @@
 import {logger} from './config.js';
 
 import './route/home.js';
-import './route/put.js';
+import './route/post.js';
 import './route/storage.js';
 
 logger.logOther('..:: Alwatr Form Registration API Nanoservice ::..');
