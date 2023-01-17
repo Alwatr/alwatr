5c2ac530292d0bea361fa442b57748e06cb733d2
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-01-17 08:34:56 +0330

    feat(salavat-api): vowCountSum

--- uniquely/salavat-api/src/index.ts
+++ uniquely/salavat-api/src/index.ts
@@ -3,4 +3,6 @@ import {logger} from './config.js';
 import './route/home.js';
 import './route/vow/post.js';
 
+import './global-vow.js';
+
 logger.logOther('..:: Alwatr Salavat Application API ::..');
