1165cf76464baed19933e66d98117ad31d92ea03
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-18 01:50:41 +0330

    feat(pwa): replace file hash in html

--- ui/demo-pwa/esbuild.mjs
+++ ui/demo-pwa/esbuild.mjs
@@ -1,42 +1,52 @@
 #!/usr/bin/env node
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 
-import {rmSync, cp} from 'node:fs';
+import {promises as fs, existsSync} from 'node:fs';
 
 import {createLogger} from '@alwatr/logger';
-import {build} from 'esbuild';
+import * as esbuild from 'esbuild';
 
 import packageJson from './package.json' assert {type: 'json'};
 
 const logger = createLogger('alwatr-pwa-build');
 const banner = '/* ..:: Alwatr UI Demo ::.. */\n';
-const dist = 'dist';
+const resDir = 'res';
+const outDir = 'dist';
+const srcFilename = 'alwatr-pwa';
 
 logger.logOther(banner);
 
-rmSync(dist, {recursive: true, force: true});
-logger.logOther('dist removed');
+if (process.argv.includes('--clean')) {
+  logger.logMethod('clean build');
+  await fs.rmdir(outDir, {recursive: true, force: true});
+}
 
-cp('assets', dist, {recursive: true, force: true, verbatimSymlinks: true}, ()=>logger.logOther('cp assets done'));
+const copyPromise = fs.cp(resDir, outDir, {recursive: true, force: true, verbatimSymlinks: true});
 
-await build({
-  entryPoints: ['src/alwatr-pwa.ts'],
+const buildPromise = esbuild.build({
+  entryPoints: [`src/${srcFilename}.ts`],
 
-  logLevel: 'debug',
+  logLevel: 'info',
   platform: 'browser',
   target: 'es2018',
   format: 'esm',
+  conditions: ['development'], // dev-mode
 
-  // minify: true,
+  minify: false, // dev-mode
   treeShaking: true,
   sourcemap: true,
+  sourcesContent: true, // dev-mode
   bundle: true,
   splitting: true,
-  charset: 'utf8',
+  charset: 'ascii',
   legalComments: 'none',
+  metafile: true,
 
   define: {
-    '_ALWATR_VERSION_': `'${packageJson.version}'`,
+    _ALWATR_VERSION_: `'${packageJson.version}'`,
   },
+  // drop: ['debugger'],
 
   loader: {
     '.png': 'copy',
@@ -51,21 +61,69 @@ await build({
   },
 
   outbase: 'src',
-  outdir: dist,
+  outdir: outDir,
   assetNames: 'asset/[name]-[hash]',
   entryNames: '[dir]/[name]-[hash]',
   chunkNames: 'chunks/[name]-[hash]',
 });
 
+async function makeHtml() {
+  logger.logMethod('makeHtml');
+
+  let htmlContent = await fs.readFile(`${resDir}/index.html`, {encoding: 'utf-8'});
+
+  const metafile = (await buildPromise).metafile;
+  const outFiles = Object.keys(metafile.outputs);
+
+  const jsFilename = outFiles
+      .find((filename) => filename.includes(srcFilename) && filename.endsWith('.js'))
+      .substring(outDir.length + 1);
+
+  const cssFilename = outFiles
+      .find((filename) => filename.includes(srcFilename) && filename.endsWith('.css'))
+      .substring(outDir.length + 1);
+
+  logger.logProperty('jsFilename', jsFilename);
+  logger.logProperty('cssFilename', cssFilename);
+
+  if (!existsSync(`${outDir}/${jsFilename}`)) {
+    logger.error('makeHtml', 'js_filename_not_found', {jsFilename});
+    throw new Error('js_filename_not_found');
+  }
+
+  if (!existsSync(`${outDir}/${cssFilename}`)) {
+    logger.error('makeHtml', 'css_filename_not_found', {cssFilename});
+    throw new Error('css_filename_not_found');
+  }
+
+  htmlContent = htmlContent
+      .replace('alwatr-pwa.css', cssFilename)
+      .replace('alwatr-pwa.js', jsFilename)
+  ;
+
+  await copyPromise; // wait to cp done
+  await fs.writeFile(`${outDir}/index.html`, htmlContent, {encoding: 'utf-8', flag: 'w'});
+}
+
+makeHtml();
+
+console.log(await esbuild.analyzeMetafile((await buildPromise).metafile));
+
 /*
   TODO:
   - Serve mode or use wds
   - Watch mode
-  - Replace file hash in html
   - Assets hash
   - PostCSS css file
+  - lit css loader
   - PostCSS lit internal styles
-
+  - Dynamic from @alwatr/build
+  - sideEffects
+    https://esbuild.github.io/api/#ignore-annotations
+    https://webpack.js.org/guides/tree-shaking/
+  - readme
     https://esbuild.github.io/api/#write
     https://github.com/fakundo/esbuild-plugin-replace-regex/blob/master/src/index.js#L30
+    https://github.com/zandaqo/esbuild-plugin-lit/blob/master/css-loader.ts
+    https://github.com/chialab/rna/tree/main/packages/esbuild-plugin-html
 */
