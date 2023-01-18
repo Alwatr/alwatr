dc1eaa44aa1c5694d1f8ff635bd04f56dac672b1
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-18 23:44:01 +0330

    fix(soffit-pwa): review and fix root element

--- uniquely/soffit-site-pwa/src/alwatr-pwa.ts
+++ uniquely/soffit-site-pwa/src/alwatr-pwa.ts
@@ -11,15 +11,15 @@ import type {RoutesConfig} from '@alwatr/router';
 
 declare global {
   interface HTMLElementTagNameMap {
-    'alwatr-pwa-root': AlwatrPwaRoot;
+    'alwatr-pwa': AlwatrPwa;
   }
 }
 
 /**
  * Alwatr PWA Root Element
  */
-@customElement('alwatr-pwa-root')
-export class AlwatrPwaRoot extends AlwatrPwaElement {
+@customElement('alwatr-pwa')
+class AlwatrPwa extends AlwatrPwaElement {
   protected override _routes: RoutesConfig = {
     map: (route) => route.sectionList[0]?.toString(),
     list: {
