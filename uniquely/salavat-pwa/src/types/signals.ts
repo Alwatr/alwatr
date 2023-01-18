2a037dc130874a89b4a17439378efcf3dc7b8f10
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-01-18 14:14:58 +0330

    feat(uniquely/salavat-pwa): signals type

---
+++ uniquely/salavat-pwa/src/types/signals.ts
@@ -0,0 +1,11 @@
+// import type {NavigationDrawer} from './signals/navigation-drawer.js';
+
+declare global {
+  // interface AlwatrSignals {
+  //   readonly 'navigation-drawer': NavigationDrawer;
+  // }
+
+  // interface AlwatrRequestSignals {
+  //   readonly 'content-change': number;
+  // }
+}
