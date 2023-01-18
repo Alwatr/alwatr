6ec8b59a617d514315926dac7c3e3318867782d2
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-19 01:18:24 +0330

    feat(element): service-worker helper

--- ui/element/src/helper/service-worker/register.ts
+++
@@ -1,5 +0,0 @@
-export function registerServiceWorker(serviceWorkerPath: string): Promise<ServiceWorkerRegistration> | null {
-  if (!('serviceWorker' in navigator)) return null;
-
-  return navigator.serviceWorker.register(serviceWorkerPath);
-}
