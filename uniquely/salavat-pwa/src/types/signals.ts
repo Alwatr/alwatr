319a2896b0af3f2f96806bd101003190f37c491a
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2023-01-24 16:36:12 +0330

    feat(uniquely/salavat-pwa): define new signal

--- uniquely/salavat-pwa/src/types/signals.ts
+++ uniquely/salavat-pwa/src/types/signals.ts
@@ -1,4 +1,5 @@
 import type {NavigationDrawer} from './signals/navigation-drawer.js';
+import type {SalavatCount} from './signals/salavat-count.js';
 import type {SalavatSubmitButton} from './signals/salavat-submit-button.js';
 import type {SnackBar} from './signals/snack-bar.js';
 
@@ -6,11 +7,12 @@ declare global {
   interface AlwatrSignals {
     readonly 'navigation-drawer': NavigationDrawer;
     readonly 'snack-bar': Partial<SnackBar>;
+    readonly 'salavat-count': SalavatCount;
     readonly 'salavat-submit-button': SalavatSubmitButton;
     readonly 'salavat-increase': number;
   }
 
-  // interface AlwatrRequestSignals {
-  //   readonly 'content-change': number;
-  // }
+  interface AlwatrRequestSignals {
+    readonly 'salavat-count': number;
+  }
 }
---
+++ uniquely/salavat-pwa/src/types/signals/salavat-count.ts
@@ -0,0 +1,4 @@
+export type SalavatCount = {
+  mySalavatCount: number;
+  totalSalavatCount: number;
+}
