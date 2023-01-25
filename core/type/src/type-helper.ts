b5a778f3da03244a0ad693ab15ef8dd782039163
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-25 15:45:13 +0330

    feat(type): OmitFirstParam

--- core/type/src/type-helper.ts
+++ core/type/src/type-helper.ts
@@ -1,4 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export type Constructor<ClassType = Record<string, unknown>> = new (...args: any[]) => ClassType;
 
 export type MaybePromise<T> = T | Promise<T>;
+
+export type OmitFirstParam<F> = F extends (x: any, ...args: infer A) => infer R ? (...args: A) => R : never;
