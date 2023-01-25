d9e5837f3d61be92159e42797d0e083c6e8db7ca
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-01-25 23:15:10 +0330

    feat(toast): close

--- ui/ui-kit/src/toast/toast.ts
+++ ui/ui-kit/src/toast/toast.ts
@@ -1,4 +1,4 @@
-import {css, customElement, html, property, state} from '@alwatr/element';
+import {css, customElement, html, nothing, property, TemplateResult} from '@alwatr/element';
 
 import '@alwatr/icon';
 import {AlwatrSurface} from '../card/surface.js';
@@ -9,9 +9,9 @@ declare global {
   }
 }
 
-export type ToastOptions = {
+export type ToastContent = {
   message: string;
-  autoClose: boolean;
+  autoHideDelay: number;
   show: boolean;
 };
 
@@ -20,6 +20,11 @@ export type ToastOptions = {
  */
 @customElement('alwatr-toast')
 export class AlwatrToast extends AlwatrSurface {
+  constructor() {
+    super();
+    this._hide = this._hide.bind(this);
+  }
+
   static override styles = [
     AlwatrSurface.styles,
     css`
@@ -34,18 +39,19 @@ export class AlwatrToast extends AlwatrSurface {
         bottom: 0;
         right: 0;
         left: 0;
-        margin: var(--sys-spacing-track) calc(2 * var(--sys-spacing-track));
+        margin: var(--sys-spacing-track) calc(4 * var(--sys-spacing-track));
         padding: calc(2 * var(--sys-spacing-track));
         background-color: var(--sys-color-inverse-surface);
         border-radius: var(--sys-radius-small);
         transition: bottom, top, margin, opacity;
-        transition-duration:var(--sys-motion-duration-small);
+        transition-duration: var(--sys-motion-duration-small);
         transition-timing-function: var(--sys-motion-easing-normal);
         opacity: 0;
+        user-select: none;
       }
 
       :host([show]) {
-        margin: calc(2 * var(--sys-spacing-track));
+        margin: calc(2 * var(--sys-spacing-track)) calc(4 * var(--sys-spacing-track));
         opacity: 1;
       }
 
@@ -53,6 +59,7 @@ export class AlwatrToast extends AlwatrSurface {
         margin: 0;
         padding: 0;
         display: inline;
+        max-width: 50em;
         color: var(--sys-color-inverse-on-surface);
         font-family: var(--sys-typescale-body-medium-font-family-name);
         font-weight: var(--sys-typescale-body-medium-font-weight);
@@ -69,37 +76,46 @@ export class AlwatrToast extends AlwatrSurface {
     `,
   ];
 
-  @state() protected _options: ToastOptions = {
-    message: 'Simple message to show a message to the user',
-    autoClose: false,
-    show: true,
-  };
-
-  @property({type: Boolean, reflect: true})
-    show = this._options.show;
+  @property({type: Object, reflect: true})
+    content: ToastContent = {
+      message: '',
+      show: false,
+      autoHideDelay: -1,
+    };
 
   protected _show(): void {
     this._logger.logMethod('_show');
     this.setAttribute('show', '');
   }
 
-  protected _close(): void {
-    this._logger.logMethod('_close');
+  protected _hide(): void {
+    this._logger.logMethod('_hide');
     this.removeAttribute('show');
   }
 
-  protected override async firstUpdated(): Promise<void> {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+  protected _newMessage(options: ToastContent): void {
+    this.content = options;
     this._show();
+    if (this.content.autoHideDelay !== 0) setTimeout(this._hide, this.content.autoHideDelay);
+  }
+
+  override connectedCallback(): void {
+    super.connectedCallback();
     this.setAttribute('elevated', '3');
   }
 
   override render(): unknown {
-    this._logger.logMethod('render');
-
+    super.render();
     return html`
-      <p class="message">${this._options.message}</p>
-      <alwatr-icon @click=${this._close} class="close-icon" name="close"></alwatr-icon>
+      <p class="message">${this.content.message}</p>
+      ${this._closeButtonTemplate()}
     `;
+  }
+
+  protected _closeButtonTemplate(): TemplateResult | typeof nothing {
+    this._logger.logMethod('_closeButtonTemplate');
+
+    if (this.content.autoHideDelay !== 0) return nothing;
+    return html`<alwatr-icon @click=${this._hide} class="close-icon" name="close"></alwatr-icon>`;
   }
 }
