c2e077118e0371fb32a5fc3fc81cf7f87d65104b
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-26 14:12:58 +0330

    feat(type): Stringifyable

--- core/type/src/type-helper.ts
+++ core/type/src/type-helper.ts
@@ -1,7 +1,19 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export type Constructor<ClassType = Record<string, unknown>> = new (...args: any[]) => ClassType;
+export type Constructor<T> = new (...args: any[]) => T;
 
 export type MaybePromise<T> = T | Promise<T>;
 
 export type OmitFirstParam<F> = F extends (x: any, ...args: infer A) => infer R ? (...args: A) => R : never;
+
+/**
+ * Object that can be JSON.stringify.
+ */
+export type Stringifyable =
+    | string
+    | number
+    | boolean
+    | null
+    | undefined
+    | { [x: string]: Stringifyable }
+    | Array<Stringifyable>;
