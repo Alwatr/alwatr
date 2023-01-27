7077800cbb17681acfd85ad6786b7b0e8532cab9
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-01-27 18:14:33 +0330

    feat(dcd-notifier): day countdown action

---
+++ labs/telegram-day-countdown/src/command/daycountdown.ts
@@ -0,0 +1,33 @@
+import {serviceRequest} from '@alwatr/fetch';
+
+import {config, logger} from '../config.js';
+import {bot} from '../lib/bot.js';
+
+
+bot.action('daycountdown', async (ctx): Promise<void> => {
+  const chatId = ctx.chat?.id;
+  logger.logMethodArgs('action/daycountdown', {chatId});
+  if (chatId == null) return;
+
+  try {
+    const response = await serviceRequest<{message: string}>({
+      url: config.apiService.domain + '/subscribe/',
+      method: 'PATCH',
+      bodyJson: {
+        chatId,
+      },
+      token: config.apiService.accessToken,
+    });
+
+    if (response.data.message != null) {
+      ctx.reply(response.data.message);
+    }
+    else {
+      throw new Error('Unhandled error', {cause: {response}});
+    }
+  }
+  catch (err) {
+    ctx.reply('Oh! error occurred');
+    logger.error('action/daycountdown', (err as Error).message, {err});
+  }
+});
--- labs/telegram-day-countdown/src/index.ts
+++ labs/telegram-day-countdown/src/index.ts
@@ -2,6 +2,7 @@ import {logger} from './config.js';
 import {launchBot} from './lib/launch.js';
 
 import './command/start.js';
+import './command/daycountdown.js';
 import './route/home.js';
 import './route/notify.js';
 
