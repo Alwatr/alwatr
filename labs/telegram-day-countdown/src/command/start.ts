5ca33794c73a9df0a607115f2ce1edd3b04ba182
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-01-27 18:14:54 +0330

    feat(dcd-notifier): start command

---
+++ labs/telegram-day-countdown/src/command/start.ts
@@ -0,0 +1,13 @@
+import {logger} from '../config.js';
+import {bot} from '../lib/bot.js';
+
+bot.command('start', async (ctx): Promise<void> => {
+  const chatId = ctx.chat.id;
+  logger.logMethodArgs('command/start', {chatId});
+
+  ctx.reply(`Hello 🤚\nWellcome to Alwatr Day countdown Bot!`, {
+    reply_markup: {
+      inline_keyboard: [[{text: 'Start Day Countdown', callback_data: 'daycountdown'}]],
+    },
+  });
+});
