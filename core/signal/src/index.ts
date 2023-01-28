8704eac6ae7a8a45e77c24ef5e602e36af3fd21e
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-28 23:45:10 +0330

    fix(signal): export all interfaces

--- core/signal/src/index.ts
+++ core/signal/src/index.ts
@@ -1,5 +1,11 @@
-export {eventListener} from './event-listener.js';
-export {eventTrigger} from './event-trigger.js';
+export * from './command-handler.js';
+export * from './command-trigger.js';
+export * from './context-consumer.js';
+export * from './context-provider.js';
+export * from './event-listener.js';
+export * from './event-trigger.js';
+export * from './requestable-context-consumer.js';
+export * from './requestable-context-provider.js';
 
 /*
 TODO:
@@ -7,8 +13,5 @@ TODO:
   2. Get signal value without undefined
     (Get signal value from last dispatched signal (if any) or wait for new signal received.)
   3. dispatched bool
-  4. command callback signal
-  5. remove all SignalObject
-  6. use Stringifyable
-  7. optional commandProvider debounce
+  4. optional commandProvider debounce
 */
