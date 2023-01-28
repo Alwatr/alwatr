9b905979a5065640981433ec7d120ba2494966bb
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-28 14:43:49 +0330

    feat(signal): event trigger interface

---
+++ core/signal/src/event-trigger.ts
@@ -0,0 +1,82 @@
+import {Stringifyable, OmitFirstParam} from '@alwatr/type';
+
+import {signalManager} from './signal-manager.js';
+
+/**
+ * Event signal trigger/dispatcher interface.
+ */
+export const eventTrigger = {
+  /**
+   * Get last event dispatched detail.
+   *
+   * Return undefined if signal not dispatched before or expired.
+   *
+   * Example:
+   *
+   * ```ts
+   * const currentSize = eventTrigger.getLastDetail<ResizeType>('window-resize');
+   * if (currentSize === undefined) {
+   *   // event-signal not dispatched yet
+   * }
+   * ```
+   */
+  getLastDetail: signalManager.getDetail,
+
+  /**
+   * Dispatch (send) signal to all listeners.
+   *
+   * Signal detail changed immediately without any debounce.
+   *
+   * Example:
+   *
+   * ```ts
+   * eventTrigger.dispatch<ResizeType>('window-resize', newSize);
+   * ```
+   */
+  dispatch: signalManager.dispatch,
+
+  /**
+   * Bind this interface to special event.
+   *
+   * Example:
+   *
+   * ```ts
+   * const resizeEvent = eventTrigger.bind<ResizeType>('window-resize');
+   * ```
+   */
+  bind: <T extends Stringifyable>(eventId: string) =>({
+    /**
+     * Event signal Id.
+     */
+    id: eventId,
+
+    /**
+     * Get last event dispatched detail.
+     *
+     * Return undefined if signal not dispatched before or expired.
+     *
+     * Example:
+     *
+     * ```ts
+     * const currentSize = resizeEvent.getLastDetail();
+     * if (currentSize === undefined) {
+     *   // signal not dispatched yet
+     * }
+     * ```
+     */
+    getLastDetail: signalManager.getDetail.bind(null, eventId) as OmitFirstParam<typeof signalManager.getDetail<T>>,
+
+    /**
+     * Dispatch (send) signal to all listeners.
+     *
+     * Signal detail changed immediately without any debounce.
+     *
+     * Example:
+     *
+     * ```ts
+     * eventTrigger.dispatch<ResizeType>(newSize);
+     * ```
+     */
+    dispatch: signalManager.dispatch.bind(null, eventId) as OmitFirstParam<typeof signalManager.dispatch<T>>,
+  }),
+} as const;
