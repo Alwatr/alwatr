e5d29ebaa75d71bb5a87bedddb2be7600e68b210
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-28 16:51:47 +0330

    feat(signal): new contextProvider interface

---
+++ core/signal/src/context-provider.ts
@@ -0,0 +1,82 @@
+import {Stringifyable, OmitFirstParam} from '@alwatr/type';
+
+import {signalManager} from './signal-manager.js';
+
+/**
+ * Context provider interface.
+ */
+export const contextProvider = {
+  /**
+   * Get context value.
+   *
+   * Return undefined if context not set before or expired.
+   *
+   * Example:
+   *
+   * ```ts
+   * const currentProductList = contextProvider.getValue<ProductListType>('product-list');
+   * if (currentProductList === undefined) {
+   *   // productList not set before or expired.
+   * }
+   * ```
+   */
+  getValue: signalManager.getDetail,
+
+  /**
+   * Set context value and send signal to all consumers.
+   *
+   * Signal detail changed immediately without any debounce.
+   *
+   * Example:
+   *
+   * ```ts
+   * contextProvider.setValue<ProductListType>('product-list', newProductList);
+   * ```
+   */
+  setValue: signalManager.dispatch,
+
+  /**
+   * Bind this interface to special context.
+   *
+   * Example:
+   *
+   * ```ts
+   * const productListProvider = contextProvider.bind<ProductListType>('product-list');
+   * ```
+   */
+  bind: <T extends Stringifyable>(eventId: string) =>({
+    /**
+     * Event signal Id.
+     */
+    id: eventId,
+
+    /**
+     * Get context value.
+     *
+     * Return undefined if context not set before or expired.
+     *
+     * Example:
+     *
+     * ```ts
+     * const currentProductList = productListProvider.getValue();
+     * if (currentProductList === undefined) {
+     *   // productList not set before or expired.
+     * }
+     * ```
+     */
+    getValue: signalManager.getDetail.bind(null, eventId) as OmitFirstParam<typeof signalManager.getDetail<T>>,
+
+    /**
+     * Set context value and send signal to all consumers.
+     *
+     * Signal detail changed immediately without any debounce.
+     *
+     * Example:
+     *
+     * ```ts
+     * contextProvider.setValue(newProductList);
+     * ```
+     */
+    setValue: signalManager.dispatch.bind(null, eventId) as OmitFirstParam<typeof signalManager.dispatch<T>>,
+  }),
+} as const;
