020a0837de496d8c4943fe5cdb659fee3dc0e051
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-29 03:40:07 +0330

    fix(element): compatible with new signal api

--- ui/element/src/mixins/signal.ts
+++ ui/element/src/mixins/signal.ts
@@ -1,22 +1,24 @@
+import {unsubscribe} from '@alwatr/signal/core2.js';
+
+import type {ListenerSpec} from '@alwatr/signal/type.js';
 import type {Constructor} from '@alwatr/type';
 import type {LitElement} from 'lit';
 
 export declare class SignalMixinInterface extends LitElement {
-  protected _signalListenerList: Array<unknown>;
+  protected _signalListenerList: Array<ListenerSpec>;
 }
 
 export function SignalMixin<T extends Constructor<LitElement>>(superClass: T): Constructor<SignalMixinInterface> & T {
   class SignalMixinClass extends superClass {
-    protected _signalListenerList: Array<Record<string, unknown>> = [];
+    protected _signalListenerList: Array<ListenerSpec> = [];
 
     override disconnectedCallback(): void {
-      super.disconnectedCallback();
-
       for (const listener of this._signalListenerList) {
-        if (typeof listener.remove === 'function') {
-          listener.remove();
-        }
+        unsubscribe(listener);
       }
+      this._signalListenerList.length = 0;
+      this._signalListenerList = [];
+      super.disconnectedCallback();
     }
   }
 
