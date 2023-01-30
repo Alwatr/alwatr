ced1de64f7d7458d14578096484f492be35665fc
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-01-30 19:24:59 +0330

    feat(util): arrow fn and untilEvent types

--- core/util/src/delay.ts
+++ core/util/src/delay.ts
@@ -1,15 +1,18 @@
-export function untilEvent(element: Element, eventName: string): Promise<Event> {
-  return new Promise((resolve) => element.addEventListener(eventName, resolve));
-}
-
-export function delay(duration: number): Promise<void> {
+export const delay = (duration: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, duration));
-}
+};
 
-export function untilNextFrame(): Promise<DOMHighResTimeStamp> {
+export const untilNextFrame = (): Promise<DOMHighResTimeStamp> => {
   return new Promise((resolve) => requestAnimationFrame(resolve));
-}
+};
 
-export function untilIdle(timeout?: number): Promise<IdleDeadline> {
+export const untilIdle = (timeout?: number): Promise<IdleDeadline> => {
   return new Promise((resolve) => requestIdleCallback(resolve, {timeout}));
-}
+};
+
+export const untilEvent = <T extends keyof HTMLElementEventMap>(
+  element: HTMLElement,
+  eventName: T,
+): Promise<HTMLElementEventMap[T]> => {
+  return new Promise((resolve) => element.addEventListener(eventName, resolve, {once: true, passive: true}));
+};
