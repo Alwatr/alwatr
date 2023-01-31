807576e7643ff2dfd83a6ee3878548b5a74ac1c1
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-01-31 11:49:45 +0330

    feat(dcd-bot): start command

---
+++ labs/telegram-day-countdown/src/command/start.ts
@@ -0,0 +1,19 @@
+import {Context} from 'telegraf';
+
+import {logger} from '../config.js';
+import {bot} from '../lib/bot.js';
+import {storageEngine} from '../lib/storage.js';
+
+bot.command('start', commandStart);
+
+function commandStart(ctx: Context): void {
+  const chatId = ctx.chat?.id.toString();
+  if (chatId == null) return;
+  logger.logMethodArgs('command/start', {chatId});
+
+  ctx.reply('Salam');
+
+  if (!storageEngine.has(chatId)) {
+    storageEngine.set({id: chatId});
+  }
+}
