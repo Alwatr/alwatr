2569728683e49e020ce77a573aa79f2453bfe774
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-03 19:14:51 +0330

    feat(pwa-helper): user toast on sw signals

---
+++ ui/pwa-helper/src/signal/sw-user-notify.ts
@@ -0,0 +1,36 @@
+import {replaceNumber, localeContextConsumer} from '@alwatr/i18n';
+import {eventListener} from '@alwatr/signal';
+import {snackbarSignalTrigger} from '@alwatr/ui-kit/snackbar/show-snackbar.js';
+import {delay} from '@alwatr/util';
+
+// FIXME: i18n
+
+eventListener.subscribe('service_worker_registered', async () => {
+  if (localStorage.getItem('notify_new_version') !== null) {
+    if (localeContextConsumer.getValue() == null) {
+      await localeContextConsumer.untilChange();
+    }
+    localStorage.removeItem('notify_new_version');
+    snackbarSignalTrigger.request({
+      message: `به نسخه ${replaceNumber(_ALWATR_VERSION_)} خوش‌آمدید.`,
+    });
+  }
+});
+
+eventListener.subscribe('service_worker_installed', () => void snackbarSignalTrigger.request({
+  message: 'برنامه نصب شد و اکنون به صورت آفلاین در دسترس است.',
+}));
+
+eventListener.subscribe('service_worker_updated', async () => {
+  localStorage.setItem('notify_new_version', '');
+  const response = await snackbarSignalTrigger.requestWithResponse({
+    message: 'نسخه جدید این برنامه نصب و هم‌اکنون در دسترس است.',
+    actionLabel: 'به‌روزرسانی',
+    duration: -1,
+  });
+  if (response.actionButton) {
+    await delay(500);
+    window.location.reload();
+  }
+});
+
