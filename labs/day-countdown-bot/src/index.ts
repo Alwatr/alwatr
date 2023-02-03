f7296f7aa091b1c88c12ef82f62fd94507f619a7
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-04 01:09:13 +0330

    feat(dcd-bot): notify duration

--- labs/day-countdown-bot/src/index.ts
+++ labs/day-countdown-bot/src/index.ts
@@ -1,5 +1,6 @@
 import {logger} from './config.js';
 import {notify} from './dayCountdown.js';
+import {nextDayCountdownDuration} from './lib/calender.js';
 import {launchBot} from './lib/launch.js';
 
 import './command/start.js';
@@ -7,4 +8,10 @@ import './command/start.js';
 logger.logOther('..:: Day Countdown Telegram Bot ::..');
 
 launchBot();
-setInterval(notify, 5_000);
+
+// run on 9:00am on every day!
+const duration = nextDayCountdownDuration(9);
+setTimeout(async () => {
+  await notify();
+  setInterval(notify, 86400000);
+}, duration);
--- labs/day-countdown-bot/src/lib/calender.ts
+++ labs/day-countdown-bot/src/lib/calender.ts
@@ -5,3 +5,15 @@ export const nime = moment('1401/12/17', 'jYYYY/jM/jD');
 export function dateDistance(dateMilliseconds: number): number {
   return Math.ceil((dateMilliseconds - Date.now()) / 60 / 60 / 24 / 1000);
 }
+
+export function nextDayCountdownDuration(hour: number): number {
+  const now = new Date();
+  const sendTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, 0, 0, 0);
+
+  let duration = sendTime.valueOf() - now.valueOf();
+  if (duration < 0) {
+    duration += 86400000; // it's after hour, try hour tomorrow.
+  }
+
+  return duration;
+}
