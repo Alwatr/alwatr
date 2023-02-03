d61fb9b8a48e7ab2385fadc09bd7d3ff6e9723fd
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-03 12:01:42 +0330

    feat(dcd-bot): extra options on sendMessage

--- labs/day-countdown-bot/src/lib/send-message.ts
+++ labs/day-countdown-bot/src/lib/send-message.ts
@@ -1,10 +1,14 @@
 import {bot} from './bot.js';
 import {logger} from '../config.js';
 
-import type {Message} from 'telegraf/types';
+import type {Message, Convenience} from 'telegraf/types';
 
-export async function sendMessage(chatId: string, message: string): Promise<Message> {
+export function sendMessage(
+    chatId: string,
+    message: string,
+    options?: Convenience.ExtraReplyMessage,
+): Promise<Message> {
   logger.logMethodArgs('sendMessage', {chatId, message});
 
-  return bot.telegram.sendMessage(chatId, message);
+  return bot.telegram.sendMessage(chatId, message, options);
 }
