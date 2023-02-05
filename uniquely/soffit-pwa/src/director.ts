a1554487e867362bcb097b51bb3345c08c07bfa1
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-05 18:26:09 +0330

    fix(soffit): import json issue

--- uniquely/soffit-pwa/src/director.ts
+++ uniquely/soffit-pwa/src/director.ts
@@ -2,11 +2,13 @@ import '@alwatr/pwa-helper/director.js';
 
 import {setL18eLoader} from '@alwatr/i18n';
 
+import type {L18eContext} from '@alwatr/type';
+
 setL18eLoader((locale) => {
   if (locale.language === 'en') {
-    return import('./l18r/en.json', {assert: {type: 'json'}});
+    return import('./l18r/en.json', {assert: {type: 'json'}}) as Promise<L18eContext>;
   }
   else {
-    return import('./l18r/fa.json', {assert: {type: 'json'}});
+    return import('./l18r/fa.json', {assert: {type: 'json'}}) as Promise<L18eContext>;
   }
 });
