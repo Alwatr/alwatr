2e98a93c1f8fc84a9301123c41cf030887cb8eda
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-05 18:16:13 +0330

    feat(soffit-pwa): json i18n resource
    Co-authored-by: S. Ali Mihandoost <ali@mihandoost.com>

--- uniquely/soffit-pwa/src/director.ts
+++ uniquely/soffit-pwa/src/director.ts
@@ -2,11 +2,11 @@ import '@alwatr/pwa-helper/director.js';
 
 import {setL18eLoader} from '@alwatr/i18n';
 
-setL18eLoader(async (locale) => {
+setL18eLoader((locale) => {
   if (locale.language === 'en') {
-    return (await import('./l18r/en.js')).l18r;
+    return import('./l18r/en.json', {assert: {type: 'json'}});
   }
   else {
-    return (await import('./l18r/fa.js')).l18r;
+    return import('./l18r/fa.json', {assert: {type: 'json'}});
   }
 });
