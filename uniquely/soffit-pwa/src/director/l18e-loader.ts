5a1f49f9ab7ff4c761681097d6019b8bfd3e23d0
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-06 16:54:50 +0330

    fix(soffit-pwa): director logging

--- uniquely/soffit-pwa/src/director/l18e-loader.ts
+++ uniquely/soffit-pwa/src/director/l18e-loader.ts
@@ -1,9 +1,13 @@
 import {setL18eLoader} from '@alwatr/i18n';
 
+import {logger} from './logger';
+
 import type {L18eContext} from '@alwatr/type';
 
 setL18eLoader((locale) => {
-  return locale.language === 'en'
+  const language = locale.language;
+  logger.logMethodArgs('l18eLoader', {language});
+  return language === 'en'
     ? import('../l18r/en.json', {assert: {type: 'json'}}) as Promise<L18eContext>
     : import('../l18r/fa.json', {assert: {type: 'json'}}) as Promise<L18eContext>;
 });
