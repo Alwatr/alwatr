f645d4371d25ff75a41ce1f15f0352b7141d7e40
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-06 13:42:06 +0330

    fix(snackbar): position

--- ui/ui-kit/src/snackbar/element.ts
+++ ui/ui-kit/src/snackbar/element.ts
@@ -32,9 +32,9 @@ export class AlwatrSnackbar extends AlwatrSurface {
         padding: calc(0.5 * var(--sys-spacing-track));
         gap: var(--sys-spacing-track);
         position: fixed;
-        bottom: calc(4 * var(--sys-spacing-track));
-        left: calc(4 * var(--sys-spacing-track));
-        right: calc(4 * var(--sys-spacing-track));
+        bottom: calc(2 * var(--sys-spacing-track));
+        left: var(--sys-spacing-track);
+        right: var(--sys-spacing-track);
         z-index: var(--sys-zindex-snackbar);
         border-radius: var(--sys-radius-xsmall);
 
