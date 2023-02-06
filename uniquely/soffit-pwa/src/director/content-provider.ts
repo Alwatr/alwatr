580e7d261e2e9460597db03ff92e366adb5ce787
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-06 16:26:13 +0330

    refactor(soffit): director

---
+++ uniquely/soffit-pwa/src/director/content-provider.ts
@@ -0,0 +1,20 @@
+import {localeContextConsumer} from '@alwatr/i18n';
+import {contextProvider} from '@alwatr/signal';
+
+import {logger} from './logger';
+
+import type {PageHomeContent} from '../type';
+
+localeContextConsumer.subscribe(async () => {
+  const language = localeContextConsumer.getValue()?.language;
+  logger.logMethodArgs('contentProvider', {language});
+
+  if (language == null) return;
+
+  const content =
+    language === 'en'
+      ? (await import('../content/en.js')).homePageContent
+      : (await import('../content/fa.js')).homePageContent;
+
+  contextProvider.setValue<PageHomeContent>('home_page_content', content);
+});
--- uniquely/soffit-pwa/src/director/index.ts
+++ uniquely/soffit-pwa/src/director/index.ts
@@ -1,5 +1,5 @@
 import '@alwatr/pwa-helper/director.js';
 
-import './l18e.js';
-import './content-loader.js';
+import './l18e-loader.js';
+import './content-provider.js';
 import './form-submit.js';
