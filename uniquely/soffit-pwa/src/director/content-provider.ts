084883da9b18890f1ec227ad2a78e4b13c00500d
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-08 13:25:34 +0330

    feat(soffit-pwa): product page content provider

--- uniquely/soffit-pwa/src/director/content-provider.ts
+++ uniquely/soffit-pwa/src/director/content-provider.ts
@@ -3,7 +3,7 @@ import {contextProvider} from '@alwatr/signal';
 
 import {logger} from './logger.js';
 
-import type {PageHomeContent} from '../type.js';
+import type {PageHomeContent, ProductPageContent} from '../type.js';
 
 localeContextConsumer.subscribe(async () => {
   const language = localeContextConsumer.getValue()?.language;
@@ -11,10 +11,15 @@ localeContextConsumer.subscribe(async () => {
 
   if (language == null) return;
 
-  const content =
+  const homePageContent =
     language === 'en'
       ? (await import('../content/en.js')).homePageContent
       : (await import('../content/fa.js')).homePageContent;
+  const productPageContent =
+    language === 'en'
+      ? (await import('../content/en.js')).productPageContent
+      : (await import('../content/fa.js')).productPageContent;
 
-  contextProvider.setValue<PageHomeContent>('home_page_content', content);
+  contextProvider.setValue<PageHomeContent>('home_page_content', homePageContent);
+  contextProvider.setValue<ProductPageContent>('product_page_content', productPageContent);
 });
