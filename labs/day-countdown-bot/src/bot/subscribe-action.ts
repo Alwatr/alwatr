4dd49a8abd95c1129d5b971b2c7330ed630dadfe
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-10 00:42:20 +0330

    feat(dcd-bot): send day countdown after subscribe

--- labs/day-countdown-bot/src/bot/subscribe-action.ts
+++ labs/day-countdown-bot/src/bot/subscribe-action.ts
@@ -1,4 +1,5 @@
 import {logger} from '../config.js';
+import {sendDayCountDown} from '../dayCountdown.js';
 import {message} from '../director/l18e-loader.js';
 import {bot} from '../lib/bot.js';
 import {sendMessage} from '../lib/send-message.js';
@@ -12,6 +13,7 @@ bot.action('subscribe', (ctx) => {
   if (!storageEngine.has(chatId)) {
     storageEngine.set({id: chatId});
     sendMessage(chatId, message('action_subscribe_success'), {parse_mode: 'MarkdownV2'});
+    sendDayCountDown(chatId);
   }
   else {
     sendMessage(chatId, message('action_subscribe_added_before'), {parse_mode: 'MarkdownV2'});
