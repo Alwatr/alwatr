a0ffba8467a8bf9452e6c971445950dee7f8ac8c
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-10 21:56:38 +0330

    feat(dcd-bot): refactor notify with alwatr telegraf

--- labs/day-countdown-bot/src/bot/notify-command.ts
+++ labs/day-countdown-bot/src/bot/notify-command.ts
@@ -1,55 +1,29 @@
-import {isAdmin} from '../admin.js';
+import {deleteAdmin, isAdmin} from '../admin.js';
 import {logger} from '../config.js';
 import {message} from '../director/l18e-loader.js';
 import {bot} from '../lib/bot.js';
-import {sendMessage} from '../lib/send-message.js';
 import {chatStorageEngine} from '../lib/storage.js';
-import {deleteUser} from '../user.js';
-
-import type {User} from '../type.js';
-import type {TelegramError} from 'telegraf';
+import {setLastNotifyMessageId} from '../user.js';
 
 bot.command('notify', async (ctx) => {
-  const chatId = ctx.chat?.id.toString();
-  if (chatId == null) return;
-  logger.logMethodArgs('command/notify', {chatId});
+  if (ctx.chatId == null) return;
+  logger.logMethodArgs('command/notify', {chatId: ctx.chatId});
 
-  if (!isAdmin(chatId)) return;
+  ctx.isAdminCallback = isAdmin;
+  ctx.onSendMessageForbidden = deleteAdmin;
+  if (!ctx.isAdmin) return;
 
-  const messageText = ctx.message.text.replace('/notify', '').trim();
-  if (messageText === '') {
-    try {
-      await sendMessage(chatId, message('command_notify_empty_message'));
-    }
-    catch (err) {
-      logger.error('command/notify', 'send_message_failed', {err});
-    }
+  const messageText = ctx.commandArgs;
+  if (messageText == null) {
+    await ctx.replyToUser(message('command_notify_empty_message'));
     return;
   }
 
   for (const chat of chatStorageEngine.allObject()) {
-    let message;
-    try {
-      message = await sendMessage(chat.id, messageText);
-    }
-    catch (err) {
-      const _err = err as TelegramError;
-      if (_err.code === 403) {
-        deleteUser(chatId);
-      }
-      logger.error('command/notify', 'send_message_failed', {err});
-      continue;
-    }
-
-    const user = chatStorageEngine.get(chat.id) as User;
-    user.lastNotifyMessageId = message.message_id;
-    chatStorageEngine.set(user);
+    const response = await ctx.sendMessageToUser(messageText);
+    if (response == null) return;
+    setLastNotifyMessageId(chat.id, response.message_id);
   }
 
-  try {
-    await sendMessage(chatId, message('command_notify_success'));
-  }
-  catch (err) {
-    logger.error('command/notify', 'send_message_failed', {err});
-  }
+  ctx.replyToUser(message('command_notify_success'));
 });
