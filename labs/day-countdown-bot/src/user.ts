df3272116b134aad345dd8a40ca59784a5d9d88f
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-10 21:55:31 +0330

    feat(dcd-bot): setLastNotifyMessageId, isSubscribed user util

--- labs/day-countdown-bot/src/user.ts
+++ labs/day-countdown-bot/src/user.ts
@@ -1,10 +1,24 @@
 import {logger} from './config.js';
 import {chatStorageEngine} from './lib/storage.js';
 
+import type {User} from './type.js';
+
 /**
  * Make disabled property in User true!
  */
 export function deleteUser(chatId: string): void {
   logger.logMethodArgs('deleteUser', {chatId});
   chatStorageEngine.delete(chatId);
+}
+
+export function isSubscribed(chatId: string): boolean {
+  logger.logMethodArgs('isSubscribed', {chatId});
+  return chatStorageEngine.has(chatId);
+}
+
+export function setLastNotifyMessageId(chatId: string, messageId: number): void {
+  logger.logMethodArgs('setLastNotifyMessageId', {chatId, messageId});
+  const user = chatStorageEngine.get(chatId) as User;
+  user.lastNotifyMessageId = messageId;
+  chatStorageEngine.set(user);
 }
