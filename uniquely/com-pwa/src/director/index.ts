852367438c746b726b79cfc744d4cb83555cc5e7
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-11 20:30:27 +0330

    fix(com-pwa): director imports

--- uniquely/com-pwa/src/director/index.ts
+++ uniquely/com-pwa/src/director/index.ts
@@ -3,6 +3,6 @@ import '@alwatr/pwa-helper/director.js';
 import './l18e-loader.js';
 import './content-provider.js';
 import './product-storage-context-provider.js';
-import './/user-context-provider.js';
+import './user-context-provider.js';
 import './order-storage-context-provider.js';
 import './submit-order-handler.js';
