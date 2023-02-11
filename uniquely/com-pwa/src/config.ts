fc6091293fbb74129a31a70c88acd7bf21347c51
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-11 13:22:40 +0330

    feat(com-pwa): add cdn to config

--- uniquely/com-pwa/src/config.ts
+++ uniquely/com-pwa/src/config.ts
@@ -2,5 +2,6 @@ import {getConfKey} from '@alwatr/pwa-helper/config.js';
 
 export const config = {
   api: getConfKey<string>('api'),
+  cdn: getConfKey<string>('cdn'),
   token: getConfKey<string>('token'),
 };
