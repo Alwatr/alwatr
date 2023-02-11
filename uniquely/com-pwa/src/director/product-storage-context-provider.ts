a5dd2aefeff3cb42685f55a7374807d28432ee8b
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-12 02:34:15 +0330

    feat(com-pwa): enhance director context provider

--- uniquely/com-pwa/src/director/product-storage-context-provider.ts
+++ uniquely/com-pwa/src/director/product-storage-context-provider.ts
@@ -1,5 +1,5 @@
 import {fetchContext} from '@alwatr/fetch';
-import {message} from '@alwatr/i18n';
+import {l18eReadyPromise, message} from '@alwatr/i18n';
 import {snackbarSignalTrigger} from '@alwatr/ui-kit/src/snackbar/show-snackbar.js';
 
 import {logger} from './logger.js';
@@ -13,13 +13,16 @@ const provideProductStorageContext = async (): Promise<void> => {
       method: 'GET',
       url: config.api + '/product/',
       token: config.token,
+      removeDuplicate: 'auto',
+      retry: 10,
+      retryDelay: 3_000,
     });
   }
   catch (err) {
     logger.error('provideProductStorageContext', 'fetch_failed', err);
-
+    await l18eReadyPromise;
     const response = await snackbarSignalTrigger.requestWithResponse({
-      message: message('fetch_failed'),
+      message: message('fetch_failed') + '1',
       actionLabel: message('retry'),
       duration: -1,
     });
