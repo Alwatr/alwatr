338099af32d63420dbb93dbd8010f0e83af5b610
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-11 11:45:30 +0330

    feat(cop-api): requireToken

--- uniquely/com-api/src/route/get-order.ts
+++ uniquely/com-api/src/route/get-order.ts
@@ -1,7 +1,6 @@
-import {logger} from '../lib/config.js';
+import {config, logger} from '../lib/config.js';
 import {nanoServer} from '../lib/server.js';
 import {storageClient} from '../lib/storage.js';
-import {tokenGenerator} from '../lib/token.js';
 
 import type {Order} from '@alwatr/type/customer-order-management.js';
 
@@ -10,9 +9,10 @@ nanoServer.route('GET', '/order/', async (connection) => {
   logger.logMethod('get-order');
 
   const params = connection.requireQueryParams<{userId: string}>({userId: 'string'});
-  connection.requireToken((token: string) => {
-    return tokenGenerator.verify(params.userId, token) === 'valid';
-  });
+  // connection.requireToken((token: string) => {
+  //   return tokenGenerator.verify(params.userId, token) === 'valid';
+  // });
+  connection.requireToken(config.nanoServer.accessToken);
 
   return await storageClient.getStorage<Order>(params.userId);
 });
--- uniquely/com-api/src/route/get-product.ts
+++ uniquely/com-api/src/route/get-product.ts
@@ -4,7 +4,8 @@ import {storageClient} from '../lib/storage.js';
 
 import type {Product} from '@alwatr/type/src/customer-order-management.js';
 
-nanoServer.route('GET', '/product/', async () => {
+nanoServer.route('GET', '/product/', async (connection) => {
   logger.logMethod('get-product');
+  connection.requireToken(config.nanoServer.accessToken);
   return await storageClient.getStorage<Product>(config.storage.productStorageName);
 });
