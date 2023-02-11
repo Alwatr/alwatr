ffe16889c6760f0b387d70eb30fa527bd26d95a7
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-11 20:26:19 +0330

    feat(com-pwa): enhance order-storage-context-provider

--- uniquely/com-pwa/src/director/order-storage-context-provider.ts
+++ uniquely/com-pwa/src/director/order-storage-context-provider.ts
@@ -1,31 +1,28 @@
 import {fetchContext} from '@alwatr/fetch';
 import {message} from '@alwatr/i18n';
-import {contextConsumer} from '@alwatr/signal';
 import {snackbarSignalTrigger} from '@alwatr/ui-kit/src/snackbar/show-snackbar.js';
 
 import {logger} from './logger.js';
 import {config} from '../config.js';
+import {orderStorageContextProvider, orderStorageContextConsumer, userContextConsumer} from '../context.js';
 
-import type {User} from '@alwatr/type';
+orderStorageContextProvider.setProvider(async (args) => {
+  logger.logMethod('orderStorageContextProvider');
 
-const provideOrderStorageContext = async (): Promise<void> => {
-  logger.logMethod('provideOrderStorageContext');
-
-  const userId = contextConsumer.getValue<User>('user-context');
-  if (userId == null) throw new Error('user_id_required');
+  const userContext = userContextConsumer.getValue() ?? await userContextConsumer.untilChange();
 
   try {
     await fetchContext('order-storage-context', {
       method: 'GET',
       url: config.api + '/order/',
       queryParameters: {
-        userId: userId.id,
+        userId: userContext.id,
       },
       token: config.token,
     });
   }
   catch (err) {
-    logger.error('provideOrderStorageContext', 'fetch_failed', err);
+    logger.error('orderStorageContextProvider', 'fetch_failed', err);
 
     const response = await snackbarSignalTrigger.requestWithResponse({
       message: message('fetch_failed'),
@@ -33,13 +30,9 @@ const provideOrderStorageContext = async (): Promise<void> => {
       duration: -1,
     });
     if (response.actionButton) {
-      provideOrderStorageContext();
+      orderStorageContextConsumer.request(args);
     }
   }
-};
-
-provideOrderStorageContext();
-
-contextConsumer.subscribe('order-storage-context', (value) => {
-  logger.logProperty('order-storage-context', value);
 });
+
+orderStorageContextConsumer.request(null);
