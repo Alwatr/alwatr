67956546cbda722380c775b2da182b58291ddc9d
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-11 22:26:25 +0330

    feat(com-pwa): order list page content provider

--- uniquely/com-pwa/src/context.ts
+++ uniquely/com-pwa/src/context.ts
@@ -6,6 +6,8 @@ import {
   commandTrigger,
 } from '@alwatr/signal';
 
+import {PageHomeContent, PageOrderListContent} from './type.js';
+
 import type {AlwatrDocumentStorage, User} from '@alwatr/type';
 import type {Product, Order} from '@alwatr/type/customer-order-management.js';
 
@@ -14,12 +16,21 @@ export const productStorageContextConsumer =
 
 export const orderStorageContextProvider =
     requestableContextProvider.bind<AlwatrDocumentStorage<Order>, null>('order-storage-context');
-
 export const orderStorageContextConsumer =
   requestableContextConsumer.bind<AlwatrDocumentStorage<Order>, null>(orderStorageContextProvider.id);
 
 
 export const userContextProvider = contextProvider.bind<User>('user-context');
 export const userContextConsumer = contextConsumer.bind<User>(userContextProvider.id);
+
+export const homePageContentContextProvider =
+  contextProvider.bind<PageHomeContent>('home-page-content-context');
+export const homePageContentContextConsumer =
+  contextConsumer.bind<PageHomeContent>('home-page-content-context');
+
+export const orderListPageContentContextProvider =
+  contextProvider.bind<PageOrderListContent>('home-order-list-content-context');
+export const orderListPageContentContextConsumer =
+  contextConsumer.bind<PageOrderListContent>('home-order-list-content-context');
 
 export const submitOrderCommandTrigger = commandTrigger.bind<Partial<Order>, Order>('submit-order-command');
