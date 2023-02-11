2cd76a76c0f69fd827e7fbced3b223cc2230271c
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-11 21:36:47 +0330

    refactor(com-pwa): submit-order-command-handler name

--- uniquely/com-pwa/src/director/index.ts
+++ uniquely/com-pwa/src/director/index.ts
@@ -5,4 +5,4 @@ import './content-provider.js';
 import './product-storage-context-provider.js';
 import './user-context-provider.js';
 import './order-storage-context-provider.js';
-import './submit-order-handler.js';
+import './submit-order-command-handler.js';
