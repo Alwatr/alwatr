69cab1147690e2084dc24aa63f119ab4da5230a9
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-14 14:59:26 +0330

    feat(com-api): get price list route

--- uniquely/com-api/src/index.ts
+++ uniquely/com-api/src/index.ts
@@ -2,7 +2,7 @@ import {logger} from './lib/config.js';
 
 import './route/home.js';
 import './route/get-order.js';
-import './route/get-product.js';
+import './route/get-price-list.js';
 import './route/put-order.js';
 import './route/patch-product.js';
 
