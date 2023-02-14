b6f4cd0eb00d95f9ca6963e45604b86b256bd833
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-14 15:01:27 +0330

    feat(com-api): multi storage support for product route

--- uniquely/com-api/src/index.ts
+++ uniquely/com-api/src/index.ts
@@ -2,8 +2,9 @@ import {logger} from './lib/config.js';
 
 import './route/home.js';
 import './route/get-order.js';
+import './route/get-product-list.js';
 import './route/get-price-list.js';
 import './route/put-order.js';
-import './route/patch-product.js';
+import './route/patch-product-list.js';
 
 logger.logOther('..:: Alwatr Customer Order Management API ::..');
