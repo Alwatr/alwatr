a0d2f5e4fdb937a149fc96bd4499fa158496d9f4
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-15 01:04:44 +0330

    fix(com-api): imports path

--- uniquely/com-api/src/route/patch-product-list.ts
+++ uniquely/com-api/src/route/patch-product-list.ts
@@ -2,7 +2,7 @@ import {config, logger} from '../lib/config.js';
 import {nanoServer} from '../lib/server.js';
 import {storageClient} from '../lib/storage.js';
 
-import type {Product} from '@alwatr/type/src/customer-order-management.js';
+import type {Product} from '@alwatr/type/customer-order-management.js';
 
 nanoServer.route('PATCH', '/product-list/', async (connection) => {
   logger.logMethod('patch-product-list');
