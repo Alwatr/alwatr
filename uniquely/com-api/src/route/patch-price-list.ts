7a7a03a3510b8b8c230ed765cd6f1709dddf9c0b
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-14 15:01:52 +0330

    feat(com-api): patch price list route

---
+++ uniquely/com-api/src/route/patch-price-list.ts
@@ -0,0 +1,21 @@
+import {config, logger} from '../lib/config.js';
+import {nanoServer} from '../lib/server.js';
+import {storageClient} from '../lib/storage.js';
+
+import type {ProductPrice} from '@alwatr/type/src/customer-order-management.js';
+
+nanoServer.route('PATCH', '/price-list/', async (connection) => {
+  logger.logMethod('patch-price-list');
+  connection.requireToken(config.nanoServer.adminToken);
+  const params = connection.requireQueryParams<{name: string}>({name: 'string'});
+  const bodyJson = await connection.requireJsonBody<{data: Array<ProductPrice>}>();
+
+  for (const price of bodyJson.data) {
+    await storageClient.set(price, config.storage.priceStorageName + '-' + params.name);
+  }
+
+  return {
+    ok: true,
+    data: {},
+  };
+});
