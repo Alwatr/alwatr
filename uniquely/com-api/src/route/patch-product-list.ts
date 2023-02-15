20ef97eb20840e91f12c80798bd26b227239518a
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-15 23:21:10 +0330

    fix(com-api): patch-product-list storage param

--- uniquely/com-api/src/route/patch-product-list.ts
+++ uniquely/com-api/src/route/patch-product-list.ts
@@ -7,11 +7,11 @@ import type {Product} from '@alwatr/type/customer-order-management.js';
 nanoServer.route('PATCH', '/product-list/', async (connection) => {
   logger.logMethod('patch-product-list');
   connection.requireToken(config.nanoServer.adminToken);
-  const params = connection.requireQueryParams<{name: string}>({name: 'string'});
+  const params = connection.requireQueryParams<{storage: string}>({storage: 'string'});
   const bodyJson = await connection.requireJsonBody<{data: Array<Product>}>();
 
   for (const price of bodyJson.data) {
-    await storageClient.set(price, config.productStoragePrefix + params.name);
+    await storageClient.set(price, config.productStoragePrefix + params.storage);
   }
 
   return {
