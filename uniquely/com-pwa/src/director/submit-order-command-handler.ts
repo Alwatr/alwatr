3938fbd4e5a97aeecdfbcb6dab9daae3476985d2
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-17 21:30:16 +0330

    feat(com-pwa): submit order command
    Co-authored-by: S. Amir Mohammad Najafi <njfamirm@gmail.com>

--- uniquely/com-pwa/src/director/submit-order-command-handler.ts
+++ uniquely/com-pwa/src/director/submit-order-command-handler.ts
@@ -4,18 +4,17 @@ import {redirect} from '@alwatr/router';
 import {commandHandler} from '@alwatr/signal';
 import {snackbarSignalTrigger} from '@alwatr/ui-kit/snackbar/show-snackbar.js';
 
+import {logger} from './logger.js';
 import {config} from '../config.js';
 import {userContextConsumer, orderStorageContextProvider, submitOrderCommandTrigger} from '../context.js';
 
 import type {Order} from '@alwatr/type/customer-order-management.js';
 
-commandHandler.define<Order, Order | null>(submitOrderCommandTrigger.id, async (order) => {
+commandHandler.define<Partial<Order>, Order | null>(submitOrderCommandTrigger.id, async (order) => {
   const userContext = userContextConsumer.getValue() ?? await userContextConsumer.untilChange();
 
-  // 2. send to server
-  let response;
   try {
-    response = await serviceRequest<Order>({
+    const response = await serviceRequest<Order>({
       method: 'PUT',
       url: config.api + '/order/',
       queryParameters: {
@@ -24,31 +23,30 @@ commandHandler.define<Order, Order | null>(submitOrderCommandTrigger.id, async (
       token: config.token,
       bodyJson: order,
     });
-  }
-  catch {
-    await snackbarSignalTrigger.requestWithResponse({
-      message: message('order_form_submit_failed'),
-      actionLabel: message('retry'),
-      duration: -1,
+
+    const newOrder = response.data;
+
+    const orderStorage = orderStorageContextProvider.getValue();
+    if (orderStorage != null) {
+      orderStorage.data[newOrder.id] = newOrder;
+      orderStorageContextProvider.setValue(orderStorage);
+    }
+
+    snackbarSignalTrigger.request({
+      message: message('submit_order_success'),
     });
 
-    return submitOrderCommandTrigger.requestWithResponse(order);
+    redirect('/order/' + newOrder.id);
+
+    return newOrder;
   }
+  catch (err) {
+    logger.error('submitOrderCommand', 'submit_failed', err, order);
 
-  // TODO: handle response.ok === false!
+    await snackbarSignalTrigger.requestWithResponse({
+      message: message('submit_order_failed'),
+    });
 
-  const newOrder = response.data;
-  const orderStorage = orderStorageContextProvider.getValue();
-  if (orderStorage != null) {
-    orderStorage.data[newOrder.id] = newOrder;
-    orderStorageContextProvider.setValue(orderStorage);
+    return null;
   }
-
-  snackbarSignalTrigger.request({
-    message: 'اع زدی که! یک سفارش جدید الکی ثبت شد!',
-  });
-
-  redirect('/orders');
-
-  return response.data;
 });
