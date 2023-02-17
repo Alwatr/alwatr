d630b429f5b3716b3196f142927ae418afcc0122
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-17 19:32:29 +0330

    refactor(com-pwa): submit order director

--- uniquely/com-pwa/src/director/submit-order-command-handler.ts
+++ uniquely/com-pwa/src/director/submit-order-command-handler.ts
@@ -2,42 +2,15 @@ import {serviceRequest} from '@alwatr/fetch';
 import {message} from '@alwatr/i18n';
 import {redirect} from '@alwatr/router';
 import {commandHandler} from '@alwatr/signal';
-import {snackbarSignalTrigger} from '@alwatr/ui-kit/src/snackbar/show-snackbar.js';
-import {JsonSchema, validator} from '@alwatr/validator';
+import {snackbarSignalTrigger} from '@alwatr/ui-kit/snackbar/show-snackbar.js';
 
 import {config} from '../config.js';
 import {userContextConsumer, orderStorageContextProvider, submitOrderCommandTrigger} from '../context.js';
 
+import type {Order} from '@alwatr/type/customer-order-management.js';
 
-import type {Order, OrderDelivery} from '@alwatr/type/customer-order-management.js';
-
-const validSchema: JsonSchema = {
-  recipientName: String,
-  recipientNationalCode: Number,
-  address: String,
-  carType: String,
-  shipmentType: String,
-  timePeriod: String,
-};
-
-commandHandler.define<Partial<Order>, Order | null>(submitOrderCommandTrigger.id, async (order) => {
+commandHandler.define<Order, Order | null>(submitOrderCommandTrigger.id, async (order) => {
   const userContext = userContextConsumer.getValue() ?? await userContextConsumer.untilChange();
-
-  // TODO: get product list
-
-  // 1. validate
-  let validOrder;
-  try {
-    validOrder = validator(validSchema, order.delivery as OrderDelivery) as Partial<Order>;
-  }
-  catch {
-    snackbarSignalTrigger.request({
-      message: message('order_form_invalid_detail'),
-      duration: 5,
-    });
-
-    return null;
-  }
 
   // 2. send to server
   let response;
@@ -49,9 +22,7 @@ commandHandler.define<Partial<Order>, Order | null>(submitOrderCommandTrigger.id
         userId: userContext.id,
       },
       token: config.token,
-      bodyJson: {
-        delivery: validOrder.delivery,
-      },
+      bodyJson: order,
     });
   }
   catch {
@@ -61,7 +32,7 @@ commandHandler.define<Partial<Order>, Order | null>(submitOrderCommandTrigger.id
       duration: -1,
     });
 
-    return await submitOrderCommandTrigger.requestWithResponse(order);
+    return submitOrderCommandTrigger.requestWithResponse(order);
   }
 
   // TODO: handle response.ok === false!
