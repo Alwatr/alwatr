96aed7deec8c93d7fb81aaa058e5f2847e16b525
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-26 02:14:15 +0330

    feat(com-pwa): make page-order-list state machine

--- uniquely/com-pwa/src/director/index.ts
+++
@@ -1,7 +0,0 @@
-import './content-provider.js';
-import './l18e-loader.js';
-import './order-storage-context-provider.js';
-import './product-storage-context-provider.js';
-import './shipping-order-command-handler.js';
-import './submit-order-command-handler.js';
-import './user-context-provider.js';
