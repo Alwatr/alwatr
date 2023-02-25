f251eadc18ee0070bbcd7b9bbd69c890192b60ca
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-25 13:59:28 +0330

    fix(com-pwa): page detail

--- uniquely/com-pwa/src/page-order/page-order-detail.ts
+++ uniquely/com-pwa/src/page-order/page-order-detail.ts
@@ -10,6 +10,7 @@ import {
   mapIterable,
   state,
   nothing,
+  svg,
 } from '@alwatr/element';
 import {message, number, replaceNumber} from '@alwatr/i18n';
 import {contextConsumer, ListenerSpec} from '@alwatr/signal';
@@ -24,7 +25,7 @@ import {topAppBarContextProvider} from '../context.js';
 
 import type {AlwatrDocumentStorage} from '@alwatr/type';
 import type {Order, OrderDraft, OrderItem, Product} from '@alwatr/type/customer-order-management.js';
-import type {IconBoxContent} from '@alwatr/ui-kit/src/card/icon-box.js';
+import type {IconBoxContent} from '@alwatr/ui-kit/card/icon-box.js';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -77,9 +78,11 @@ export class AlwatrPageOrderDetail extends LocalizeMixin(SignalMixin(AlwatrBaseE
     }
 
     alwatr-text-field {
-      width: 3em;
+      width: 5em;
       padding: 0 var(--sys-spacing-track);
-      margin-top: var(--sys-spacing-track);
+      border-radius: 0;
+      border: none;
+      border-bottom: 1px solid var(--sys-color-outline);
     }
 
     svg {
@@ -95,11 +98,18 @@ export class AlwatrPageOrderDetail extends LocalizeMixin(SignalMixin(AlwatrBaseE
     }
   `;
 
-  @property({attribute: false})
-    order?: Order | OrderDraft | null;
-
   @state()
   protected _productStorage?: AlwatrDocumentStorage<Product> | null;
+
+  @state()
+  protected _editMode = false;
+
+  @state()
+  // eslint-disable-next-line max-len
+  protected tomanSvg = svg`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14"><path fill-rule="evenodd" stroke="none" fill="currentcolor" d="M3.057 1.742L3.821 1l.78.75-.776.741-.768-.749zm3.23 2.48c0 .622-.16 1.111-.478 1.467-.201.221-.462.39-.783.505a3.251 3.251 0 01-1.083.163h-.555c-.421 0-.801-.074-1.139-.223a2.045 2.045 0 01-.9-.738A2.238 2.238 0 011 4.148c0-.059.001-.117.004-.176.03-.55.204-1.158.525-1.827l1.095.484c-.257.532-.397 1-.419 1.403-.002.04-.004.08-.004.12 0 .252.055.458.166.618a.887.887 0 00.5.354c.085.028.178.048.278.06.079.01.16.014.243.014h.555c.458 0 .769-.081.933-.244.14-.139.21-.383.21-.731V2.02h1.2v2.202zm5.433 3.184l-.72-.7.709-.706.735.707-.724.7zm-2.856.308c.542 0 .973.19 1.293.569.297.346.445.777.445 1.293v.364h.18v-.004h.41c.221 0 .377-.028.467-.084.093-.055.14-.14.14-.258v-.069c.004-.243.017-1.044 0-1.115L13 8.05v1.574a1.4 1.4 0 01-.287.863c-.306.405-.804.607-1.495.607h-.627c-.061.733-.434 1.257-1.117 1.573-.267.122-.58.21-.937.265a5.845 5.845 0 01-.914.067v-1.159c.612 0 1.072-.082 1.38-.247.25-.132.376-.298.376-.499h-.515c-.436 0-.807-.113-1.113-.339-.367-.273-.55-.667-.55-1.18 0-.488.122-.901.367-1.24.296-.415.728-.622 1.296-.622zm.533 2.226v-.364c0-.217-.048-.389-.143-.516a.464.464 0 00-.39-.187.478.478 0 00-.396.187.705.705 0 00-.136.449.65.65 0 00.003.067c.008.125.066.22.177.283.093.054.21.08.352.08h.533zM9.5 6.707l.72.7.724-.7L10.209 6l-.709.707zm-6.694 4.888h.03c.433-.01.745-.106.937-.29.024.012.065.035.12.068l.074.039.081.042c.135.073.261.133.379.18.345.146.67.22.977.22a1.216 1.216 0 00.87-.34c.3-.285.449-.714.449-1.286a2.19 2.19 0 00-.335-1.145c-.299-.457-.732-.685-1.3-.685-.502 0-.916.192-1.242.575-.113.132-.21.284-.294.456-.032.062-.06.125-.084.191a.504.504 0 00-.03.078 1.67 1.67 0 00-.022.06c-.103.309-.171.485-.205.53-.072.09-.214.14-.427.147-.123-.005-.209-.03-.256-.076-.057-.054-.085-.153-.085-.297V7l-1.201-.5v3.562c0 .261.048.496.143.703.071.158.168.296.29.413.123.118.266.211.43.28.198.084.42.13.665.136v.001h.036zm2.752-1.014a.778.778 0 00.044-.353.868.868 0 00-.165-.47c-.1-.134-.217-.201-.35-.201-.18 0-.33.103-.447.31-.042.071-.08.158-.114.262a2.434 2.434 0 00-.04.12l-.015.053-.015.046c.142.118.323.216.544.293.18.062.325.092.433.092.044 0 .086-.05.125-.152z"></path></svg>`;
+
+  @property({attribute: false})
+    order?: Order | OrderDraft | null;
 
   @property()
     storageName?: string = 'tile';
@@ -157,9 +167,9 @@ export class AlwatrPageOrderDetail extends LocalizeMixin(SignalMixin(AlwatrBaseE
   override render(): unknown {
     this._logger.logMethod('render');
     if (this.order == null) return message('loading');
-    const editMode = this.order?.status === 'draft';
-    if (this.hasAttribute('edit-mode') !== editMode) {
-      this.toggleAttribute('edit-mode', editMode);
+    this._editMode = this.order?.status === 'draft';
+    if (this.hasAttribute('edit-mode') !== this._editMode) {
+      this.toggleAttribute('edit-mode', this._editMode);
     }
 
     const iconBoxContent: IconBoxContent = {
@@ -167,7 +177,7 @@ export class AlwatrPageOrderDetail extends LocalizeMixin(SignalMixin(AlwatrBaseE
       tinted: 1,
       icon: 'receipt-outline',
       flipRtl: true,
-      headline: editMode
+      headline: this._editMode
         ? message('order_detail_headline_new')
         : message('order_item_headline').replace('${orderId}', replaceNumber(this.order.id.padStart(2, '0'))),
       description: message('order_item_status') + ': ' + message('order_status_' + this.order.status),
@@ -176,8 +186,72 @@ export class AlwatrPageOrderDetail extends LocalizeMixin(SignalMixin(AlwatrBaseE
     return html`
       <alwatr-icon-box .content=${iconBoxContent}></alwatr-icon-box>
       ${mapIterable(this, this.order?.itemList, this._itemDetailTemplate, message('loading'))}
-      <alwatr-button tinted class="add-button" @click=${this._addNewItem}>
-        ${message('order_detail_add_product_button')}
+      <alwatr-surface elevated>
+        <div class="detail-container">
+          <div>
+            <span>${message('order_shipping_recipient_name')}:</span>
+            <span>
+              <b>${this.order.delivery?.recipientName}</b>
+            </span>
+          </div>
+          <div>
+            <span>${message('order_shipping_recipient_national_code')}:</span>
+            <span>
+              <b>${this.order.delivery?.recipientNationalCode}</b>
+            </span>
+          </div>
+          <div>
+            <span>${message('order_shipping_address')}:</span>
+            <span><b>${this.order.delivery?.address}</b></span>
+          </div>
+          <div>
+            <span>${message('order_shipping_car_type_title')}:</span>
+            <span>
+              <b>${this.order.delivery?.carType}</b>
+            </span>
+          </div>
+          <div>
+            <span>${message('order_shipping_shipment_type_title')}:</span>
+            <span>
+              <b>${this.order.delivery?.shipmentType}</b>
+            </span>
+          </div>
+          <div>
+            <span>${message('order_shipping_time_period_title')}:</span>
+            <span>
+              <b>${message('time_period_' + this.order.delivery?.timePeriod.replace('-', '_'))}</b>
+            </span>
+          </div>
+        </div>
+      </alwatr-surface>
+
+      <alwatr-surface elevated>
+        <div class="detail-container">
+          <div>
+            <span>${message('order_detail_shipping_price')}:</span>
+            <span>
+              <b>${number(this.order.shippingPrice)}</b>
+              ${this.tomanSvg}
+            </span>
+          </div>
+          <div>
+            <span>${message('order_detail_total_price')}:</span>
+            <span>
+              <b>${number(this.order.totalPrice)}</b>
+              ${this.tomanSvg}
+            </span>
+          </div>
+          <div>
+            <span>${message('order_detail_final_price')}:</span>
+            <span>
+              <b>${number(this.order.finalPrice)}</b>
+              ${this.tomanSvg}
+            </span>
+          </div>
+        </div>
+      </alwatr-surface>
+      <alwatr-button tinted class="add-button" @click=${this._addNewItem}
+        >${message('order_detail_add_product_button')}
       </alwatr-button>
     `;
   }
@@ -198,29 +272,26 @@ export class AlwatrPageOrderDetail extends LocalizeMixin(SignalMixin(AlwatrBaseE
       <alwatr-surface elevated>
         <img src="${config.cdn + product.image.id}" />
         <div class="detail-container">
-          <div>
-            <span>${message('order_detail_product_name')}:</span>
-            <span><b>${product.title.fa}</b></span>
-          </div>
+          <b>${product.title.fa}</b>
           <div>
             <span>${message('order_detail_price')}:</span>
             <span>
               <b>${number(item.price)}</b>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14"><path fill-rule="evenodd" stroke="none" fill="currentcolor" d="M3.057 1.742L3.821 1l.78.75-.776.741-.768-.749zm3.23 2.48c0 .622-.16 1.111-.478 1.467-.201.221-.462.39-.783.505a3.251 3.251 0 01-1.083.163h-.555c-.421 0-.801-.074-1.139-.223a2.045 2.045 0 01-.9-.738A2.238 2.238 0 011 4.148c0-.059.001-.117.004-.176.03-.55.204-1.158.525-1.827l1.095.484c-.257.532-.397 1-.419 1.403-.002.04-.004.08-.004.12 0 .252.055.458.166.618a.887.887 0 00.5.354c.085.028.178.048.278.06.079.01.16.014.243.014h.555c.458 0 .769-.081.933-.244.14-.139.21-.383.21-.731V2.02h1.2v2.202zm5.433 3.184l-.72-.7.709-.706.735.707-.724.7zm-2.856.308c.542 0 .973.19 1.293.569.297.346.445.777.445 1.293v.364h.18v-.004h.41c.221 0 .377-.028.467-.084.093-.055.14-.14.14-.258v-.069c.004-.243.017-1.044 0-1.115L13 8.05v1.574a1.4 1.4 0 01-.287.863c-.306.405-.804.607-1.495.607h-.627c-.061.733-.434 1.257-1.117 1.573-.267.122-.58.21-.937.265a5.845 5.845 0 01-.914.067v-1.159c.612 0 1.072-.082 1.38-.247.25-.132.376-.298.376-.499h-.515c-.436 0-.807-.113-1.113-.339-.367-.273-.55-.667-.55-1.18 0-.488.122-.901.367-1.24.296-.415.728-.622 1.296-.622zm.533 2.226v-.364c0-.217-.048-.389-.143-.516a.464.464 0 00-.39-.187.478.478 0 00-.396.187.705.705 0 00-.136.449.65.65 0 00.003.067c.008.125.066.22.177.283.093.054.21.08.352.08h.533zM9.5 6.707l.72.7.724-.7L10.209 6l-.709.707zm-6.694 4.888h.03c.433-.01.745-.106.937-.29.024.012.065.035.12.068l.074.039.081.042c.135.073.261.133.379.18.345.146.67.22.977.22a1.216 1.216 0 00.87-.34c.3-.285.449-.714.449-1.286a2.19 2.19 0 00-.335-1.145c-.299-.457-.732-.685-1.3-.685-.502 0-.916.192-1.242.575-.113.132-.21.284-.294.456-.032.062-.06.125-.084.191a.504.504 0 00-.03.078 1.67 1.67 0 00-.022.06c-.103.309-.171.485-.205.53-.072.09-.214.14-.427.147-.123-.005-.209-.03-.256-.076-.057-.054-.085-.153-.085-.297V7l-1.201-.5v3.562c0 .261.048.496.143.703.071.158.168.296.29.413.123.118.266.211.43.28.198.084.42.13.665.136v.001h.036zm2.752-1.014a.778.778 0 00.044-.353.868.868 0 00-.165-.47c-.1-.134-.217-.201-.35-.201-.18 0-.33.103-.447.31-.042.071-.08.158-.114.262a2.434 2.434 0 00-.04.12l-.015.053-.015.046c.142.118.323.216.544.293.18.062.325.092.433.092.044 0 .086-.05.125-.152z"></path></svg>
+              ${this.tomanSvg}
             </span>
           </div>
           <div>
             <span>${message('order_detail_final_price')}:</span>
             <span>
               <b>${number(item.finalPrice)}</b>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14"><path fill-rule="evenodd" stroke="none" fill="currentcolor" d="M3.057 1.742L3.821 1l.78.75-.776.741-.768-.749zm3.23 2.48c0 .622-.16 1.111-.478 1.467-.201.221-.462.39-.783.505a3.251 3.251 0 01-1.083.163h-.555c-.421 0-.801-.074-1.139-.223a2.045 2.045 0 01-.9-.738A2.238 2.238 0 011 4.148c0-.059.001-.117.004-.176.03-.55.204-1.158.525-1.827l1.095.484c-.257.532-.397 1-.419 1.403-.002.04-.004.08-.004.12 0 .252.055.458.166.618a.887.887 0 00.5.354c.085.028.178.048.278.06.079.01.16.014.243.014h.555c.458 0 .769-.081.933-.244.14-.139.21-.383.21-.731V2.02h1.2v2.202zm5.433 3.184l-.72-.7.709-.706.735.707-.724.7zm-2.856.308c.542 0 .973.19 1.293.569.297.346.445.777.445 1.293v.364h.18v-.004h.41c.221 0 .377-.028.467-.084.093-.055.14-.14.14-.258v-.069c.004-.243.017-1.044 0-1.115L13 8.05v1.574a1.4 1.4 0 01-.287.863c-.306.405-.804.607-1.495.607h-.627c-.061.733-.434 1.257-1.117 1.573-.267.122-.58.21-.937.265a5.845 5.845 0 01-.914.067v-1.159c.612 0 1.072-.082 1.38-.247.25-.132.376-.298.376-.499h-.515c-.436 0-.807-.113-1.113-.339-.367-.273-.55-.667-.55-1.18 0-.488.122-.901.367-1.24.296-.415.728-.622 1.296-.622zm.533 2.226v-.364c0-.217-.048-.389-.143-.516a.464.464 0 00-.39-.187.478.478 0 00-.396.187.705.705 0 00-.136.449.65.65 0 00.003.067c.008.125.066.22.177.283.093.054.21.08.352.08h.533zM9.5 6.707l.72.7.724-.7L10.209 6l-.709.707zm-6.694 4.888h.03c.433-.01.745-.106.937-.29.024.012.065.035.12.068l.074.039.081.042c.135.073.261.133.379.18.345.146.67.22.977.22a1.216 1.216 0 00.87-.34c.3-.285.449-.714.449-1.286a2.19 2.19 0 00-.335-1.145c-.299-.457-.732-.685-1.3-.685-.502 0-.916.192-1.242.575-.113.132-.21.284-.294.456-.032.062-.06.125-.084.191a.504.504 0 00-.03.078 1.67 1.67 0 00-.022.06c-.103.309-.171.485-.205.53-.072.09-.214.14-.427.147-.123-.005-.209-.03-.256-.076-.057-.054-.085-.153-.085-.297V7l-1.201-.5v3.562c0 .261.048.496.143.703.071.158.168.296.29.413.123.118.266.211.43.28.198.084.42.13.665.136v.001h.036zm2.752-1.014a.778.778 0 00.044-.353.868.868 0 00-.165-.47c-.1-.134-.217-.201-.35-.201-.18 0-.33.103-.447.31-.042.071-.08.158-.114.262a2.434 2.434 0 00-.04.12l-.015.053-.015.046c.142.118.323.216.544.293.18.062.325.092.433.092.044 0 .086-.05.125-.152z"></path></svg>
+              ${this.tomanSvg}
             </span>
           </div>
           <div>
             <span>${message('order_detail_qty')}:</span>
-            <span>
-              <alwatr-text-field type="number" .value=${item.qty.toString()} outlined></alwatr-text-field>
-            </span>
+            ${this._editMode === true
+              ? html`<alwatr-text-field type="number" .value=${item.qty.toString()} outlined></alwatr-text-field>`
+              : html`<span><b>${item.qty}</b></span>`}
           </div>
         </div>
       </alwatr-surface>
