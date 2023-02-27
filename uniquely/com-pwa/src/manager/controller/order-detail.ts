bf2ec78458cd575849d70469d31c3ec265705208
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-27 18:12:35 +0330

    feat(com-pwa): new order detail controller

--- uniquely/com-pwa/src/manager/controller/order-detail.ts
+++ uniquely/com-pwa/src/manager/controller/order-detail.ts
@@ -7,17 +7,20 @@ import {orderStorageContextConsumer, topAppBarContextProvider} from '../context.
 import type {AlwatrDocumentStorage, ClickSignalType} from '@alwatr/type';
 import type {Order} from '@alwatr/type/customer-order-management.js';
 
-export const pageOrderListFsm = new FiniteStateMachine({
-  id: 'page-order-list',
+export const pageOrderDetailFsm = new FiniteStateMachine({
+  id: 'page-order-detail',
   initial: 'unresolved',
   context: {
+    order: <Order | null>null,
+    orderId: <number | null>null,
     orderStorage: <AlwatrDocumentStorage<Order> | null>null,
   },
   states: {
     $all: {
       on: {
+        SHOW_DETAIL: '$self',
         CONNECTED: '$self',
-        CONTEXT_LOADED: 'list',
+        CONTEXT_LOADED: 'detail',
       },
     },
     unresolved: {
@@ -33,21 +36,25 @@ export const pageOrderListFsm = new FiniteStateMachine({
     loading: {
       on: {},
     },
-    list: {
+    detail: {
       on: {
         REQUEST_UPDATE: 'reloading',
+        INVALID_ORDER: 'notFound',
       },
     },
     reloading: {
       on: {},
     },
+    notFound: {
+      on: {},
+    },
   },
 } as const);
 
-pageOrderListFsm.signal.subscribe(async (state) => {
-  // logger.logMethodArgs('pageOrderListFsm.changed', state);
+pageOrderDetailFsm.signal.subscribe(async (state) => {
+  // logger.logMethodArgs('pageOrderDetailFsm.changed', state);
   switch (state.by) {
-    case 'IMPORT':
+    case 'IMPORT': {
       // just in unresolved
       topAppBarContextProvider.setValue({
         headlineKey: 'loading',
@@ -56,28 +63,33 @@ pageOrderListFsm.signal.subscribe(async (state) => {
         fetchOrderStorage();
       }
       break;
+    }
 
-    case 'CONNECTED':
+    case 'CONNECTED': {
       topAppBarContextProvider.setValue({
         headlineKey: 'page_order_list_headline',
       });
       break;
+    }
 
-    case 'REQUEST_UPDATE':
+    case 'REQUEST_UPDATE': {
       await fetchOrderStorage();
-      pageOrderListFsm.transition('CONTEXT_LOADED');
+      pageOrderDetailFsm.transition('CONTEXT_LOADED');
       break;
+    }
+  }
+
+  if (state.to === 'detail') {
+    // validate order id
+    const order = pageOrderDetailFsm.context.orderStorage?.data[pageOrderDetailFsm.context.orderId ?? ''] ?? null;
+    pageOrderDetailFsm.transition('INVALID_ORDER', {order});
   }
 });
 
 orderStorageContextConsumer.subscribe((orderStorage) => {
-  pageOrderListFsm.transition('CONTEXT_LOADED', {orderStorage});
+  pageOrderDetailFsm.transition('CONTEXT_LOADED', {orderStorage});
 });
 
-eventListener.subscribe<ClickSignalType>('page_order_list_reload_click_event', () => {
-  pageOrderListFsm.transition('REQUEST_UPDATE');
-});
-
-eventListener.subscribe<ClickSignalType>('new_order_click_event', () => {
-  // TODO: redirect
+eventListener.subscribe<ClickSignalType>('page_order_detail_reload_click_event', () => {
+  pageOrderDetailFsm.transition('REQUEST_UPDATE');
 });
