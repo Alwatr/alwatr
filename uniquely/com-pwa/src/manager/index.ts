8c0781b3583195f756e15e2339298251663821dd
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-27 18:39:57 +0330

    refactor(com-pwa): rearrange files

--- uniquely/com-pwa/src/manager/index.ts
+++ uniquely/com-pwa/src/manager/index.ts
@@ -1,2 +1,5 @@
+import './context-provider/home-page-content.js';
+import './context-provider/l18e.js';
 import './context-provider/order-storage.js';
+import './context-provider/user.js';
 import './controller/order-list.js';
