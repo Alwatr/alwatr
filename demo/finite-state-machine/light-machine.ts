1fa8aac30ca6b1e15fd29309384466962b4b9f64
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-02-27 18:34:32 +0330

    fix(demo/fsm): context object

--- demo/finite-state-machine/light-machine.ts
+++ demo/finite-state-machine/light-machine.ts
@@ -3,7 +3,10 @@ import {FiniteStateMachine} from '@alwatr/fsm';
 const lightMachine = new FiniteStateMachine({
   id: 'light-machine',
   initial: 'green',
-  context: <number>0,
+  context: {
+    a: <number> 0,
+    b: <number> 0,
+  },
   states: {
     $all: {
       on: {
@@ -37,9 +40,9 @@ lightMachine.signal.subscribe((state) => {
   console.log('****\nstate: %s, context: %s\n****', state, lightMachine.context);
 }, {receivePrevious: 'No'});
 
-lightMachine.transition('TIMER', 1);
-lightMachine.transition('TIMER', 2);
-lightMachine.transition('TIMER', 3);
-lightMachine.transition('POWER_LOST', 4);
-lightMachine.transition('TIMER', 5);
-lightMachine.transition('POWER_BACK', 6);
+lightMachine.transition('TIMER', {a: 1});
+lightMachine.transition('TIMER', {b: 2});
+lightMachine.transition('TIMER');
+lightMachine.transition('POWER_LOST', {a: 4});
+lightMachine.transition('TIMER', {a: 5, b: 5});
+lightMachine.transition('POWER_BACK', {a: 6});
