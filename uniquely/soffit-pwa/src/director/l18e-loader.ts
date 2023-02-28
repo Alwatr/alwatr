4c8544f655023f1ad3e50af3f82909262aedf3f9
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-02-28 17:55:02 +0330

    refactor: l18e name
    Co-authored-by: S. Amir Mohammad Najafi <njfamirm@gmail.com>

--- uniquely/soffit-pwa/src/director/l18e-loader.ts
+++ uniquely/soffit-pwa/src/director/l18e-loader.ts
@@ -8,6 +8,6 @@ setL18eLoader((locale) => {
   const language = locale.language;
   logger.logMethodArgs('l18eLoader', {language});
   return language === 'en'
-    ? import('../l18r/en.json', {assert: {type: 'json'}}) as unknown as Promise<L18eContext>
-    : import('../l18r/fa.json', {assert: {type: 'json'}}) as unknown as Promise<L18eContext>;
+    ? import('../l18e/en.json', {assert: {type: 'json'}}) as unknown as Promise<L18eContext>
+    : import('../l18e/fa.json', {assert: {type: 'json'}}) as unknown as Promise<L18eContext>;
 });
