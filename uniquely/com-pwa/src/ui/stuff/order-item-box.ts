410025692122c7e23bba8cf6963476df64f5ac08
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-03-01 15:12:13 +0330

    chore(com-pwa): new elements todo

---
+++ uniquely/com-pwa/src/ui/stuff/order-item-box.ts
@@ -0,0 +1,3 @@
+// TODO: Dummy element for display single order icon box
+// @prop order: Order
+// @prop clickSignalId: string => dispatch with ClickSignalType<Order>
---
+++ uniquely/com-pwa/src/ui/stuff/order-list.ts
@@ -0,0 +1,3 @@
+// TODO: Dummy element for display list of order icon box without any action icon
+// @prop orderStorage: AlwatrDocument<Order>
+// @prop clickSignalId: strimg => dispatch with ClickSignalType<Order>
