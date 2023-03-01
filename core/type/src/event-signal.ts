c79f534f61136fde26ff28292228033bae644679
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-03-01 15:49:17 +0330

    feat(button): click signal with detail

--- core/type/src/event-signal.ts
+++ core/type/src/event-signal.ts
@@ -1,7 +1,10 @@
-export type ClickSignalType = {
+import type {Stringifyable} from './type-helper.js';
+
+export type ClickSignalType<T extends Stringifyable = Stringifyable> = {
   readonly x: number;
   readonly y: number;
   readonly altKey: boolean;
   readonly ctrlKey: boolean;
   readonly metaKey: boolean;
+  detail: T;
 };
