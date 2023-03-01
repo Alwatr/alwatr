d03c8cfd826ecdf681d91f72595ae4454b5d5644
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-03-01 17:12:21 +0330

    feat(com-pwa): order-list element

--- uniquely/com-pwa/src/ui/stuff/order-list.ts
+++ uniquely/com-pwa/src/ui/stuff/order-list.ts
@@ -1,3 +1,45 @@
-// TODO: Dummy element for display list of order icon box without any action icon
-// @prop orderStorage: AlwatrDocument<Order>
-// @prop clickSignalId: strimg => dispatch with ClickSignalType<Order>
+import {customElement, AlwatrBaseElement, html, property, nothing, css, mapObject, ifDefined} from '@alwatr/element';
+import '@alwatr/ui-kit/card/icon-box.js';
+
+import type {AlwatrDocumentStorage} from '@alwatr/type';
+import type {Order} from '@alwatr/type/customer-order-management.js';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'alwatr-order-list': AlwatrOrderList;
+  }
+}
+
+/**
+ * Alwatr Order List Element.
+ */
+@customElement('alwatr-order-list')
+export class AlwatrOrderList extends AlwatrBaseElement {
+  static override styles = css`
+    :host {
+      display: block;
+    }
+
+    :host > * {
+      margin-bottom: var(--sys-spacing-track);
+    }
+  `;
+
+  @property({attribute: false})
+    orderStorage?: AlwatrDocumentStorage<Order> | null;
+
+  @property({attribute: 'order-click-signal-id'})
+    orderClickSignalId?: string;
+
+  override render(): unknown {
+    this._logger.logMethod('render');
+    if (this.orderStorage == null) return nothing;
+
+    return mapObject(this, this.orderStorage.data, (order) => {
+      return html`<alwatr-order-item-box
+        order-click-signal-id=${ifDefined(this.orderClickSignalId)}
+        .order=${order}
+      ></alwatr-order-item-box>`;
+    });
+  }
+}
