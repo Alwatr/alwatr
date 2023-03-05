2ddb008510438f9e80642ae72eb28df796629fb6
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-03-05 18:40:37 +0330

    fix(soffit-pwa): set name prop

--- uniquely/soffit-pwa/src/lottery-form.ts
+++ uniquely/soffit-pwa/src/lottery-form.ts
@@ -109,31 +109,31 @@ export class AlwatrLotteryForm extends LocalizeMixin(SignalMixin(AlwatrBaseEleme
     this._logger.logMethod('render');
     return html`
       <alwatr-text-field
-        name="code"
-        type="number"
+        .name=${'code'}
+        .type=${'number'}
+        .placeholder=${message('lottery_code')}
         outlined
         active-outline
         stated
-        placeholder=${message('lottery_code')}
       ></alwatr-text-field>
       <alwatr-text-field
-        name="name"
-        type="text"
+        .name=${'name'}
+        .type=${'text'}
+        .placeholder=${message('full_name')}
         outlined
         active-outline
         stated
-        placeholder=${message('full_name')}
       ></alwatr-text-field>
       <alwatr-text-field
-        name="phone"
-        type="tel"
+        .name=${'phone'}
+        .type=${'tel'}
         outlined
         active-outline
         stated
-        placeholder=${message('phone_number')}
+        .placeholder=${message('phone_number')}
       ></alwatr-text-field>
       <alwatr-radio-group
-        name="activity"
+        .name=${'activity'}
         .options=${this._radioGroupOptions}
       ></alwatr-radio-group>
       <div class="button-container">
