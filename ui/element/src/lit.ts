fc4a44357b3a5e5071634ac9d6390100c80d2436
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-03-05 14:37:54 +0330

    feat(element): add live directive

--- ui/element/src/lit.ts
+++ ui/element/src/lit.ts
@@ -2,6 +2,7 @@ export * from 'lit';
 export * from 'lit/decorators.js';
 export {map} from 'lit/directives/map.js';
 export {when} from 'lit/directives/when.js';
+export {live} from 'lit/directives/live.js';
 export {repeat} from 'lit/directives/repeat.js';
 export {ifDefined} from 'lit/directives/if-defined.js';
 export {unsafeSVG} from 'lit/directives/unsafe-svg.js';
