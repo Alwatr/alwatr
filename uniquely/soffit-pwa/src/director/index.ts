b9b36fe747c24967668835c5244f1319330b44e9
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-03-08 19:29:35 +0330

    refactor(soffit-pwa): manager folder structure

--- uniquely/soffit-pwa/src/director/index.ts
+++
@@ -1,4 +0,0 @@
-import './change-language.js';
-import './content-provider.js';
-import './l18e-loader.js';
-import './submit-form-command-handler.js';
---
+++ uniquely/soffit-pwa/src/manager/index.ts
@@ -0,0 +1,5 @@
+import './change-language.js';
+import './context-provider/home-page-content.js';
+import './context-provider/product-page-content.js';
+import './l18e.js';
+import './submit-form-command-handler.js';
