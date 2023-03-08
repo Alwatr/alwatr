6e0c0029bc9171fcabe45c6477f88b46c193036a
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-03-08 15:19:03 +0330

    feat(ui-kit/navigation-bar): rewrite

--- ui/pwa-helper/src/context.ts
+++ ui/pwa-helper/src/context.ts
@@ -1,8 +1,12 @@
-import {contextProvider, commandTrigger} from '@alwatr/signal';
+import {contextProvider, commandTrigger, eventTrigger} from '@alwatr/signal';
 
+import type {NavigationBarContent} from '@alwatr/ui-kit/navigation-bar/navigation-bar.js';
 import type {TopAppBarContent} from '@alwatr/ui-kit/top-app-bar/top-app-bar.js';
 
 export const topAppBarContextProvider = contextProvider.bind<TopAppBarContent>('top_app_bar_context');
 // export const topAppBarContextConsumer = contextConsumer.bind<TopAppBarContent>(topAppBarContextProvider.id);
+
+export const navigationBarActiveItemIdEventTrigger = eventTrigger.bind<{id: string}>('active_item_id_signal');
+export const navigationBarContextProvider = contextProvider.bind<NavigationBarContent>('active_item_id_signal');
 
 export const scrollToTopCommand = commandTrigger.bind<{smooth?: boolean}, undefined>('command_scroll_to_top');
--- ui/ui-kit/src/navigation-bar/navigation-bar.ts
+++ ui/ui-kit/src/navigation-bar/navigation-bar.ts
@@ -1,35 +1,163 @@
-import {AlwatrDummyElement, customElement, html, css} from '@alwatr/element';
+import {customElement, property, html, mapIterable, css, SignalMixin, LocalizeMixin} from '@alwatr/element';
+import {message} from '@alwatr/i18n';
+import '@alwatr/icon';
+import {url} from '@alwatr/router';
+import {contextConsumer, eventListener, eventTrigger} from '@alwatr/signal';
 
-import './navigation-bar-item.js';
+import {AlwatrSurface} from '../card/surface.js';
 
-/**
- * @slot - alwatr-navigation-bar-item list
- */
-@customElement('alwatr-navigation-bar')
-export class AlwatrNavigationBar extends AlwatrDummyElement {
-  static override styles = [
-    css`
-      :host {
-        display: flex;
+import type {RouteContextBase} from '@alwatr/router/src/type.js';
+import type {StringifyableRecord} from '@alwatr/type';
 
-        z-index: var(--sys-zindex-above);
-        background-color: var(--sys-color-surface);
 
-        width: 100%;
-        height: calc(10 * var(--sys-spacing-track));
-
-        box-shadow: var(--elevation-5);
-      }
-    `,
-  ];
-
-  override render(): unknown {
-    return html`<slot></slot>`;
-  }
+export interface NavigationBarItemContent extends StringifyableRecord {
+  id: string;
+  icon: string;
+  iconFlipRtl?: boolean;
+  link: RouteContextBase['sectionList'];
+  label?: string;
+  // badgeValue: string,
+}
+export interface NavigationBarContent extends StringifyableRecord {
+  itemList: Array<NavigationBarItemContent>;
 }
 
 declare global {
   interface HTMLElementTagNameMap {
     'alwatr-navigation-bar': AlwatrNavigationBar;
+  }
+}
+
+/**
+ * Alwatr Navigation Bar Element.
+ *
+ * @attr {String} context-signal - context signal name.
+ * @attr {String} active-item-id-signal - active item id signal name.
+ */
+@customElement('alwatr-navigation-bar')
+export class AlwatrNavigationBar extends LocalizeMixin(SignalMixin(AlwatrSurface)) {
+  static override styles = [
+    AlwatrSurface.styles,
+    css`
+      :host {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        gap: calc(var(--sys-spacing-track) / 2);
+        padding: calc(1.5 * var(--sys-spacing-track)) 0 calc(2 * var(--sys-spacing-track)) 0;
+        color: var(--sys-color-surface);
+        border-radius: 0;
+        user-select: none;
+        z-index: var(--sys-zindex-sticky);
+        cursor: pointer;
+      }
+
+      :host > * {
+        height: 100%;
+        flex-grow: 1;
+        text-align: center;
+        text-decoration: none;
+      }
+
+      .indicator {
+        display: inline-block;
+      }
+      .item[data-active] .indicator {
+        background-color: var(--sys-color-secondary-container);
+        color: var(--sys-color-secondary-container);
+        border-radius: var(--sys-radius-large);
+      }
+
+      alwatr-icon {
+        padding: calc(var(--sys-spacing-track) / 2) calc(2.5 * var(--sys-spacing-track));
+        color: var(--sys-color-on-surface-variant);
+        font-size: calc(3 * var(--sys-spacing-track));
+      }
+      .item[data-active] alwatr-icon {
+        color: var(--sys-color-on-secondary-container);
+      }
+
+      .label {
+        margin-top: calc(var(--sys-spacing-track));
+        color: var(--sys-color-on-surface-variant);
+        font-family: var(--sys-typescale-label-medium-font-family-name);
+        font-weight: var(--sys-typescale-label-medium-font-weight);
+        font-size: var(--sys-typescale-label-medium-font-size);
+        letter-spacing: var(--sys-typescale-label-medium-letter-spacing);
+        line-height: var(--sys-typescale-label-medium-line-height);
+      }
+      .item[data-active] .label {
+        color: var(--sys-color-on-surface);
+      }
+    `,
+  ];
+
+  @property()
+    content?: NavigationBarContent;
+
+  @property()
+    activeItemId?: string;
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+    this.setAttribute('elevated', '2');
+    this.setAttribute('tinted', '');
+
+    const contextSignal = this.getAttribute('context-signal');
+    if (contextSignal) {
+      this._signalListenerList.push(
+          contextConsumer.subscribe<NavigationBarContent>(contextSignal, (context) => {
+            this.content = context;
+            this.requestUpdate(); // Ensure update on child properties changes.
+          },
+          {receivePrevious: 'NextCycle'}),
+      );
+    }
+
+    const activeItemIdSignal = this.getAttribute('active-item-id-signal');
+    if (activeItemIdSignal) {
+      this._signalListenerList.push(
+          eventListener.subscribe<{id: string}>(activeItemIdSignal, (detail) => {
+            this.activeItemId = detail.id;
+            this.requestUpdate(); // Ensure update on child properties changes.
+          },
+          {receivePrevious: 'NextCycle'}),
+      );
+    }
+  }
+
+  override render(): unknown {
+    this._logger.logMethod('render');
+    if (this.content?.itemList == null) return;
+
+    return mapIterable(this, this.content.itemList, this.navigationItem);
+  }
+
+  protected navigationItem(content: NavigationBarItemContent): unknown {
+    const iconName = content.icon + (content.id === this.activeItemId ? '-sharp' : '-outline');
+    return html`
+      <a
+        class="item"
+        href=${url({sectionList: content.link})}
+        data-id=${content.id}
+        ?data-active=${this.activeItemId === content.id}
+        @click=${this.itemClickHandler}>
+        <div class="indicator">
+          <alwatr-icon .name=${iconName} ?flip-rtl=${content.iconFlipRtl}></alwatr-icon>
+        </div>
+        <div class="label">${message(content.label)}</div>
+      </a>
+    `;
+  }
+
+  protected itemClickHandler(event: MouseEvent): void {
+    this._logger.logMethod('itemClickHandler');
+    const target = event.currentTarget as HTMLDivElement;
+    const id = target.getAttribute('data-id');
+    const activeItemIdSignal = this.getAttribute('active-item-id-signal');
+    if (id == null || activeItemIdSignal == null) return;
+
+    eventTrigger.dispatch(activeItemIdSignal, {id});
   }
 }
