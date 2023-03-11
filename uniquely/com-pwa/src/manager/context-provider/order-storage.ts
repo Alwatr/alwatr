e7a5faae356383ff6d581e1a6ec28f280f11529f
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-03-11 19:15:57 +0330

    feat(com/provider/storage): new provider base on new context state

--- uniquely/com-pwa/src/manager/context-provider/order-storage.ts
+++ uniquely/com-pwa/src/manager/context-provider/order-storage.ts
@@ -1,37 +1,97 @@
-import {fetchContext} from '@alwatr/fetch';
-import {l18eReadyPromise} from '@alwatr/i18n';
-import {snackbarSignalTrigger} from '@alwatr/ui-kit/snackbar/show-snackbar.js';
+import {serviceRequest, type FetchOptions} from '@alwatr/fetch';
+import {contextConsumer, DispatchOptions, requestableContextProvider} from '@alwatr/signal';
 
 import {config} from '../../config.js';
-import {
-  orderStorageContextConsumer,
-  userContextConsumer,
-} from '../context.js';
 import {logger} from '../logger.js';
 
-export const fetchOrderStorage = async (): Promise<void> => {
-  logger.logMethod('fetchOrderStorage');
+import type {AlwatrDocumentStorage, User} from '@alwatr/type';
+import type {Order} from '@alwatr/type/customer-order-management.js';
+
+const orderStorageContextProvider = requestableContextProvider.bind<AlwatrDocumentStorage<Order>>(
+    'order-storage-context',
+);
+
+const userContextConsumer = contextConsumer.bind<User>('user-context');
+
+orderStorageContextProvider.setProvider(async () => {
+  logger.logMethod('requestOrderStorageContext');
+  let context = orderStorageContextProvider.getValue();
+
+  if (context.state === 'pending' || context.state === 'reloading') return;
 
   const userContext = userContextConsumer.getValue() ?? (await userContextConsumer.untilChange());
+  const dispatchOptions: Partial<DispatchOptions> = {debounce: 'NextCycle'};
+  const fetchOption: FetchOptions = {
+    ...config.fetchContextOptions,
+    url: config.api + '/order-list/',
+    queryParameters: {
+      userId: userContext.id,
+    },
+  };
+
+  if (context.state === 'initial') {
+    context = {state: 'pending'};
+    orderStorageContextProvider.setValue(context, dispatchOptions);
+    try {
+      fetchOption.cacheStrategy = 'cache_only';
+      const response = await serviceRequest(fetchOption) as AlwatrDocumentStorage<Order>;
+      context = {
+        state: 'reloading',
+        content: response,
+      };
+      orderStorageContextProvider.setValue(context, dispatchOptions);
+    }
+    catch (err) {
+      if ((err as Error).message === 'fetch_cache_not_found') {
+        logger.logOther('requestOrderStorageContext:', 'fetch_cache_not_found');
+      }
+      else {
+        logger.error('requestOrderStorageContext', 'fetch_failed', err);
+        context = {state: 'error'};
+        orderStorageContextProvider.setValue(context, dispatchOptions);
+        return;
+      }
+    }
+  }
+
+  if (navigator.onLine === false) {
+    context = {
+      state: 'error',
+      content: context.content, // maybe offline exist
+    };
+    orderStorageContextProvider.setValue(context, dispatchOptions);
+    return;
+  }
 
   try {
-    await fetchContext(
-        orderStorageContextConsumer.id,
-        {
-          ...config.fetchContextOptions,
-          url: config.api + '/order-list/',
-          queryParameters: {
-            userId: userContext.id,
-          },
-        },
-        {debounce: 'NextCycle'},
-    );
+    fetchOption.cacheStrategy = 'update_cache';
+    const response = await serviceRequest(fetchOption) as AlwatrDocumentStorage<Order>;
+    if (
+      context.content != null &&
+      response.meta?.lastUpdated != undefined &&
+      response.meta.lastUpdated === context.content.meta?.lastUpdated
+    ) {
+      // no changed
+      context = {
+        state: 'complete',
+        content: context.content,
+      };
+    }
+    else {
+      context = {
+        state: 'complete',
+        content: response,
+      };
+    }
+    orderStorageContextProvider.setValue(context, dispatchOptions);
   }
   catch (err) {
-    logger.error('fetchOrderStorage', 'fetch_failed', err);
-    await l18eReadyPromise;
-    await snackbarSignalTrigger.requestWithResponse({
-      messageKey: 'fetch_failed',
-    });
+    logger.error('fetchContext', 'fetch_failed', err);
+    context = {
+      state: 'error',
+      content: context.content, // maybe offline exist
+    };
+    orderStorageContextProvider.setValue(context, dispatchOptions);
+    return;
   }
-};
+});
