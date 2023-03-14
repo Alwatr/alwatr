035a381f84710d47693831b725568f1983173f20
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-03-14 17:26:41 +0330

    refactor(com-pwa): priceProductStorage

--- uniquely/com-pwa/src/manager/index.ts
+++ uniquely/com-pwa/src/manager/index.ts
@@ -1,6 +1,7 @@
 import './context-provider/home-page-content.js';
 import './context-provider/l18e.js';
 import './context-provider/order-storage.js';
+import './context-provider/product-price-storage.js';
 import './context-provider/product-storage.js';
 import './context-provider/user.js';
 import './submit-order-command-handler.js';
