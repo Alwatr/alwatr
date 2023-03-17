560dd343c688d6545f1d0a64a87fb829eb68252e
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-03-18 00:11:45 +0330

    feat(com-pwa/new-order): use new fsm

--- uniquely/com-pwa/src/manager/index.ts
+++ uniquely/com-pwa/src/manager/index.ts
@@ -3,5 +3,6 @@ import './context-provider/l18e.js';
 import './context-provider/order-storage.js';
 import './context-provider/product-price-storage.js';
 import './context-provider/user.js';
+import './controller/new-order.js';
 import './controller/order-detail.js';
 import './submit-order-command-handler.js';
