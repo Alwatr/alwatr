a8d80d1d7b3fb15d490eb7c3ddd1e021973678f6
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-03-18 17:45:59 +0330

    fix(com-pwa): import price storage context provider

--- uniquely/com-pwa/src/manager/index.ts
+++ uniquely/com-pwa/src/manager/index.ts
@@ -1,7 +1,8 @@
 import './context-provider/home-page-content.js';
 import './context-provider/l18e.js';
 import './context-provider/order-storage.js';
-import './context-provider/product-price-storage.js';
+import './context-provider/price-storage.js';
+import './context-provider/product-storage.js';
 import './context-provider/user.js';
 import './controller/new-order.js';
 import './controller/order-detail.js';
