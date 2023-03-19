9129aad052f49afbca13a80e3e5b92127bfabc3e
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-03-19 13:39:50 +0330

    feat(com): new order storage provider base on new context

--- uniquely/com-pwa/src/manager/context-provider/order-storage.ts
+++ uniquely/com-pwa/src/manager/context-provider/order-storage.ts
@@ -1,96 +1,27 @@
-import {serviceRequest, type FetchOptions} from '@alwatr/fetch';
-import {contextConsumer, DispatchOptions, requestableContextProvider} from '@alwatr/signal';
+import {serverContextConsumer} from '@alwatr/context';
 
 import {config} from '../../config.js';
-import {logger} from '../logger.js';
 
 import type {AlwatrDocumentStorage, User} from '@alwatr/type';
-import type {Order} from '@alwatr/type/customer-order-management.js';
+import type {Order} from '@alwatr/type/src/customer-order-management.js';
 
-const orderStorageContextProvider =
-  requestableContextProvider.bind<AlwatrDocumentStorage<Order>>('order-storage-context');
-
-const userContextConsumer = contextConsumer.bind<User>('user-context');
-
-orderStorageContextProvider.setProvider(async () => {
-  logger.logMethod('requestOrderStorageContext');
-  let context = orderStorageContextProvider.getValue();
-
-  if (context.state === 'pending' || context.state === 'reloading') return;
-
-  const userContext = userContextConsumer.getValue() ?? (await userContextConsumer.untilChange());
-  const dispatchOptions: Partial<DispatchOptions> = {debounce: 'NextCycle'};
-  const fetchOption: FetchOptions = {
-    ...config.fetchContextOptions,
-    url: config.api + '/order-list/',
-    queryParameters: {
-      userId: userContext.id,
+export const orderStorageContextConsumer = serverContextConsumer<AlwatrDocumentStorage<Order>>(
+    'order_storage_context',
+    {
+      ...config.fetchContextOptions,
+      url: config.api + '/order-list/',
     },
-  };
+);
 
-  if (context.state === 'initial') {
-    context = {state: 'pending'};
-    orderStorageContextProvider.setValue(context, dispatchOptions);
-    try {
-      fetchOption.cacheStrategy = 'cache_only';
-      const response = await serviceRequest(fetchOption) as AlwatrDocumentStorage<Order>;
-      context = {
-        state: 'reloading',
-        content: response,
-      };
-      orderStorageContextProvider.setValue(context, dispatchOptions);
-    }
-    catch (err) {
-      if ((err as Error).message === 'fetch_cache_not_found') {
-        logger.logOther('requestOrderStorageContext:', 'fetch_cache_not_found');
-      }
-      else {
-        logger.error('requestOrderStorageContext', 'fetch_failed', err);
-        context = {state: 'error'};
-        orderStorageContextProvider.setValue(context, dispatchOptions);
-        return;
-      }
-    }
-  }
-
-  if (navigator.onLine === false) {
-    context = {
-      state: 'error',
-      content: context.content, // maybe offline exist
-    };
-    orderStorageContextProvider.setValue(context, dispatchOptions);
-    return;
-  }
-
-  try {
-    fetchOption.cacheStrategy = 'update_cache';
-    const response = await serviceRequest(fetchOption) as AlwatrDocumentStorage<Order>;
-    if (
-      context.content != null &&
-      response.meta?.lastUpdated != undefined &&
-      response.meta.lastUpdated === context.content.meta?.lastUpdated
-    ) {
-      // no changed
-      context = {
-        state: 'complete',
-        content: context.content,
-      };
-    }
-    else {
-      context = {
-        state: 'complete',
-        content: response,
-      };
-    }
-    orderStorageContextProvider.setValue(context, dispatchOptions);
-  }
-  catch (err) {
-    logger.error('fetchContext', 'fetch_failed', err);
-    context = {
-      state: 'error',
-      content: context.content, // maybe offline exist
-    };
-    orderStorageContextProvider.setValue(context, dispatchOptions);
-    return;
-  }
-});
+orderStorageContextConsumer.fsm.defineSignals([
+  {
+    signalId: 'user_context',
+    callback: (user: User): void => {
+      (orderStorageContextConsumer.getOptions().queryParameters ??= {}).userId = user.id;
+    },
+  },
+  {
+    signalId: 'reload_order_storage',
+    transition: 'REQUEST',
+  },
+]);
