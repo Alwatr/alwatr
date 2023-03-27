c23c58f2dcca2a3646d6c259061449306d582911
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-03-27 22:50:20 +0330

    feat(ui/chat); content property

--- ui/ui-kit/src/chat/chat.ts
+++ ui/ui-kit/src/chat/chat.ts
@@ -1,9 +1,9 @@
-import {customElement, css, html, AlwatrBaseElement, SignalMixin} from '@alwatr/element';
+import {customElement, css, html, AlwatrBaseElement, SignalMixin, property} from '@alwatr/element';
 
 import './chat-footer.js';
 import './chat-list.js';
 
-import type {ChatStorage} from '@alwatr/type';
+import type {AlwatrDocumentStorage, ChatMessage, StringifyableRecord} from '@alwatr/type';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -11,102 +11,10 @@ declare global {
   }
 }
 
-const currentUser = 'user-1';
-
-const chatStorage: ChatStorage = {
-  ok: true,
-  meta: {
-    id: `${currentUser}-chat`,
-    formatVersion: 4,
-    lastAutoId: 0,
-    lastUpdated: Date.now(),
-    reversion: 100,
-  },
-  data: {
-    '1': {
-      id: '1',
-      from: 'user-1',
-      type: 'text',
-      text: 'به یاد اولتر...',
-    },
-    '2': {
-      id: '2',
-      from: 'user-2',
-      type: 'text',
-      text:
-        'با تمام جهل و مستی تصمیم گرفته‌ام دفترچه رزوگار را با پاک کنِ مهر و عطوفت پاک کنم\n' +
-        'و از اول با نام تو روزگار را آغاز کنم.',
-    },
-    '3': {
-      id: '3',
-      from: 'user-1',
-      type: 'text',
-      text: 'هنوز در نخستین صفحات آن مانده‌ام و مطلبی برای نوشتن ندارم.تا پایان نوشتن انتظارت می‌کشم.',
-    },
-    '4': {
-      id: '4',
-      from: 'user-4',
-      type: 'text',
-      text: 'دیوانه مسلمانی که در روزهای انتظار هزار بار به دیوانگی‌اش ایمان می‌آورد….',
-    },
-    '5': {
-      id: '5',
-      from: 'user-1',
-      type: 'text',
-      text: 'کودک که بودم پدرم جمعه ها صبح درب خانه را آب و جارو می کرد تا  اگر مولا از آن حوالی عبور کند …',
-    },
-    '6': {
-      id: '6',
-      from: 'user-2',
-      type: 'text',
-      text: 'و نو روز هر باره این حس را در من زنده می کند',
-    },
-    '7': {
-      id: '7',
-      from: 'user-2',
-      type: 'text',
-      text: 'مردمی را می بینم که سراسر شوق و شورند ، خانه تکانی می کنند و لباس های نو برتن…',
-    },
-    '8': {
-      id: '8',
-      from: 'user-2',
-      type: 'text',
-      text: 'اما برای چه ؟\nاینان منتظرند تا  بهار شود ؟',
-    },
-    '9': {
-      id: '1',
-      from: 'user-1',
-      type: 'text',
-      text: 'برای که ؟',
-    },
-    '10': {
-      id: '4',
-      from: 'user-2',
-      type: 'text',
-      text: 'دیوانه مسلمانی که در روزهای انتظار هزار بار به دیوانگی‌اش ایمان می‌آورد….',
-    },
-    '11': {
-      id: '9',
-      from: 'user-4',
-      type: 'text',
-      text:
-        'سالهاست می اندیشم که هنگام بهار مگر چه می شود که اینگونه به هم می ریزیم' +
-        '،مهربان می شویم، به سراغ هم می رویم و از همه مهمتر منتظر می شویم…',
-    },
-    '12': {
-      id: '10',
-      from: 'user-4',
-      type: 'text',
-      text: 'انتظار...',
-    },
-    '13': {
-      id: '1',
-      from: 'user-1',
-      type: 'text',
-      text: 'زیبا بود مرسی',
-    },
-  },
-};
+export interface ChatContent extends StringifyableRecord {
+  chatStorage: AlwatrDocumentStorage<ChatMessage>;
+  currentUserId: string;
+}
 
 /**
  * Alwatr Demo Home Page
@@ -136,10 +44,28 @@ export class AlwatrChat extends SignalMixin(AlwatrBaseElement) {
     }
   `;
 
+  @property()
+    content?: ChatContent;
+
   override render(): unknown {
     this._logger.logMethod?.('render');
+    const content = this.content ?? {
+      chatStorage: {
+        ok: true,
+        meta: {
+          id: '',
+          formatVersion: 0,
+          reversion: 0,
+          lastAutoId: 1,
+          lastUpdated: 0,
+        },
+        data: {},
+      },
+      currentUserId: '',
+    };
+
     return html`
-      <alwatr-chat-list .storage=${chatStorage} .currentUser=${currentUser}></alwatr-chat-list>
+      <alwatr-chat-list .storage=${content.chatStorage} .currentUser=${content.currentUserId}></alwatr-chat-list>
       <alwatr-chat-footer></alwatr-chat-footer>
     `;
   }
