4333dfc17040bd6c11a7504e5c4a0a048dedde32
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-03-29 02:44:52 +0330

    lint: make eslint happy

--- labs/day-countdown-bot/src/index.ts
+++ labs/day-countdown-bot/src/index.ts
@@ -1,20 +1,19 @@
+import './bot/delete-admin-command.js';
+import './bot/delete-notify-command.js';
+import './bot/help-action.js';
+import './bot/menu-command.js';
+import './bot/notify-command.js';
+import './bot/rozshomar-command.js';
+import './bot/set-admin-command.js';
+import './bot/start-command.js';
+import './bot/subscribe-action.js';
+import './bot/unsubscribe-action.js';
 import {logger} from './config.js';
 import {sendDayCountdownToAllChat} from './dayCountdown.js';
+import './director/index.js';
 import {bot} from './lib/bot.js';
 import {nextDayCountdownDuration} from './lib/calender.js';
 import {launchBot} from './lib/launch.js';
-
-import './director/index.js';
-import './bot/start-command.js';
-import './bot/menu-command.js';
-import './bot/help-action.js';
-import './bot/subscribe-action.js';
-import './bot/notify-command.js';
-import './bot/set-admin-command.js';
-import './bot/delete-admin-command.js';
-import './bot/delete-notify-command.js';
-import './bot/unsubscribe-action.js';
-import './bot/rozshomar-command.js';
 
 logger.logOther('..:: Day Countdown Telegram Bot ::..');
 
