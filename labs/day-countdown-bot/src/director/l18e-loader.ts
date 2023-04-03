dab131f3c9d65e4a040b197debc0211dcbe09ee3
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-04-03 09:23:49 +0330

    chore(labs/dcd): create content folder

--- labs/day-countdown-bot/src/director/l18e-loader.ts
+++ labs/day-countdown-bot/src/director/l18e-loader.ts
@@ -1,7 +1,6 @@
 import type {L18eContext} from '@alwatr/type';
 
-export const i18r = (await import('../l18r/fa.json', {assert: {type: 'json'}}) as unknown as L18eContext).data;
-
+const i18r = (await import('../content/l18e-fa.json', {assert: {type: 'json'}}) as unknown as L18eContext).data;
 export function message(key: Lowercase<string>): string {
   return i18r[key] ?? '...';
 }
