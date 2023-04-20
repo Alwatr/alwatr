0e6eecc269990558d64175f1d0af5ed7c7db9a50
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-04-20 14:01:10 +0330

    feat(com-pwa/config): user key

--- uniquely/com-pwa/res/config.js
+++ uniquely/com-pwa/res/config.js
@@ -1,6 +1,7 @@
 window.appConfig = {
   api: 'https://order.soffit.co/api/v0',
   cdn: 'https://order.soffit.co/cdn/',
+  user: 'https://order.soffit.co/user/',
   token: '3584fd6c82b5b76568a7f770846be94af9b7a879eb2274f2c0121ccc58d112ee',
   productStorageList: ['tile'],
 };
