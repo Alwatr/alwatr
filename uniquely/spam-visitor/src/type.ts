5b967d66379dc6caf43fa7222d8ac5cb929c6896
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-04-21 14:38:51 +0330

    chore(spam-visitor): remove additional type

--- uniquely/spam-visitor/src/type.ts
+++ uniquely/spam-visitor/src/type.ts
@@ -3,8 +3,3 @@ import type {PuppeteerLaunchOptions} from 'puppeteer-core';
 export interface PuppeteerOption extends PuppeteerLaunchOptions {
   name: string;
 }
-
-export interface SessionInfo {
-  sessionUrl: string;
-  instanceDomainList: Array<string>;
-}
