09abab28f890fb9da791aa29f377753e77dd9cec
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-04-25 14:51:58 +0330

    fix(com-pwa): import type

--- uniquely/com-pwa/src/manager/context-provider/user.ts
+++ uniquely/com-pwa/src/manager/context-provider/user.ts
@@ -1,5 +1,6 @@
 import {contextProvider} from '@alwatr/context';
-import {ComUser} from '@alwatr/type/customer-order-management.js';
+
+import type {ComUser} from '@alwatr/type/customer-order-management.js';
 
 const user = localStorage.getItem('user-context');
 if (user != null) {
