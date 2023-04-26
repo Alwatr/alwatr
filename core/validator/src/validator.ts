d08e7bee06e939d53cde5998819c72054915d835
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-04-26 20:43:04 +0330

    refactor(validator): review

--- core/validator/src/validator.ts
+++ core/validator/src/validator.ts
@@ -141,16 +141,14 @@ export function validator<T extends StringifyableRecord>(
 }
 
 /**
- * Coverts an input to a valid and sanitized phone number
+ * Validate a phone number and return it in a standard format with country code.
  */
 export const sanitizePhoneNumber = (input?: string | number | null, countryCode = '98'): number | null => {
   if (input == null) return null;
 
-  if (typeof input === 'number') input = input + '';
-
-  input = input.replace(/[ )(-]+/g, '');
-  input = input.replace(new RegExp(`^(\\+${countryCode}|${countryCode}|\\+|0)`), '');
-  input = countryCode + input;
+  input =
+    countryCode +
+    (input + '').replace(/[ )(-]+/g, '').replace(new RegExp(`^(\\+${countryCode}|${countryCode}|\\+|0)`), '');
 
   const unicodeDigits = new UnicodeDigits('en');
   input = unicodeDigits.translate(input);
