6e854769700d72b675be69a628a1c0ad2df56e14
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-04-26 20:57:41 +0330

    chore(es-bench): test reg cost

--- demo/es-bench/reg-cost.ts
+++ demo/es-bench/reg-cost.ts
@@ -9,7 +9,18 @@ function test_2(input = 'ali'): string {
   return input.replace(regex, '');
 }
 
+function test_3(input = 'ali'): string {
+  return input.replace(new RegExp('[l i]', 'g'), '');
+}
+
+const regex2 = new RegExp('[l i]', 'g');
+function test_4(input = 'ali'): string {
+  return input.replace(regex2, '');
+}
+
 bench('test_1', test_1);
 bench('test_2', test_2);
+bench('test_3', test_3);
+bench('test_4', test_4);
 
 globalThis.document?.body.append(' Done. Check the console.');
