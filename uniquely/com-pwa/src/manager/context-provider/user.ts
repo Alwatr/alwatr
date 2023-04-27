d8bf03a194b1859b8f18e9dc292e344382e3d2da
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-04-27 16:40:45 +0330

    fix(com/user): localStorageSuffixKey

--- uniquely/com-pwa/src/manager/context-provider/user.ts
+++ uniquely/com-pwa/src/manager/context-provider/user.ts
@@ -3,28 +3,28 @@ import {getLocalStorageItem, setLocalStorageItem} from '@alwatr/util';
 
 import type {ComUser} from '@alwatr/type/customer-order-management.js';
 
+const localStorageSuffixKey = '_x1';
+
 // user profile
 export const userProfileContextConsumer = contextConsumer<ComUser>('user_profile');
-export const userProfileLocalStorageKey = userProfileContextConsumer.id + '_x1';
 
-const userProfile = getLocalStorageItem<ComUser | null>(userProfileLocalStorageKey, null);
+const userProfile = getLocalStorageItem<ComUser | null>(userProfileContextConsumer.id + localStorageSuffixKey, null);
 if (userProfile != null) {
   contextProvider.setValue<ComUser>(userProfileContextConsumer.id, userProfile);
 }
 userProfileContextConsumer.subscribe((userProfile) => {
-  setLocalStorageItem<ComUser>(userProfileLocalStorageKey, userProfile);
+  setLocalStorageItem<ComUser>(userProfileContextConsumer.id + localStorageSuffixKey, userProfile);
 });
 
 // user token
 export const userTokenContextConsumer = contextConsumer<string>('user_token');
-export const userTokenLocalStorageKey = userTokenContextConsumer + '_x1';
 
-const userToken = getLocalStorageItem(userTokenLocalStorageKey, null);
+const userToken = getLocalStorageItem(userTokenContextConsumer.id + localStorageSuffixKey, null);
 if (userToken != null) {
   contextProvider.setValue<string>(userTokenContextConsumer.id, userToken);
 }
 userTokenContextConsumer.subscribe((userToken) => {
-  setLocalStorageItem<string>(userTokenLocalStorageKey, userToken);
+  setLocalStorageItem<string>(userTokenContextConsumer.id + localStorageSuffixKey, userToken);
 });
 
 // link-pass token
