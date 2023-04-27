554a0046ac28ca43ccf79f37f6d101f93b64f7dd
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-04-27 14:37:28 +0330

    fix(com-pwa): get local storage for token

--- uniquely/com-pwa/src/manager/context-provider/user.ts
+++ uniquely/com-pwa/src/manager/context-provider/user.ts
@@ -3,18 +3,20 @@ import {getLocalStorageItem} from '@alwatr/util';
 
 import type {ComUser} from '@alwatr/type/customer-order-management.js';
 
-export const userProfileLocalStorageKey = 'user-profile-x1';
-export const userTokenLocalStorageKey = 'user-token-x1';
-
+// user profile
 export const userProfileContextConsumer = contextConsumer<ComUser>('user-profile');
-export const userTokenContextConsumer = contextConsumer<string>('user-token');
+export const userProfileLocalStorageKey = userProfileContextConsumer.id + '-x1';
 
 const userProfile = getLocalStorageItem<ComUser | null>(userProfileLocalStorageKey, null);
 if (userProfile != null) {
   contextProvider.setValue<ComUser>(userProfileContextConsumer.id, userProfile);
 }
 
-const userToken = getLocalStorageItem<string | null>(userTokenLocalStorageKey, null);
+// user token
+export const userTokenContextConsumer = contextConsumer<string>('user-token');
+export const userTokenLocalStorageKey = userTokenContextConsumer + '-x1';
+
+const userToken = localStorage.getItem(userTokenLocalStorageKey);
 if (userToken != null) {
   contextProvider.setValue<string>(userTokenContextConsumer.id, userToken);
 }
