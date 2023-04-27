9a809f4b5dfb499bbc8f9ff6bb7c6a039618a83b
Mohammad Honarvar <honarvar.info@gmail.com>
2023-04-27 15:40:21 +0430

    fix(com-pwa): rename `signInContextConsumer` to `signInServerContext`

--- uniquely/com-pwa/src/manager/context-provider/sign-in.ts
+++ uniquely/com-pwa/src/manager/context-provider/sign-in.ts
@@ -8,14 +8,14 @@ import {config} from '../../config.js';
 import type {AlwatrServiceResponseSuccessWithMeta} from '@alwatr/type';
 import type {ComUser} from '@alwatr/type/customer-order-management.js';
 
-export const signInContextConsumer = serverContextConsumer<AlwatrServiceResponseSuccessWithMeta<ComUser>>(
-    'user_storage_context',
+export const signInServerContext = serverContextConsumer<AlwatrServiceResponseSuccessWithMeta<ComUser>>(
+    'sign_in_storage_context',
     config.fetchContextOptions,
 );
 
-signInContextConsumer.subscribe(() => {
-  if (signInContextConsumer.getState().target === 'complete') {
-    const user = signInContextConsumer.getResponse()?.data;
+signInServerContext.subscribe(() => {
+  if (signInServerContext.getState().target === 'complete') {
+    const user = signInServerContext.getResponse()?.data;
     if (user != null) {
       localStorage.setItem(userProfileLocalStorageKey, JSON.stringify(user));
       localStorage.removeItem('link-pass');
