7b534876ada337c4137eda64c8f569c3ce1e426a
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-04-29 16:24:47 +0330

    feat(token/user): AlwatrUserFactory

--- core/token/src/user.ts
+++ core/token/src/user.ts
@@ -1,36 +1,48 @@
 import {createLogger} from '@alwatr/logger';
+import {random} from '@alwatr/math';
+import {generateHash} from '@alwatr/math/crypto.js';
+import {User} from '@alwatr/type';
+
+import {AlwatrTokenGenerator} from './token.js';
 
 import type {UserFactoryConfig} from './type.js';
-import { AlwatrTokenGenerator } from './token.js';
 
 export class AlwatrUserFactory {
   protected _logger = createLogger('alwatr-user-factory');
 
-  private _$passwordTokengenerator = new AlwatrTokenGenerator(this.config)
-  private _$userTokengenerator = new AlwatrTokenGenerator({
-    algorithm: 'sha224',
-    duration: '6m',
-    encoding: 'base64url',
-    secret: ''
-  })
+  private _$tokenGenerator = new AlwatrTokenGenerator(this.config);
 
   constructor(public config: UserFactoryConfig) {
     this._logger.logMethodArgs?.('constructor', config);
   }
 
   generateId(): string {
-
-  }
-  verifyId(): boolean {
-
-  }
-
-  generateToken(): string {
-
-  }
-  verifyToken(): boolean {
-
+    const options = {
+      algorithm: 'sha1',
+      encoding: 'hex',
+      outputLength: 10,
+    } as const;
+    const part1 = generateHash(random.uuid, options);
+    const part2 = generateHash(part1, options);
+    return part1 + part2;
   }
 
+  verifyId(id: string): boolean {
+    const options = {
+      algorithm: 'sha1',
+      encoding: 'hex',
+      outputLength: 10,
+    } as const;
+    const part1 = id.substring(0, options.outputLength);
+    const part2 = id.substring(options.outputLength, options.outputLength * 2);
+    return part2 === generateHash(part1, options);
+  }
 
+  generateToken(user: User): string {
+    return this._$tokenGenerator.generate(user.id + '-' + user.lpe);
+  }
+
+  verifyToken(user: User, token: string): boolean {
+    return this.generateToken(user) === token;
+  }
 }
