5c6b4db1b073b974a9942401b68786f81827f96b
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-04-29 19:03:49 +0330

    revert(type): crypto

--- core/type/src/crypto.ts
+++
@@ -1,3 +0,0 @@
-export type CryptoAlgorithm = 'md5' | 'sha1' | 'sha224' | 'sha256' | 'sha384' | 'sha512';
-
-export type CryptoEncoding = 'base64' | 'base64url' | 'hex' | 'binary';
--- core/type/src/index.ts
+++ core/type/src/index.ts
@@ -2,7 +2,6 @@ export * from './chat.js';
 export * from './service-response.js';
 export * from './storage.js';
 export * from './global.js';
-export * from './crypto.js';
 export * from './i18n.js';
 export * from './type-helper.js';
 export * from './event-signal.js';
