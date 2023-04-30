66ae2b7286bd8b422e31296f2c17fc477656ab29
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-04-30 20:58:01 +0330

    fix(demo/crypto): use all token status
    Co-authored-by: Mohammad Honarvar <honarvar.info@gmail.com>

--- demo/crypto/token.ts
+++ demo/crypto/token.ts
@@ -1,5 +1,6 @@
 import {AlwatrTokenGenerator} from '@alwatr/crypto';
 import {createLogger} from '@alwatr/logger';
+import {delay} from '@alwatr/util';
 
 const logger = createLogger('crypto/token', true);
 
@@ -10,8 +11,18 @@ const tokenGenerator = new AlwatrTokenGenerator({
   encoding: 'base64url',
 });
 
-const token = tokenGenerator.generate('userId');
+const userId = 'test_user';
+
+const token = tokenGenerator.generate(userId);
 logger.logOther?.('token:', token);
 
-const tokenValidationStatus = tokenGenerator.verify('userId', token);
-logger.logOther?.('token validation status:', tokenValidationStatus);
+const userTokenValidation = (): void => {
+  const tokenValidationStatus = tokenGenerator.verify(userId, token);
+  logger.logOther?.('user token validation status:', tokenValidationStatus);
+};
+
+userTokenValidation();
+await delay(2000);
+userTokenValidation();
+await delay(2000);
+userTokenValidation();
