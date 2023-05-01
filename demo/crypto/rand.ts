76d92647fbd4b79f969b9b169eba8ab3e139d445
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-05-01 14:52:09 +0330

    refactor(demo/crypto): hashGenerator config

--- demo/crypto/rand.ts
+++ demo/crypto/rand.ts
@@ -1,7 +1,8 @@
 import {AlwatrHashGenerator} from '@alwatr/crypto';
 
-const hashGenerator = new AlwatrHashGenerator();
+const hashGenerator = new AlwatrHashGenerator({algorithm: 'sha256', encoding: 'base64url', crcLength: 4});
 
 for (let i = 0; i <= 10; i++) {
   console.log('hash: %s', hashGenerator.randomSelfValidate());
 }
+
