dcc453675651fef9d4222d939088fcaa637255f4
Mohammad Honarvar <honarvar.info@gmail.com>
2023-05-08 00:06:44 +0330

    fix(com-api): use 'validateUserAuth' to check the user authentication & authorization

--- uniquely/com-api/src/route/patch-price-list.ts
+++ uniquely/com-api/src/route/patch-price-list.ts
@@ -1,12 +1,14 @@
 import {config, logger} from '../lib/config.js';
 import {nanoServer} from '../lib/server.js';
 import {storageClient} from '../lib/storage.js';
+import {validateUserAuth} from '../lib/validate-user-auth.js';
 
 import type {ProductPrice} from '@alwatr/type/customer-order-management.js';
 
 nanoServer.route('PATCH', '/price-list/', async (connection) => {
   logger.logMethod?.('patch-price-list');
-  connection.requireToken(config.nanoServer.adminToken);
+
+  await validateUserAuth(connection.getUserAuth());
   const params = connection.requireQueryParams<{name: string}>({name: 'string'});
   const bodyJson = await connection.requireJsonBody<{data: Array<ProductPrice>}>();
 
--- uniquely/com-api/src/route/patch-product-list.ts
+++ uniquely/com-api/src/route/patch-product-list.ts
@@ -1,12 +1,14 @@
 import {config, logger} from '../lib/config.js';
 import {nanoServer} from '../lib/server.js';
 import {storageClient} from '../lib/storage.js';
+import {validateUserAuth} from '../lib/validate-user-auth.js';
 
 import type {Product} from '@alwatr/type/customer-order-management.js';
 
 nanoServer.route('PATCH', '/product-list/', async (connection) => {
   logger.logMethod?.('patch-product-list');
-  connection.requireToken(config.nanoServer.adminToken);
+
+  await validateUserAuth(connection.getUserAuth());
   const params = connection.requireQueryParams<{storage: string}>({storage: 'string'});
   const bodyJson = await connection.requireJsonBody<{data: Array<Product>}>();
 
