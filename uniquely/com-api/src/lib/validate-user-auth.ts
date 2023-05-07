c3e17137a0747ac84b2e8a42bf75dc48229bffdf
Mohammad Honarvar <honarvar.info@gmail.com>
2023-05-07 23:52:30 +0330

    fix(com-api): use `validateUserAuth` when getting the list of users

---
+++ uniquely/com-api/src/lib/validate-user-auth.ts
@@ -0,0 +1,31 @@
+import {userFactory} from './crypto.js';
+import {userStorage} from './storage.js';
+
+import type {User, UserAuth} from '@alwatr/type';
+import type {UserPermission} from '@alwatr/type/src/customer-order-management.js';
+
+export const validateUserAuth = async (userAuth: UserAuth | null, permission?: UserPermission): Promise<User> => {
+  if (userAuth == null) {
+    throw {
+      ok: false,
+      statusCode: 401,
+      errorCode: 'authorization_required',
+    };
+  }
+
+  const error403 = {
+    ok: false,
+    statusCode: 403,
+    errorCode: 'access_denied',
+  } as const;
+
+  const user = await userStorage.get(userAuth.id);
+
+  if (user == null) throw error403;
+
+  if (!userFactory.verifyToken([user.id, user.lpe], userAuth.token)) throw error403;
+
+  if (permission && user.permissions?.includes(permission) !== true) throw error403;
+
+  return user;
+};
