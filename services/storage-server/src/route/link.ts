c4db8f4f0aabb7a83a6081776e992bc75a34f5f7
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-05-08 15:55:25 +0330

    fix(storage-server/link): resolve and security check paths

--- services/storage-server/src/route/link.ts
+++ services/storage-server/src/route/link.ts
@@ -1,5 +1,6 @@
 import {existsSync} from 'node:fs';
 import {symlink, rm} from 'node:fs/promises';
+import {resolve} from 'node:path';
 
 import {config, logger} from '../config.js';
 import {nanoServer} from '../lib/nano-server.js';
@@ -9,12 +10,35 @@ nanoServer.route('GET', '/link', async (connection) => {
 
   connection.requireToken(config.nanoServer.accessToken);
 
-  const params = connection.requireQueryParams<{src: string; dest: string}>({
+  let {src, dest} = connection.requireQueryParams<{src: string; dest: string}>({
     src: 'string',
     dest: 'string',
   });
 
-  makeLink(params.src, params.dest);
+  const base = resolve(config.storage.path);
+  src = resolve(config.storage.path, src + '.json');
+  dest = resolve(config.storage.path, dest + '.json');
+
+  if (!src.startsWith(base) || !dest.startsWith(base)) {
+    // Prevent to access outside storage path by '../' in address.
+    return {
+      ok: false,
+      statusCode: 403,
+      errorCode: 'path_outside_base',
+      meta: {base, src, dest},
+    };
+  }
+
+  if (!existsSync(src)) {
+    return {
+      ok: false,
+      statusCode: 404,
+      errorCode: 'src_not_found',
+      meta: {base, src, dest},
+    };
+  }
+
+  makeLinkForce(src, dest);
 
   return {
     ok: true,
@@ -22,17 +46,22 @@ nanoServer.route('GET', '/link', async (connection) => {
   };
 });
 
-const makeLink = async (src: string, dest: string): Promise<void> => {
+/**
+ * Make a symbolic link
+ *
+ * **CAUTION: the destination path will be removed if exists**
+ */
+const makeLinkForce = async (src: string, dest: string): Promise<void> => {
   logger.logMethodArgs?.('makeLink', {src, dest});
 
   try {
     if (existsSync(dest)) {
-      await rm(dest);
+      await rm(dest, {recursive: false, force: true});
     }
 
     await symlink(src, dest);
   }
   catch (error) {
-    logger.error('makeLink', 'makeLink_error', error);
+    logger.error('makeLink', 'symlink_failed', error);
   }
 };
