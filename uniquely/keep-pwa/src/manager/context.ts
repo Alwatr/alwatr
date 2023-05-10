523b32949ec0bbec3d824484056dcfdd5d03b608
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-05-10 15:46:30 +0330

    feat(keep-pwa): home page

--- uniquely/keep-pwa/src/manager/context.ts
+++ uniquely/keep-pwa/src/manager/context.ts
@@ -1,1 +1,13 @@
+import {contextConsumer, contextProvider, eventListener} from '@alwatr/signal';
+
+import type {PageHomeContent} from '../type.js';
+import type {ClickSignalType} from '@alwatr/type';
+
 export * from '@alwatr/pwa-helper/context.js';
+
+export const homePageContentContextProvider =
+  contextProvider.bind<PageHomeContent>('home-page-content-context');
+export const homePageContentContextConsumer =
+  contextConsumer.bind<PageHomeContent>(homePageContentContextProvider.id);
+
+export const languageButtonClickEventListener = eventListener.bind<ClickSignalType>('language-button-click-event');
--- uniquely/keep-pwa/src/ui/page/home.ts
+++ uniquely/keep-pwa/src/ui/page/home.ts
@@ -1,13 +1,21 @@
-
 import {
   customElement,
   css,
+  html,
+  unsafeHTML,
+  state,
   nothing,
   SignalMixin,
   AlwatrBaseElement,
+  mapIterable,
   UnresolvedMixin,
 } from '@alwatr/element';
+import {localeContextConsumer, message} from '@alwatr/i18n';
 import '@alwatr/ui-kit/card/icon-box.js';
+
+import {homePageContentContextConsumer, topAppBarContextProvider} from '../../manager/context.js';
+
+import type {BoxType, PageHomeContent} from '../../type.js';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -22,17 +30,60 @@ declare global {
 export class AlwatrPageHome extends UnresolvedMixin(SignalMixin(AlwatrBaseElement)) {
   static override styles = css`
     :host {
-      box-sizing: border-box;
-      min-height: 100%;
       display: flex;
       flex-wrap: wrap;
       padding: calc(2 * var(--sys-spacing-track));
+      justify-content: center;
       gap: var(--sys-spacing-track);
+      overflow-y: auto;
+    }
+
+    alwatr-icon-box {
+      width: 40%;
+      flex-grow: 1;
+    }
+
+    .logo {
+      width: 90%;
+      margin-bottom: var(--sys-spacing-track);
+    }
+
+    alwatr-icon-box[wide] {
+      width: 100%;
     }
   `;
 
+  @state() content?: PageHomeContent;
+
+  override connectedCallback(): void {
+    super.connectedCallback();
+
+    this._addSignalListeners(
+        homePageContentContextConsumer.subscribe((content) => {
+          this.content = content;
+          topAppBarContextProvider.setValue(content.topAppBar);
+        }),
+    );
+  }
+
   override render(): unknown {
     this._logger.logMethod?.('render');
-    return nothing;
+    return this._menuTemplate();
+  }
+
+  protected* _menuTemplate(): unknown {
+    yield html`<img class="logo" src="image/keep.svg" alt="Keep Company Logo" />`;
+    yield this._boxTemplate(this?.content?.about);
+    yield this._boxTemplate(this?.content?.product);
+    yield this._boxTemplate(this?.content?.catalogue);
+    yield mapIterable(this, this?.content?.socialList, this._boxTemplate, message('loading'));
+    if (localeContextConsumer.getValue()?.language === 'fa') {
+      yield this._boxTemplate(this?.content?.agency as BoxType);
+    }
+  }
+
+  protected _boxTemplate(box?: BoxType): unknown {
+    const slot = box?.slot == null ? nothing : unsafeHTML(box.slot);
+    return html`<alwatr-icon-box .content=${box} ?wide=${box?.wide}>${slot}</alwatr-icon-box>`;
   }
 }
