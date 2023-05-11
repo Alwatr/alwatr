139c040ae64bd3ab22e90ab9df13bb7ee5edb31f
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-05-11 16:12:15 +0330

    feat(wp): update all memory limits

--- cloud/classic/wordpress/php/fix-permition.sh
+++ cloud/classic/wordpress/php/fix-permition.sh
@@ -13,3 +13,6 @@ chown www-data:www-data -R $data
 find $data -type d -exec chmod 755 {} \;
 find $data -type f -exec chmod 644 {} \;
 ls -lahF $data
+
+chown www-data:www-data -R /var/cache
+ls -lahF /var/cache
