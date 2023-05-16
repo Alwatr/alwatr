163a122fe806383a3dc4dd86fd845a7f91c9ae5e
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-05-16 21:23:30 +0330

    fix(com-api): user list return data

--- uniquely/com-api/src/route/user-list.ts
+++ uniquely/com-api/src/route/user-list.ts
@@ -3,10 +3,9 @@ import {nanoServer} from '../lib/server.js';
 import {storageClient} from '../lib/storage.js';
 import {validateUserAuth} from '../lib/validate-user-auth.js';
 
-import type {AlwatrDocumentStorage} from '@alwatr/type';
 import type {ComUser} from '@alwatr/type/customer-order-management.js';
 
-nanoServer.route<AlwatrDocumentStorage<ComUser>>('GET', '/user-list', async (connection) => {
+nanoServer.route<Record<string, ComUser>>('GET', '/user-list', async (connection) => {
   logger.logMethod?.('get-user-list');
 
   await validateUserAuth(connection.getUserAuth(), 'user-list/read');
