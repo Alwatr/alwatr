46ad6caa9982521846dc86a4cf9524d5b7657129
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-05-18 12:04:29 +0330

    feat(util): flatStr

---
+++ core/util/src/flat-str.ts
@@ -0,0 +1,9 @@
+/**
+ * Flattens the underlying C structures of a concatenated JavaScript string.
+ */
+export const flatStr = (s: string): string => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  s | 0;
+  return s;
+};
