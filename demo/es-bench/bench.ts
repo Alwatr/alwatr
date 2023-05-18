cbe566cb42b44819b46a5d5cfc77675e8f6ef947
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-05-18 12:05:26 +0330

    feat(es-bench): enhance with optional count and warning

--- demo/es-bench/bench.ts
+++ demo/es-bench/bench.ts
@@ -1,13 +1,16 @@
-const count = 1_000_000;
-export const bench = (name: string, func: () => void): void => {
+if (process.env.NODE_ENV !== 'production' || typeof globalThis.gc !== 'function') {
+  console.warn('Please run node in production with `--expose-gc` for benchmark\nNODE_ENV=production node --expose-gc demo/...');
+}
+
+export const bench = (name: string, func: () => void, count = 1_000_000): void => {
   globalThis.gc?.();
   const startMemory = globalThis.process?.memoryUsage.rss() ?? 0;
   const startTime = performance.now();
-  for (let i = count; i; i--) {
+  while (count--) {
     func();
   }
   const duration = performance.now() - startTime;
-  const runPerSec = Math.round( count / duration * 1000);
+  const runPerSec = Math.ceil(count / duration * 1000);
   const memoryUsage = Math.round((globalThis.process?.memoryUsage.rss() ?? 0 - startMemory) / 10) / 100;
 
   console.log(`run ${name} ${runPerSec.toLocaleString()}/s with ${memoryUsage.toLocaleString()}kb`);
