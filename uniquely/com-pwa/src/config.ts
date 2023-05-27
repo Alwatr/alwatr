4e936d69696ef3778cc3337cb5faaec8215e4e4a
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2023-05-27 15:51:03 +0330

    fix(com-api): rollback config

--- uniquely/com-pwa/src/config.ts
+++ uniquely/com-pwa/src/config.ts
@@ -30,7 +30,7 @@ export const config = {
     newOrder: apiBaseUrl + 'order',
 
     // admin access
-    adminUserListIncOrder: apiBaseUrl + 'admin/user-list-inc-order',
+    userList: apiBaseUrl + 'user-list',
   },
 
   fetchContextOptions: <Partial<FetchOptions>>{
