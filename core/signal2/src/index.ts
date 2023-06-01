19a21249c308efea330d6bcc46989173f7a312c2
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-06-01 22:51:34 +0330

    fix(signal2): root exports and reg alwatr module

--- core/signal2/src/index.ts
+++ core/signal2/src/index.ts
@@ -1,3 +1,11 @@
-export * from './multithread-context.js';
-export * from './context.js';
+import {globalAlwatr} from '@alwatr/logger';
+
+export * from './simple-signal.js';
 export * from './event.js';
+export * from './context.js';
+export * from './multithread-context.js';
+
+globalAlwatr.registeredList.push({
+  name: '@alwatr/signal2',
+  version: _ALWATR_VERSION_,
+});
