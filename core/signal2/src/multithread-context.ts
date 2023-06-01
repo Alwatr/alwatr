7320764f25d89540fba44826ce64bf1894f2d875
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-06-01 22:50:41 +0330

    feat(signal2): new AlwatrMultithreadContextSignal extends AlwatrBaseSignal

--- core/signal2/src/multithread-context.ts
+++ core/signal2/src/multithread-context.ts
@@ -1,52 +1,52 @@
-import {createLogger, globalAlwatr} from '@alwatr/logger';
+import {createLogger} from '@alwatr/logger';
 
-import {AlwatrContext} from './context.js';
-
-globalAlwatr.registeredList.push({
-  name: '@alwatr/signal2/multithread-context',
-  version: _ALWATR_VERSION_,
-});
+import {AlwatrContextSignal} from './context.js';
 
 type AlwatrContextChangedMessage = {
-  type: 'alwatr_context_changed',
-  name: string,
-  payload: unknown,
-}
+  type: 'alwatr_context_changed';
+  name: string;
+  payload: unknown;
+};
 
 /**
  * Alwatr multithread context signal.
  */
-export class AlwatrMultithreadContext<TValue> extends AlwatrContext<TValue> {
-  protected static _worker: Worker;
-  protected static _registry: Record<string, AlwatrMultithreadContext<unknown>> = {};
+export class AlwatrMultithreadContextSignal<TValue> extends AlwatrContextSignal<TValue> {
+  protected static _logger = createLogger(`alwatr/mt-context`);
+  protected static _worker?: Worker;
+  protected static _registry: Record<string, AlwatrMultithreadContextSignal<unknown>> = {};
 
-  static setupChannel(worker?: Worker): void {
-    AlwatrMultithreadContext._worker = worker ?? self as unknown as Worker;
-    AlwatrMultithreadContext._worker.addEventListener('message', AlwatrMultithreadContext._onMessage);
+  static setupChannel(worker: Worker = self as unknown as Worker): void {
+    AlwatrMultithreadContextSignal._worker = worker;
+    worker.addEventListener('message', AlwatrMultithreadContextSignal._onMessage);
   }
 
   static _onMessage(event: MessageEvent): void {
     const message = event.data as AlwatrContextChangedMessage;
     if (message.type !== 'alwatr_context_changed') return;
-    const context = AlwatrMultithreadContext._registry[message.name];
+    AlwatrMultithreadContextSignal._logger.logMethodArgs?.('_onMessage', {message});
+    const context = AlwatrMultithreadContextSignal._registry[message.name];
     context._dispatch(message.payload);
   }
 
   static _postMessage(name: string, payload: unknown): void {
-    AlwatrMultithreadContext._worker.postMessage(<AlwatrContextChangedMessage>{
+    AlwatrMultithreadContextSignal._logger.logMethodArgs?.('_postMessage', {name, payload});
+    if (AlwatrMultithreadContextSignal._worker === undefined) {
+      throw new Error('worker_not_defined', {cause: 'setupChannel must be called before any setValue.'});
+    }
+    AlwatrMultithreadContextSignal._worker.postMessage(<AlwatrContextChangedMessage>{
       type: 'alwatr_context_changed',
       name,
       payload,
     });
   }
 
-  constructor(public override name: string) {
+  constructor(name: string) {
     super(name);
-    this._logger = createLogger(`{multithread-context/signal: ${name}}`);
-    if (AlwatrMultithreadContext._registry[name] !== undefined) {
+    if (AlwatrMultithreadContextSignal._registry[name] !== undefined) {
       throw new Error('context_name_exist');
     }
-    AlwatrMultithreadContext._registry[name] = this as AlwatrMultithreadContext<unknown>;
+    AlwatrMultithreadContextSignal._registry[name] = this as AlwatrMultithreadContextSignal<unknown>;
   }
 
   /**
@@ -54,6 +54,6 @@ export class AlwatrMultithreadContext<TValue> extends AlwatrContext<TValue> {
    */
   override setValue(value: TValue): void {
     super.setValue(value);
-    AlwatrMultithreadContext._postMessage(this.name, value);
+    AlwatrMultithreadContextSignal._postMessage(this.name, value);
   }
 }
