9312b65868c8add8a336b0e800519d05d1f003b3
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-06-03 18:14:38 +0330

    feat(demo/signal2): simple signal

--- demo/signal2/simple/index.ts
+++ demo/signal2/simple/index.ts
@@ -1,17 +1,32 @@
 import {AlwatrEventSignal, AlwatrSimpleSignal} from '@alwatr/signal2';
 
-const clickEvent = new AlwatrSimpleSignal('myClick');
+// button 1
+const clickEvent = new AlwatrSimpleSignal('button-1-click-event');
+document.getElementById('myButton1')?.addEventListener('click', () => clickEvent.dispatch());
+function clickEventSubscribeHandler(): void {
+  console.log('subscribe: a new signal received');
+}
+clickEvent.subscribe(clickEventSubscribeHandler);
 
-document.getElementById('myButton')?.addEventListener('click', () => clickEvent.dispatch());
 
-clickEvent.subscribe(() => {
-  console.log('subscribe: a new signal received ');
+// button 2
+const clickEvent2 = new AlwatrEventSignal('button-2-click-event');
+document.getElementById('myButton2')?.addEventListener('click', () => clickEvent2.dispatch({clickedBy: 'BTN_2'}));
+function clickEvent2SubscribeHandler(detail: unknown): void {
+  console.log('subscribe: a new signal received ', detail);
+}
+clickEvent2.subscribe(clickEvent2SubscribeHandler);
+
+
+// button 3
+document.getElementById('myButton3')?.addEventListener('click', () => {
+  clickEvent.subscribe(clickEventSubscribeHandler);
+  clickEvent2.subscribe(clickEvent2SubscribeHandler);
 });
 
-const clickEvent2 = new AlwatrEventSignal('myClick2');
 
-document.getElementById('myButton2')?.addEventListener('click', () => clickEvent2.dispatch({clickedBy: 'MHF'}));
-
-clickEvent2.subscribe((detail) => {
-  console.log('subscribe: a new signal received ', detail);
+// button 4
+document.getElementById('myButton4')?.addEventListener('click', () => {
+  clickEvent.unsubscribe(clickEventSubscribeHandler);
+  clickEvent2.unsubscribe(clickEvent2SubscribeHandler);
 });
