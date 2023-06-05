c9c51e2f29413a2e1e448ab108b8618e937455f0
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2023-06-05 14:34:03 +0330

    fix(signal2): index.ts

--- core/signal2/src/index.ts
+++ core/signal2/src/index.ts
@@ -4,7 +4,6 @@ export * from './simple-signal.js';
 export * from './event.js';
 export * from './context.js';
 export * from './multithread-context.js';
-export * from './base.js';
 
 globalAlwatr.registeredList.push({
   name: '@alwatr/signal2',
