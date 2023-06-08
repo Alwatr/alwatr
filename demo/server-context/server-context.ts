5092c9505ea433c7918fd3925439a085c489d4fa
Mohammad Honarvar <honarvar.info@gmail.com>
2023-06-08 13:22:07 +0330

    fix(demo): use `AlwatrApiRequest`

--- demo/server-context/server-context.ts
+++ demo/server-context/server-context.ts
@@ -1,6 +1,6 @@
-import {AlwatrServerRequest} from '@alwatr/server-context';
+import {AlwatrApiRequest} from '@alwatr/server-context';
 
-const serverRequest = new AlwatrServerRequest({
+const serverRequest = new AlwatrApiRequest({
   name: 'demo.server-request',
 });
 
