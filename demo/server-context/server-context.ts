d4c60e38f3ed03d9e488f3e7fbbf1a921e8fc7a5
Mohammad Honarvar <honarvar.info@gmail.com>
2023-06-08 13:10:06 +0330

    feat(demo): add `server-context` demo

---
+++ demo/server-context/server-context.ts
@@ -0,0 +1,13 @@
+import {AlwatrServerRequest} from '@alwatr/server-context';
+
+const serverRequest = new AlwatrServerRequest({
+  name: 'demo.server-request',
+});
+
+serverRequest.request({
+  url: 'https://canary.order.soffit.co/api/v1/publistore/hub/product-list/tile',
+  method: 'GET',
+});
+serverRequest.subscribe(function StateChanged() {
+  console.log('serverRequest ', {state: this.state, response: this.response});
+});
