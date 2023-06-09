aa5e83eece3e4564770470cd0cd3d3ed056b3fb9
Mohammad Honarvar <honarvar.info@gmail.com>
2023-06-09 22:09:41 +0330

    feat(demo): add `i18n`

---
+++ demo/i18n/i18n.ts
@@ -0,0 +1,30 @@
+import {l10n} from '@alwatr/i18n2';
+import {createLogger} from '@alwatr/logger';
+import {delay} from '@alwatr/util';
+const logger = createLogger('demo/l18n', true);
+
+l10n.subscribe(() => {
+  logger.logProperty?.('locale', l10n.locale);
+  logger.logProperty?.('hi', l10n.message('hi'));
+});
+
+l10n.setResourceLoader((locale) => {
+  return {
+    ok: true,
+    meta: {
+      code: locale.language === 'fa' ? 'fa-IR' : 'en-US',
+      rev: 2,
+    },
+    data: locale.language === 'fa' ? {
+      'hi': 'سلام',
+    } : {
+      'hi': 'hello',
+    },
+  };
+});
+
+l10n.setLocale('fa');
+
+await delay(500);
+
+l10n.setLocale('en');
