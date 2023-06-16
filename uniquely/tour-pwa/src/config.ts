cc23e812fece350ae0afdb66922e7527431c3d47
Mohammad Honarvar <honarvar.info@gmail.com>
2023-06-16 19:42:19 +0330

    fix(tour-pwa): provide `icon`s based on `icon2`

--- uniquely/tour-pwa/src/config.ts
+++ uniquely/tour-pwa/src/config.ts
@@ -1,37 +1,45 @@
 import {FetchOptions} from '@alwatr/fetch';
+import callIcon from '@alwatr/icon/svg/call-outline.svg';
 import {getConfKey} from '@alwatr/pwa-helper/config.js';
 import {NavigationBarContent} from '@alwatr/ui-kit/src/navigation-bar/navigation-bar.js';
 import {getLocalStorageItem} from '@alwatr/util';
+
+const defaultExport = <K extends string, T extends {default: K}>(m: Promise<T>): Promise<K> =>
+  m.then((_m) => _m.default);
+
+const starIcon = defaultExport(import('@alwatr/icon/svg/star-outline.svg'));
+const homeIcon = defaultExport(import('@alwatr/icon/svg/home-outline.svg'));
+const triangleIcon = defaultExport(import('@alwatr/icon/svg/triangle-outline.svg'));
 
 export const navigationBarData: NavigationBarContent = {
   itemList: [
     {
       id: 'unknown',
-      icon: 'star',
+      icon: starIcon,
       link: ['unknown'],
       label: 'مبهم',
     },
     {
       id: 'favorites',
-      icon: 'star',
+      icon: starIcon,
       link: ['favorites'],
       label: 'علاقه مندی ها',
     },
     {
       id: 'home',
-      icon: 'home',
+      icon: homeIcon,
       link: ['home'],
       label: 'خانه',
     },
     {
       id: 'tours',
-      icon: 'triangle',
+      icon: triangleIcon,
       link: ['tours'],
       label: 'تورها',
     },
     {
       id: 'call',
-      icon: 'call',
+      icon: callIcon,
       link: ['call'],
       label: 'تماس',
     },
