91c1b392252fbc407f533aad427a5d80b4d25749
Mohammad Honarvar <honarvar.info@gmail.com>
2023-06-20 21:42:32 +0330

    refactor(tour-pwa): storages

--- uniquely/tour-pwa/src/manager/tour-storage.ts
+++ uniquely/tour-pwa/src/manager/tour-storage.ts
@@ -1,19 +1,65 @@
-import {AlwatrServerRequest} from '@alwatr/server-context';
+import {router} from '@alwatr/router2';
+import {AlwatrApiRequest} from '@alwatr/server-context';
+import {AlwatrContextSignal} from '@alwatr/signal2';
 import {AlwatrDocumentStorage, StringifyableRecord} from '@alwatr/type';
+
+import {type PriceDetailItem, priceStorageRequest} from './price-storage.js';
+
+export interface Image extends StringifyableRecord {
+  url: string;
+  alt?: string;
+}
 
 export interface Tour extends StringifyableRecord {
   id: string;
   categories: Array<string>;
   title: string;
-  indexImageUrl: string;
-  imageUrlList?: Array<string>;
+  indexImage: Image;
+  imageList?: Array<Image>;
 }
-export type TourRecord = AlwatrDocumentStorage<Tour>
 
-export const tourServerContext = new AlwatrServerRequest({
-  name: 'tour-storage.server-request',
+export const tourStorageRequest = new AlwatrApiRequest<AlwatrDocumentStorage<Tour>>({
+  name: 'tour_storage.server_request',
 });
 
-tourServerContext.request({
+tourStorageRequest.request({
   url: '...',
+});
+
+export const tourDetailContext = new AlwatrContextSignal<Tour & {priceRecord: Record<string, Array<PriceDetailItem>>}>({
+  name: 'tour_detail_context',
+});
+
+router.subscribe(async () => {
+  if (router.route.sectionList[0] !== 'tour') return;
+
+  const tourId = router.route.sectionList[1];
+  if (!tourId) {
+    // FIXME:
+    return;
+  }
+
+  const tourStorage = tourStorageRequest.response?.data;
+  const priceStorage = priceStorageRequest.response?.data;
+
+  if (tourStorage == null || priceStorage == null) {
+    // FIXME:
+    return;
+  }
+
+  if (!Object.prototype.hasOwnProperty.call(tourStorage.data, tourId)) {
+    // FIXME:
+    return;
+  }
+
+  if (!Object.prototype.hasOwnProperty.call(priceStorage.data, tourId)) {
+    // FIXME:
+    return;
+  }
+
+  const tour = tourStorage.data[tourId];
+  tourDetailContext.setValue({
+    ...tour,
+    priceRecord: priceStorage.data[tourId].priceRecord,
+  });
 });
