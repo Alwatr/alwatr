4c0ec8ffb4608e30e28e08a212b1470366d23c8a
Mohammad Honarvar <honarvar.info@gmail.com>
2023-06-20 21:43:05 +0330

    fix(tour-pwa): structure of data

--- uniquely/tour-pwa/src/ui/page/tour-detail.ts
+++ uniquely/tour-pwa/src/ui/page/tour-detail.ts
@@ -3,8 +3,13 @@ import {
   css,
   html,
   AlwatrBaseElement,
+  property,
+  state,
 } from '@alwatr/element';
 import '@alwatr/ui-kit/card/icon-box.js';
+
+import {PriceDetailItem} from '../../manager/price-storage.js';
+import {tourDetailContext, type Tour} from '../../manager/tour-storage.js';
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -28,12 +33,38 @@ export class AlwatrPageTour extends AlwatrBaseElement {
     }
   `;
 
+  @property({type: Object}) content?: Tour & {priceRecord: Record<string, Array<PriceDetailItem>>};
+
+  @state() selectedCategory?: string;
+
   override connectedCallback(): void {
     super.connectedCallback();
+    tourDetailContext.subscribe((tour) => {
+      this.content = tour;
+      this.selectedCategory = tour.categories[0];
+    });
   }
 
   override render(): unknown {
     this._logger.logMethod?.('render');
-    return html`Tour Page...`;
+    return [this._renderCategoryList(), this._renderTourSummary()];
+    // return [this._renderCategoryList(), this._renderTourSummary(), priceListDirective()];
+  }
+
+  protected _renderCategoryList(): unknown {
+    return this.content?.categories.map((category) => html`${category}`);
+  }
+
+  protected _renderTourSummary(): unknown {
+    return html`
+      <div>
+        ${this.content?.title}
+        <img src=${this.content?.indexImage.url} alt=${this.content?.indexImage.alt ?? ''} />
+        <div>
+          ${this.content?.priceRecord[this.selectedCategory!][0].amount}
+          ${this.content?.priceRecord[this.selectedCategory!][0].date}
+        </div>
+      </div>
+    `;
   }
 }
