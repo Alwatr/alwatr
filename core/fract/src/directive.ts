899467c878a36a3dd92acc07dfb861ee9db400cf
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-06-22 14:30:32 +0330

    feat(fract): AlwatrDirective

---
+++ core/fract/src/directive.ts
@@ -0,0 +1,22 @@
+import {createLogger, globalAlwatr} from '@alwatr/logger';
+
+import {Directive, PartType, type PartInfo} from './lit.js';
+
+globalAlwatr.registeredList.push({
+  name: '@alwatr/fract',
+  version: _ALWATR_VERSION_,
+});
+
+export class AlwatrDirective extends Directive {
+  protected _logger = createLogger(this.constructor.name);
+
+  constructor(_partInfo: PartInfo) {
+    super(_partInfo);
+    this._logger.logMethodArgs?.('constructor', Object.keys(PartType)[_partInfo.type - 1]);
+  }
+
+  override update(_part: Part, props: Array<unknown>): unknown {
+    this._logger.logMethodArgs?.('update', props);
+    return this.render(...props);
+  }
+}
