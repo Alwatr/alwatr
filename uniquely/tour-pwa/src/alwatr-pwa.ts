6c249c9d862cdc915a17aa3eb88c1aabfa20d766
Mohammad Honarvar <honarvar.info@gmail.com>
2023-06-23 18:35:07 +0330

    fix(tour-pwa): create & use directives

--- uniquely/tour-pwa/src/alwatr-pwa.ts
+++ uniquely/tour-pwa/src/alwatr-pwa.ts
@@ -1,2 +1,13 @@
 // TODO: rename me to index
-import './ui/alwatr-pwa.js';
+import {render} from 'lit-html';
+
+import './alwatr-pwa.css';
+// import './ui/alwatr-pwa.js';
+import {alwatrPwaDirective} from './ui/alwatr-pwa-directive.js';
+
+const rootElement = document.getElementById('root');
+if (rootElement == null) {
+  throw new Error('Root element not found in the body');
+}
+
+render(alwatrPwaDirective(), rootElement);
