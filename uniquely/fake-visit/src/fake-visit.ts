40f3dcc5918804fc22fdef6cf5bd06b347c9b27a
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-06-24 01:21:55 +0330

    feat(fake-visit): scroll down to load all product

--- uniquely/fake-visit/src/fake-visit.ts
+++ uniquely/fake-visit/src/fake-visit.ts
@@ -12,8 +12,20 @@ export async function visit(searchUrl: string, productLinkSelector: string): Pro
   await openUrl(page, config.crawl.home);
   await openUrl(page, searchUrl);
 
-  await page.waitForSelector(productLinkSelector);
-  const productLink = await page.$(productLinkSelector);
+  let previousHeight;
+  let productLink;
+  let counter = 0;
+  do {
+    productLink = await page.$(productLinkSelector);
+    if (productLink != null) break;
+
+    previousHeight = await page.evaluate(() => document.body.scrollHeight);
+    await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight - 1000));
+    await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+    await delay(1000);
+    counter++;
+  } while (counter < 15 && previousHeight < await page.evaluate(() => document.body.scrollHeight));
+
   if (productLink == null) {
     throw new Error('product_link_not_found');
   }
