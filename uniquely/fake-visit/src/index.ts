13e834f542291c7087e8f00bca9b560dd6811499
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-06-24 02:41:32 +0330

    bugfix(fake-visit): move try catch into loop

--- uniquely/fake-visit/src/index.ts
+++ uniquely/fake-visit/src/index.ts
@@ -9,18 +9,18 @@ logger.logOther?.('..:: Alwatr Fake Visit Service ::..');
 (async (): Promise<void> => {
   // eslint-disable-next-line no-constant-condition
   while (true) {
-    try {
-      const searchUrlList = config.crawl.searchUrlList;
-      const productLinkSelectorList = config.crawl.productLinkSelectorList;
-      for (let i = 0; i < searchUrlList.length; i++) {
+    const searchUrlList = config.crawl.searchUrlList;
+    const productLinkSelectorList = config.crawl.productLinkSelectorList;
+    for (let i = 0; i < searchUrlList.length; i++) {
+      try {
         await cleanup(config.crawl.home);
         await visit(searchUrlList[i], productLinkSelectorList[i]);
         const count = await incrementCount();
         await notify(count);
       }
-    }
-    catch (error) {
-      logger.error?.('init', 'init_failed', error);
+      catch (error) {
+        logger.error?.('init', 'init_failed', error);
+      }
     }
   }
 })();
