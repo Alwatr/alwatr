85ff039b855eb7d38fef657b98a3c430a776ff76
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-06-24 17:44:01 +0330

    feat(demo-pwa): pages observe

--- ui/demo-pwa/src/icons.ts
+++ ui/demo-pwa/src/icons.ts
@@ -1,12 +1,12 @@
-import callIcon from '@alwatr/icon/svg/call-outline.svg';
+import call from '@alwatr/icon/svg/call-outline.svg';
 import {defaultExport} from '@alwatr/util';
 
 /**
  * Main app necessary icons.
  */
 export const icons = {
-  callIcon,
-  starIcon: defaultExport(import('@alwatr/icon/svg/star-outline.svg')),
-  homeIcon: defaultExport(import('@alwatr/icon/svg/home-outline.svg')),
-  triangleIcon: defaultExport(import('@alwatr/icon/svg/triangle-outline.svg')),
+  call,
+  star: defaultExport(import('@alwatr/icon/svg/star-outline.svg')),
+  home: defaultExport(import('@alwatr/icon/svg/home-outline.svg')),
+  triangle: defaultExport(import('@alwatr/icon/svg/triangle-outline.svg')),
 };
--- ui/demo-pwa/src/ui/app.ts
+++ ui/demo-pwa/src/ui/app.ts
@@ -1,5 +1,6 @@
 // TODO: rename me to index
-import {html, render} from '@alwatr/fract';
+import {alwatrObserve, html, render} from '@alwatr/fract';
+import {router} from '@alwatr/router2';
 import {alwatrNavigationBar} from '@alwatr/ui-kit/navigation-bar2/navigation-bar.js';
 import {alwatrTopAppBar} from '@alwatr/ui-kit/top-app-bar2/top-app-bar.js';
 
@@ -7,11 +8,21 @@ import './app.scss';
 import {icons} from '../icons.js';
 import {appLogger} from '../share/logger.js';
 
+import type {RouteContext} from '@alwatr/router';
+
 appLogger.logModule?.('app');
 
 const alwatrPwa = (): unknown => html`<div class="alwatr-pwa">
   ${alwatrTopAppBar({headline: 'Alwatr PWA Demo'})}
-  ${alwatrNavigationBar({itemList: [{icon: icons.callIcon}, {icon: icons.homeIcon}]})}
+  ${alwatrObserve(router, (route: RouteContext) => html`<h1>${route.pathname}</h1>`)}
+  ${alwatrNavigationBar({
+    itemList: [
+      {icon: icons.home, href: '/home'},
+      {icon: icons.star, href: '/favorites'},
+      {icon: icons.triangle, href: '/other'},
+      {icon: icons.call, href: '/contact'},
+    ],
+  })}
 </div>`;
 
 render(alwatrPwa(), document.body);
