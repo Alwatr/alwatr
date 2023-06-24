d11260f443e9d01c3ad0885a3b5ef46f466e058d
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-06-24 20:44:19 +0330

    feat(fract): export cache

--- core/fract/src/lit-html.ts
+++ core/fract/src/lit-html.ts
@@ -3,5 +3,6 @@ export {Directive, PartType, directive} from 'lit-html/directive.js';
 export {AsyncDirective} from 'lit-html/async-directive.js';
 export {unsafeSVG} from 'lit-html/directives/unsafe-svg.js';
 export {ifDefined} from 'lit-html/directives/if-defined.js';
+export {cache} from 'lit-html/directives/cache.js';
 
 export type {Part, PartInfo} from 'lit-html/directive.js';
