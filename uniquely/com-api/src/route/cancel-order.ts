2d79cf9e06a813b3983b7a9fd53d5b355f1b678a
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-07-10 18:29:23 +0330

    feat(com-api): add cancel route

---
+++ uniquely/com-api/src/route/cancel-order.ts
@@ -0,0 +1,31 @@
+import {logger} from '../lib/config.js';
+import {nanoServer} from '../lib/server.js';
+import {getUserOrder, patchUserOrder} from '../lib/user-order.js';
+import {validateUserAuth} from '../lib/validate-user-auth.js';
+
+import type {Order} from '@alwatr/type/customer-order-management.js';
+
+// Cancel order
+nanoServer.route('PUT', '/cancel-order', async (connection) => {
+  logger.logMethod?.('cancel-order');
+
+  const userAuth = await validateUserAuth(connection.getUserAuth());
+  const orderId = connection.requireQueryParams<{orderId: string}>({orderId: 'string'}).orderId;
+
+  const order = (await getUserOrder(userAuth.id)).data[orderId] as Order | null;
+  if (order == null) {
+    return {
+      ok: false,
+      statusCode: 400,
+      errorCode: 'order_not_found',
+    };
+  }
+
+  order.canceled = true;
+  await patchUserOrder(userAuth.id, order);
+
+  return {
+    ok: true,
+    data: {},
+  };
+});
