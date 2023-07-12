1606fb52650e6ca349a9bd08b106676e72887be3
Mohammad Honarvar <honarvar.info@gmail.com>
2023-07-12 21:40:05 +0330

    feat(tour-pwa): add `dest-card-list` page

---
+++ uniquely/tour-pwa/src/ui/page/dest-card-list.ts
@@ -0,0 +1,20 @@
+import {AlwatrDynamicDirective, directive, html, type PartInfo} from '@alwatr/fract';
+
+import {alwatrNavigationBar} from '../stuff/navigation-bar/navigation-bar.js';
+import {alwatrTopAppBar} from '../stuff/top-app-bar/top-app-bar.js';
+import {alwatrTourCard} from '../stuff/tour-card/tour-card.js';
+
+class AlwatrDestCardList extends AlwatrDynamicDirective {
+  protected _content?: unknown;
+  protected _selectedCategory?: string;
+
+  constructor(partInfo: PartInfo) {
+    super(partInfo, '<alwatr-tour-dest-card-list>');
+  }
+
+  override render(): unknown {
+    return html`${alwatrTopAppBar()}${alwatrTourCard()}${alwatrNavigationBar()}`;
+  }
+}
+
+export const alwatrDestCardList = directive(AlwatrDestCardList);
