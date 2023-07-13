5b3da2279e5f8211cdf800d3ee56d0e72e111bbc
Mohammad Honarvar <honarvar.info@gmail.com>
2023-07-13 20:14:50 +0330

    feat(fract): export `classMap`

--- core/fract/src/lit-html.ts
+++ core/fract/src/lit-html.ts
@@ -4,6 +4,7 @@ export {AsyncDirective} from 'lit-html/async-directive.js';
 export {unsafeSVG} from 'lit-html/directives/unsafe-svg.js';
 export {ifDefined} from 'lit-html/directives/if-defined.js';
 export {cache} from 'lit-html/directives/cache.js';
+export {classMap} from 'lit-html/directives/class-map.js';
 
 export type {Part, PartInfo} from 'lit-html/directive.js';
 export type {LitUnstable} from 'lit-html';
