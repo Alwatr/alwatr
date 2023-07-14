d4d49c66fb81876231a46fddbd7f9151289ca568
Mohammad Honarvar <honarvar.info@gmail.com>
2023-07-14 19:38:41 +0330

    fix(tour-pwa): issues of build

--- uniquely/tour-pwa/src/ui/page/tour-detail.ts
+++ uniquely/tour-pwa/src/ui/page/tour-detail.ts
@@ -1,1 +1,40 @@
-export {};
+import {AlwatrDynamicDirective, directive, html, type PartInfo} from '@alwatr/fract';
+
+import type {Tour} from '../../manager/tour-storage.js';
+
+class AlwatrTourDetail extends AlwatrDynamicDirective {
+  protected _content?: Tour;
+  protected _selectedCategory?: string;
+
+  constructor(partInfo: PartInfo) {
+    super(partInfo, '<alwatr-tour-detail>');
+  }
+
+  override render(): unknown {
+    return this._render_loading();
+  }
+
+  protected _render_loading(): unknown {
+    this._logger.logMethod?.('_render_loading');
+    return html`<p>Loading tour detail...</p>`;
+  }
+
+  protected _renderCategoryList(): unknown {
+    return this._content?.categories.map((category) => html`${category}`);
+  }
+
+  protected _renderTourSummary(): unknown {
+    return html`
+      <div>
+        ${this._content?.title}
+        <img src=${this._content?.indexImage.url} alt=${this._content?.indexImage.alt ?? ''} />
+        <div>
+          ${this._content?.priceList[this._selectedCategory!][0].amount}
+          ${this._content?.priceList[this._selectedCategory!][0].date}
+        </div>
+      </div>
+    `;
+  }
+}
+
+export const alwatrTourDetail = directive(AlwatrTourDetail);
