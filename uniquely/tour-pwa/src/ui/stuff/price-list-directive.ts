7b2134d8658158984b24a00bdd7fe55764c66a1b
Mohammad Honarvar <honarvar.info@gmail.com>
2023-07-14 19:37:53 +0330

    fix(tour-pwa): issues of build

--- uniquely/tour-pwa/src/ui/stuff/price-list-directive.ts
+++ uniquely/tour-pwa/src/ui/stuff/price-list-directive.ts
@@ -1,12 +1,12 @@
 import {css, html} from '@alwatr/element';
 
-import type {PriceDetailItem} from '../../manager/price-storage.js';
+import type {PriceDetailItem} from '../../manager/tour-storage.js';
 
 export const alwatrIconStyle = css``;
 
 export const alwatrTourPriceListDirective = (priceItem: PriceDetailItem): unknown => html`
   <div class="tour-item-container">
     <span>${priceItem.date}</span>
-    <span>${priceItem.amount}</span>
+    <span>${priceItem.price}</span>
   </div>
 `;
