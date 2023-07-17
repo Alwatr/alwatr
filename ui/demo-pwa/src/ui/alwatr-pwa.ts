41048e46aa771cbe88b98605d8edfd32bffd235a
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-07-17 14:50:59 +0330

    feat(demo-pwa): add pwa

--- ui/demo-pwa/src/ui/alwatr-pwa.ts
+++ ui/demo-pwa/src/ui/alwatr-pwa.ts
@@ -1,56 +1,57 @@
-// TODO: rename me to index
-import {AlwatrDynamicDirective, directive, html, type PartInfo} from '@alwatr/fract';
+import {AlwatrDynamicDirective, alwatrObserve, cache, directive, html, type PartInfo} from '@alwatr/fract';
+import {router, type RouteContext} from '@alwatr/router2';
+import {alwatrNavigationBar} from '@alwatr/ui-kit2/navigation-bar/navigation-bar.js';
+import {alwatrTopAppBar} from '@alwatr/ui-kit2/top-app-bar/top-app-bar.js';
+import {renderState} from '@alwatr/util';
 
-import {alwatrButton} from './button.js';
+import {alwatrPageTest} from './page-test.js';
+import {icons} from '../icons.js';
 import {appLogger} from '../share/logger.js';
 
 export type PageName = 'home' | 'favorites' | 'contact' | 'other' | '_404';
 
-appLogger.logModule?.('app');
+appLogger.logModule?.('alwatr-pwa');
 
-// function* renderApp(): unknown {
-//   yield html`${alwatrTopAppBar({
-//     headline: 'Alwatr PWA Demo',
-//   })}`;
-
-//   const main = alwatrObserve(router, (route: RouteContext) => {
-//     const page = <PageName>route.sectionList[0] ?? 'home';
-//     return cache(renderState(page, {
-//       home: () => html`<h1>home2...</h1>`,
-//       favorites: () => html`${alwatrPageTest(page)}`,
-//       other: () => html`${alwatrPageTest(page)}`,
-//       contact: () => html`<h1>call...</h1>`,
-//       _404: () => html`<h1>404!</h1>`,
-//       _default: '_404',
-//     }));
-//   });
-
-//   yield html`<main class="scroll-area">${main}</main>`;
-
-//   yield html`${alwatrNavigationBar({
-//     itemList: [
-//       {icon: icons.home, href: '/home'},
-//       {icon: icons.star, href: '/favorites'},
-//       {icon: icons.triangle, href: '/other'},
-//       {icon: icons.call, href: '/contact'},
-//     ],
-//   })}`;
-// }
+const menuContext = {
+  pageAction: {
+    itemList: [
+      {icon: icons.home, href: '/home'},
+      {icon: icons.star, href: '/favorites'},
+      {icon: icons.triangle, href: '/other'},
+      {icon: icons.call, href: '/contact'},
+    ],
+  },
+};
 
 export class AlwatrPwaDirective extends AlwatrDynamicDirective {
   constructor(partInfo: PartInfo) {
     super(partInfo, '<alwatr-pwa>');
   }
 
-  render(): unknown {
+  * render(): unknown {
     this._logger.logMethod?.('render');
-    return html`
-      <div class="p-10">
-        <h1 class="text-titleMedium text-center">سلام ...</h1>
-        ${alwatrButton({label: 'کلید ۱', extendClass: 'text-red-50'})}
-        ${alwatrButton({label: 'پریز ۲', disabled: true})}
-      </div>
-    `;
+
+    yield html`${alwatrTopAppBar({
+      headline: 'Alwatr PWA Demo',
+    })}`;
+
+    yield html`<main class="scroll-area">${this._renderContent()}</main>`;
+
+    yield html`${alwatrNavigationBar(menuContext.pageAction)}`;
+  }
+
+  protected _renderContent(): unknown {
+    return alwatrObserve(router, (route: RouteContext) => {
+      const page = <PageName>route.sectionList[0] ?? 'home';
+      return cache(renderState(page, {
+        home: () => html`<h1>home2...</h1>`,
+        favorites: () => html`${alwatrPageTest(page)}`,
+        other: () => html`${alwatrPageTest(page)}`,
+        contact: () => html`<h1>call...</h1>`,
+        _404: () => html`<h1>404!</h1>`,
+        _default: '_404',
+      }));
+    });
   }
 }
 
