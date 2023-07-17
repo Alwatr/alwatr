7d8d75557d927d045889a3463562e50093edbbdc
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-07-17 13:09:42 +0330

    feat(style): update material 3 colors

--- ui/style/lib/colors.ts
+++ ui/style/lib/colors.ts
@@ -127,17 +127,21 @@ export const colorTheme: Config['theme'] = {
     primaryLight: refColors.refPrimary[40],
     primaryDark: refColors.refPrimary[80],
 
-    onPrimary: 'rgba(var(--sys-color-on-primary), <alpha-value>)',
-    onPrimaryLight: refColors.refPrimary[100],
-    onPrimaryDark: refColors.refPrimary[20],
-
     primaryContainer: 'rgba(var(--sys-color-primary-container), <alpha-value>)',
     primaryContainerLight: refColors.refPrimary[90],
     primaryContainerDark: refColors.refPrimary[30],
 
+    onPrimary: 'rgba(var(--sys-color-on-primary), <alpha-value>)',
+    onPrimaryLight: refColors.refPrimary[100],
+    onPrimaryDark: refColors.refPrimary[20],
+
     onPrimaryContainer: 'rgba(var(--sys-color-on-primary-container), <alpha-value>)',
     onPrimaryContainerLight: refColors.refPrimary[10],
     onPrimaryContainerDark: refColors.refPrimary[90],
+
+    inversePrimary: 'rgba(var(--sys-color-inverse-primary), <alpha-value>)',
+    inversePrimaryLight: refColors.refPrimary[80],
+    inversePrimaryDark: refColors.refPrimary[40],
 
     /**
      * Secondary colors
@@ -146,13 +150,13 @@ export const colorTheme: Config['theme'] = {
     secondaryLight: refColors.refSecondary[40],
     secondaryDark: refColors.refSecondary[80],
 
-    onSecondary: 'rgba(var(--sys-color-on-secondary), <alpha-value>)',
-    onSecondaryLight: refColors.refSecondary[100],
-    onSecondaryDark: refColors.refSecondary[20],
-
     secondaryContainer: 'rgba(var(--sys-color-secondary-container), <alpha-value>)',
     secondaryContainerLight: refColors.refSecondary[90],
     secondaryContainerDark: refColors.refSecondary[30],
+
+    onSecondary: 'rgba(var(--sys-color-on-secondary), <alpha-value>)',
+    onSecondaryLight: refColors.refSecondary[100],
+    onSecondaryDark: refColors.refSecondary[20],
 
     onSecondaryContainer: 'rgba(var(--sys-color-on-secondary-container), <alpha-value>)',
     onSecondaryContainerLight: refColors.refSecondary[10],
@@ -165,64 +169,91 @@ export const colorTheme: Config['theme'] = {
     tertiaryLight: refColors.refTertiary[40],
     tertiaryDark: refColors.refTertiary[80],
 
-    onTertiary: 'rgba(var(--sys-color-on-tertiary), <alpha-value>)',
-    onTertiaryLight: refColors.refTertiary[100],
-    onTertiaryDark: refColors.refTertiary[20],
-
     tertiaryContainer: 'rgba(var(--sys-color-tertiary-container), <alpha-value>)',
     tertiaryContainerLight: refColors.refTertiary[90],
     tertiaryContainerDark: refColors.refTertiary[30],
+
+    onTertiary: 'rgba(var(--sys-color-on-tertiary), <alpha-value>)',
+    onTertiaryLight: refColors.refTertiary[100],
+    onTertiaryDark: refColors.refTertiary[20],
 
     onTertiaryContainer: 'rgba(var(--sys-color-on-tertiary-container), <alpha-value>)',
     onTertiaryContainerLight: refColors.refTertiary[10],
     onTertiaryContainerDark: refColors.refTertiary[90],
 
     /**
-     * Background colors
+     * Surface colors
      */
     background: 'rgba(var(--sys-color-background), <alpha-value>)',
-    backgroundLight: refColors.refNeutral[99],
+    backgroundLight: refColors.refNeutral[98],
     backgroundDark: refColors.refNeutral[10],
+
+    surface: 'rgba(var(--sys-color-surface), <alpha-value>)',
+    surfaceLight: refColors.refNeutral[98],
+    surfaceDark: refColors.refNeutral[10],
+
+    surfaceDim: 'rgba(var(--sys-color-surface-dim), <alpha-value>)',
+    surfaceDimLight: refColors.refNeutral[90],
+    surfaceDimDark: refColors.refNeutral[10],
+
+    surfaceBright: 'rgba(var(--sys-color-surface-bright), <alpha-value>)',
+    surfaceBrightLight: refColors.refNeutral[98],
+    surfaceBrightDark: refColors.refNeutral[20],
+
+    surfaceContainerLowest: 'rgba(var(--sys-color-surface-container-lowest), <alpha-value>)',
+    surfaceContainerLowestLight: refColors.refNeutral[100],
+    surfaceContainerLowestDark: refColors.refNeutral[0],
+
+    surfaceContainerLow: 'rgba(var(--sys-color-surface-container-low), <alpha-value>)',
+    surfaceContainerLowLight: refColors.refNeutral[98],
+    surfaceContainerLowDark: refColors.refNeutral[10],
+
+    surfaceContainer: 'rgba(var(--sys-color-surface-container), <alpha-value>)',
+    surfaceContainerLight: refColors.refNeutral[95],
+    surfaceContainerDark: refColors.refNeutral[10],
+
+    surfaceContainerHigh: 'rgba(var(--sys-color-surface-container-high), <alpha-value>)',
+    surfaceContainerHighLight: refColors.refNeutral[90],
+    surfaceContainerHighDark: refColors.refNeutral[20],
+
+    surfaceContainerHighest: 'rgba(var(--sys-color-surface-container-highest), <alpha-value>)',
+    surfaceContainerHighestLight: refColors.refNeutral[90],
+    surfaceContainerHighestDark: refColors.refNeutral[20],
+
+    surfaceVariant: 'rgba(var(--sys-color-surface-variant), <alpha-value>)',
+    surfaceVariantLight: refColors.refNeutral[90],
+    surfaceVariantDark: refColors.refNeutral[30],
+
+    inverseSurface: 'rgba(var(--sys-color-inverse-surface), <alpha-value>)',
+    inverseSurfaceLight: refColors.refNeutral[20],
+    inverseSurfaceDark: refColors.refNeutral[90],
 
     onBackground: 'rgba(var(--sys-color-on-background), <alpha-value>)',
     onBackgroundLight: refColors.refNeutral[10],
     onBackgroundDark: refColors.refNeutral[90],
 
-    /**
-     * Surface colors
-     */
-    surface: 'rgba(var(--sys-color-surface), <alpha-value>)',
-    surfaceLight: refColors.refNeutral[99],
-    surfaceDark: refColors.refNeutral[10],
-
     onSurface: 'rgba(var(--sys-color-on-surface), <alpha-value>)',
     onSurfaceLight: refColors.refNeutral[10],
     onSurfaceDark: refColors.refNeutral[90],
-
-    /**
-     * Surface variant colors
-     */
-    surfaceVariant: 'rgba(var(--sys-color-surface-variant), <alpha-value>)',
-    surfaceVariantLight: refColors.refNeutral[90],
-    surfaceVariantDark: refColors.refNeutral[30],
 
     onSurfaceVariant: 'rgba(var(--sys-color-on-surface-variant), <alpha-value>)',
     onSurfaceVariantLight: refColors.refNeutral[30],
     onSurfaceVariantDark: refColors.refNeutral[80],
 
+    inverseOnSurface: 'rgba(var(--sys-color-inverse-on-surface), <alpha-value>)',
+    inverseOnSurfaceLight: refColors.refNeutral[95],
+    inverseOnSurfaceDark: refColors.refNeutral[20],
+
     /**
      * Outline colors
      */
     outline: 'rgba(var(--sys-color-outline), <alpha-value>)',
-    outlineLight: refColors.refNeutral[50],
-    outlineDark: refColors.refNeutral[60],
+    outlineLight: refColors.refNeutralVariant[50],
+    outlineDark: refColors.refNeutralVariant[60],
 
-    /**
-     * Outline variant colors
-     */
     outlineVariant: 'rgba(var(--sys-color-outline-variant), <alpha-value>)',
-    outlineVariantLight: refColors.refNeutral[80],
-    outlineVariantDark: refColors.refNeutral[30],
+    outlineVariantLight: refColors.refNeutralVariant[80],
+    outlineVariantDark: refColors.refNeutralVariant[30],
 
     /**
      * Error colors
@@ -231,32 +262,17 @@ export const colorTheme: Config['theme'] = {
     errorLight: refColors.refError[40],
     errorDark: refColors.refError[80],
 
-    onError: 'rgba(var(--sys-color-on-error), <alpha-value>)',
-    onErrorLight: refColors.refError[100],
-    onErrorDark: refColors.refError[20],
-
     errorContainer: 'rgba(var(--sys-color-error-container), <alpha-value>)',
     errorContainerLight: refColors.refError[90],
     errorContainerDark: refColors.refError[30],
 
+    onError: 'rgba(var(--sys-color-on-error), <alpha-value>)',
+    onErrorLight: refColors.refError[100],
+    onErrorDark: refColors.refError[20],
+
     onErrorContainer: 'rgba(var(--sys-color-on-error-container), <alpha-value>)',
     onErrorContainerLight: refColors.refError[10],
     onErrorContainerDark: refColors.refError[90],
-
-    /**
-     * Inverse colors
-     */
-    inversePrimary: 'rgba(var(--sys-color-inverse-primary), <alpha-value>)',
-    inversePrimaryLight: refColors.refPrimary[80],
-    inversePrimaryDark: refColors.refPrimary[40],
-
-    inverseSurface: 'rgba(var(--sys-color-inverse-surface), <alpha-value>)',
-    inverseSurfaceLight: refColors.refNeutral[20],
-    inverseSurfaceDark: refColors.refNeutral[90],
-
-    inverseOnSurface: 'rgba(var(--sys-color-inverse-on-surface), <alpha-value>)',
-    inverseOnSurfaceLight: refColors.refNeutral[95],
-    inverseOnSurfaceDark: refColors.refNeutral[25],
 
     /**
      * Other colors
