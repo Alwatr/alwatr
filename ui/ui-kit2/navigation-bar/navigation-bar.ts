6d17dc625881b585d2b1213220486db3ce016890
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-07-20 17:11:14 +0330

    refactor(ui-kit): review navigation bar

--- ui/ui-kit2/navigation-bar/navigation-bar.ts
+++ ui/ui-kit2/navigation-bar/navigation-bar.ts
@@ -6,7 +6,8 @@ import {
   nothing,
   directive,
   noChange,
-  mapObject,
+  classMap,
+  map,
   type Part,
   type PartInfo,
 } from '@alwatr/fract';
@@ -14,15 +15,17 @@ import {l10n} from '@alwatr/i18n2';
 
 import {alwatrIcon} from '../icon/icon.js';
 
-import type {MaybePromise, StringifyableRecord} from '@alwatr/type';
+import type {MaybePromise} from '@alwatr/type';
 
 export interface NavigationContext {
   currentActive: string;
-  navigationBar: Record<string, AlwatrNavigationBarItem>
+  navigationBar: AlwatrNavigationBarItem[]
 }
 
 export interface AlwatrNavigationBarItem {
+  id: string;
   icon: MaybePromise<string>;
+  selectedIcon: MaybePromise<string>;
   label?: string;
   labelKey?: string;
   href?: string;
@@ -35,41 +38,48 @@ export interface AlwatrNavigationBarOptions {
 
 export class AlwatrNavigationBarDirective extends AlwatrDynamicDirective {
   contextSignalName = 'navigation-context';
-  navigationContext?: Record<string, AlwatrNavigationBarItem>;
+  navigationContext?: NavigationContext;
 
   constructor(partInfo: PartInfo) {
     super(partInfo, '<alwatr-navigation-bar>');
-    // this.contextSignalName = options.contextSignalName;
   }
 
   override update(_part: Part, props: unknown[]): void {
     super.update(_part, props);
-    contextConsumer<StringifyableRecord>(this.contextSignalName).subscribe((detail) => {
-      this.navigationContext = detail.navigationBar as unknown as Record<string, AlwatrNavigationBarItem>;
-      this.setValue(this.render(this.navigationContext));
+    contextConsumer(this.contextSignalName).subscribe((detail) => {
+      this.navigationContext = detail as unknown as NavigationContext;
+      this.setValue(this.render(this.navigationContext.navigationBar));
     });
   }
 
-  // render(options?: AlwatrNavigationBarOptions): unknown {
-  render(options?: Record<string, AlwatrNavigationBarItem>): unknown {
+  render(options?: AlwatrNavigationBarItem[]): unknown {
     this._logger.logMethodArgs?.('render', options);
     if (options == null) return noChange;
 
     return html` <footer class="grow-0 shrink-0 bg-surfaceContainer elevation-2">
       <nav class="flex max-w-screen-medium mx-auto select-none bg-surfaceContainer h-20 items-stretch [&>*]:grow">
-        ${mapObject(options, (item) => this._render_navigation_item(item), this)}
+        ${this._render_navigation_item_list(options)}
       </nav>
     </footer>`;
   }
 
-  protected _render_navigation_item(item: AlwatrNavigationBarItem): unknown {
-    return html` <button aria-selected="false" class="group flex flex-col pt-3 items-center justify-start">
-      <div class="px-5 py-1 rounded-2xl w-6 h-6 text-onSurfaceVariant group-hover:bg-onSurface group-hover:bg-opacity-30
-            group-focus:bg-onSurface group-focus:bg-opacity-20 [[aria-selected=true]_&]:bg-onSurface">
-        ${alwatrIcon({svg: item.icon, flipIconInRtl: item.flipIconInRtl})}
+  protected _render_navigation_item_list(items: AlwatrNavigationBarItem[]): unknown {
+    return map(items, (item) => {
+      return this._render_navigation_item(item, item.id === this.navigationContext?.currentActive);
+    });
+  }
+
+  protected _render_navigation_item(item: AlwatrNavigationBarItem, selected?: boolean): unknown {
+    return html` <button class="group flex flex-col pt-3 items-center justify-start">
+      <div class="px-5 py-1 rounded-2xl text-onSurfaceVariant group-hover:bg-onSurface group-hover:bg-opacity-30
+        [&>.alwatr-icon]:block [&>.alwatr-icon]:w-6 [&>.alwatr-icon]:h-6 [&>.alwatr-icon]:text-onSurfaceVariant
+        [&>.alwatr-icon]:group-hover:text-onSecondaryContainer [&>.alwatr-icon]:group-focus:text-onSecondaryContainer
+        ${classMap({'bg-opacity-20 bg-onSurface': selected === true})}">
+        ${alwatrIcon({svg: selected ? item.selectedIcon : item.icon, flipIconInRtl: item.flipIconInRtl})}
       </div>
 
-      <div class="py-1 text-labelMedium text-onSurfaceVariant group-hover:text-onSurface group-focus:text-onSurface">
+      <div class="py-1 text-labelMedium text-onSurfaceVariant group-hover:text-onSurface
+      ${classMap({'text-onSurface': selected === true})}">
         ${this._render_label(item)}
       </div>
     </button>`;
