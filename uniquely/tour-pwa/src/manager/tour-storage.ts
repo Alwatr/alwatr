529e08bbb8e467e88848d2bde0157dd426858bf4
Mohammad Honarvar <honarvar.info@gmail.com>
2023-07-26 21:29:08 +0330

    fix(tour-pwa): types

--- uniquely/tour-pwa/src/manager/tour-storage.ts
+++ uniquely/tour-pwa/src/manager/tour-storage.ts
@@ -1,29 +1,25 @@
 import {AlwatrServerContext} from '@alwatr/server-context';
-import {AlwatrServiceResponse, StringifyableRecord} from '@alwatr/type';
 
 import {appLogger} from '../share/logger.js';
+
+import type {AlwatrServiceResponse, Photo, StringifyableRecord} from '@alwatr/type';
 
 export interface PriceDetailItem extends StringifyableRecord {
   amount: number;
   date: number;
 }
 
-export interface Image extends StringifyableRecord {
-  url: string;
-  alt?: string;
-}
-
 export interface Tour extends StringifyableRecord {
   id: string;
-  categories: Array<string>;
+  categories: string[];
   title: string;
-  indexImage: Image;
-  imageList?: Array<Image>;
+  indexImage: Photo;
+  imageList?: Photo[];
   /**
    * @description A record of categories with their prices
    * @example { 'economy': [{ amount: 18_000_000, date: 1687284459767 }, ... ]
    */
-  priceList: Record<string, Array<PriceDetailItem>>;
+  priceList: Record<string, PriceDetailItem[]>;
 }
 
 export const tourContext = new AlwatrServerContext<AlwatrServiceResponse<Record<string, Tour>>>({
