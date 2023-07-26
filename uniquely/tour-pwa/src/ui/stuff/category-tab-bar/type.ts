d3e47f2d973b38ce0cf87609f0f00ad6ff0cf3e2
Mohammad Honarvar <honarvar.info@gmail.com>
2023-07-26 21:26:01 +0330

    fix(tour-pwa): remove some components to use them form `uikit2`

--- uniquely/tour-pwa/src/ui/stuff/category-tab-bar/type.ts
+++
@@ -1,3 +0,0 @@
-export interface TabBarContent {
-  itemList: Array<Record<string, unknown>>; // FIXME: Use `tour-storage` when defined
-}
--- uniquely/tour-pwa/src/ui/stuff/menu/type.ts
+++
@@ -1,8 +0,0 @@
-import type {MaybePromise} from '@alwatr/type';
-
-export interface MenuItem {
-  id: string;
-  label: string;
-  icon?: MaybePromise<string>;
-  children: MenuItem[];
-}
