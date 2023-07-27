2ec596cbda734d36f6684c51ec02ba4cb0e94ab0
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-07-27 16:07:23 +0330

    lint(ui-kit2): make eslint happy

--- ui/ui-kit2/menu/menu.ts
+++ ui/ui-kit2/menu/menu.ts
@@ -23,10 +23,11 @@ export class AlwatrMenuDirective extends AlwatrDirective {
     }
 
     return html`<div
-        class="overflow-y-auto py-5 px-3 h-full bg-white border-r border-gray-200
-          dark:bg-gray-800 dark:border-gray-700"
-      >
-      <ul class="space-y-2">${map(content, (item) => html`<div class="menu-item">${item.label}</div>`)}</ul>
+      class="h-full overflow-y-auto border-r border-gray-200 bg-white px-3 py-5 dark:border-gray-700 dark:bg-gray-800"
+    >
+      <ul class="space-y-2">
+        ${map(content, (item) => html`<li class="menu-item">${item.label}</li>`)}
+      </ul>
     </div>`;
   }
 
@@ -35,8 +36,10 @@ export class AlwatrMenuDirective extends AlwatrDirective {
     return html`<li>
       <a
         href="#"
-        class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg
-          dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">...</a>
+        class="group flex items-center rounded-lg p-2 text-base font-normal
+          text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+        >...</a
+      >
     </li>`;
   }
 
@@ -45,33 +48,40 @@ export class AlwatrMenuDirective extends AlwatrDirective {
     return html`<li>
       <button
         type="button"
-        class="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition
-          duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+        class="group flex w-full items-center rounded-lg p-2 text-base font-normal text-gray-900
+          transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
         aria-controls="dropdown-pages"
-        data-collapse-toggle="dropdown-pages">
+        data-collapse-toggle="dropdown-pages"
+      >
         <!--icon of item-->
-        <span class="flex-1 ml-3 text-left whitespace-nowrap">...</span>
+        <span class="ml-3 flex-1 whitespace-nowrap text-left">...</span>
         <!--arrow icon-->
       </button>
-      <ul id="dropdown-pages" class="hidden py-2 space-y-2">
-        ${map(item.children, (_child) => html`<li>
-          <a
-            href="#"
-            class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg
-              transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-          >
-            ${_child.label}
-          </a>
-        </li>`, this)}
+      <ul id="dropdown-pages" class="hidden space-y-2 py-2">
+        ${map(
+      item.children,
+      (_child) =>
+        html`<li>
+              <a
+                href="#"
+                class="group flex w-full items-center rounded-lg p-2 pl-11 text-base font-normal
+              text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+              >
+                ${_child.label}
+              </a>
+            </li>`,
+      this,
+  )}
       </ul>
     </li>`;
   }
 
   renderFooter(footerData: Record<string, unknown>): unknown {
     this._logger.logMethodArgs?.('renderMenuItem', footerData);
-    return html`<div class="hidden absolute bottom-0 left-0 justify-center p-4 space-x-4 w-full lg:flex bg-white
-        dark:bg-gray-800 z-20 border-r border-gray-200 dark:border-gray-700"
-      >
+    return html`<div
+      class="absolute bottom-0 left-0 z-20 hidden w-full justify-center space-x-4 border-r border-gray-200
+        bg-white p-4 dark:border-gray-700 dark:bg-gray-800 lg:flex"
+    >
       ...
     </div>`;
   }
