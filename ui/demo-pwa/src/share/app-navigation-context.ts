6295b23e17f92c95bae7a279db8d80fdf6809fb1
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-07-27 19:12:24 +0330

    feat(demo-pwa): set app navigation default value

--- ui/demo-pwa/src/share/app-navigation-context.ts
+++ ui/demo-pwa/src/share/app-navigation-context.ts
@@ -1,5 +1,7 @@
 import {AlwatrContextSignal} from '@alwatr/signal2';
 
+import {icons} from '../icons.js';
+
 import type {AlwatrIconOptions} from '@alwatr/ui-kit2/icon/icon.js';
 import type {NavigationDrawerContent} from '@alwatr/ui-kit2/navigation-drawer/navigation-drawer.js';
 
@@ -30,3 +32,37 @@ export interface AppNavigationContext {
 }
 
 export const appNavigationContext = new AlwatrContextSignal<AppNavigationContext>({name: 'app-navigation-context'});
+
+appNavigationContext.setValue({
+  navigationRail: {
+    selected: 'given',
+    itemList: {
+      given: {label: 'دریافتی', icon: {svg: icons.menu}},
+      contact: {label: 'مخاطبین', icon: {svg: icons.person}},
+      send: {label: 'ارسال', icon: {svg: icons.send}},
+      archive: {label: 'آرشیو', icon: {svg: icons.archive}},
+      trash: {label: 'زباله', icon: {svg: icons.trash}},
+    },
+  },
+  navigationBar: {
+    selected: 'given',
+    itemList: {
+      given: {label: 'دریافتی', icon: {svg: icons.menu}},
+      contact: {label: 'مخاطبین', icon: {svg: icons.person}},
+      send: {label: 'ارسال', icon: {svg: icons.send}},
+      archive: {label: 'آرشیو', icon: {svg: icons.archive}},
+      trash: {label: 'زباله', icon: {svg: icons.trash}},
+    },
+  },
+  navigationDrawer: {
+    title: '',
+    selected: 'contact',
+    itemList: {
+      given: {label: 'دریافتی', icon: {svg: icons.menu}},
+      contact: {label: 'مخاطبین', icon: {svg: icons.person}},
+      send: {label: 'ارسال', icon: {svg: icons.send}},
+      archive: {label: 'آرشیو', icon: {svg: icons.archive}},
+      trash: {label: 'زباله', icon: {svg: icons.trash}},
+    },
+  },
+});
