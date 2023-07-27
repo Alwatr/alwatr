87db881ca81d4300cf3e927f0fdb27dffc29dbc6
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-07-27 15:29:09 +0330

    lint(eslint): make len

--- ui/demo-pwa/src/ui/alwatr-pwa.ts
+++ ui/demo-pwa/src/ui/alwatr-pwa.ts
@@ -1,5 +1,3 @@
-/* eslint-disable lit/attribute-value-entities */
-/* eslint-disable max-len */
 import {AlwatrDynamicDirective, alwatrObserve, cache, directive, html, type PartInfo} from '@alwatr/fract';
 import {router, type RouteContext} from '@alwatr/router2';
 import {alwatrIcon} from '@alwatr/ui-kit2/icon/icon.js';
@@ -40,7 +38,8 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
 
       <!-- App container -->
       <div
-        class="flex h-full max-h-full w-full max-w-full flex-col flex-nowrap items-stretch overflow-clip medium:ps-20 extended:ps-[22.5rem]"
+        class="flex h-full max-h-full w-full max-w-full flex-col flex-nowrap
+        items-stretch overflow-clip medium:ps-20 extended:ps-[22.5rem]"
       >
         <!-- Top app bar -->
         ${this._renderTopAppBar()}
@@ -57,14 +56,18 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
     return html`
       <footer class="shrink-0 grow-0 bg-surfaceContainer elevation-2">
         <nav
-          class="mx-auto flex h-20 max-w-screen-medium cursor-pointer select-none items-stretch text-labelMedium text-onSurfaceVariant"
+          class="mx-auto flex h-20 max-w-screen-medium cursor-pointer select-none
+          items-stretch text-labelMedium text-onSurfaceVariant"
         >
           <a
             aria-selected="false"
-            class="group flex grow flex-col items-center justify-start pt-3 hover:text-onSurface aria-selected:pointer-events-none"
+            class="group flex grow flex-col items-center justify-start pt-3
+            hover:text-onSurface aria-selected:pointer-events-none"
           >
             <div
-              class="rounded-2xl px-5 py-1 group-hover:stateHover-onSurfaceVariant group-active:stateActive-onSurfaceVariant group-aria-selected:bg-secondaryContainer group-aria-selected:text-onSecondaryContainer"
+              class="rounded-2xl px-5 py-1 group-hover:stateHover-onSurfaceVariant
+              group-active:stateActive-onSurfaceVariant group-aria-selected:bg-secondaryContainer
+              group-aria-selected:text-onSecondaryContainer"
             >
               <alwatr-icon name="menu-outline" class="block h-6 w-6"> </alwatr-icon>
             </div>
@@ -73,10 +76,13 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
 
           <a
             aria-selected="false"
-            class="group flex grow flex-col items-center justify-start pt-3 hover:text-onSurface aria-selected:pointer-events-none"
+            class="group flex grow flex-col items-center justify-start pt-3
+            hover:text-onSurface aria-selected:pointer-events-none"
           >
             <div
-              class="rounded-2xl px-5 py-1 group-hover:stateHover-onSurfaceVariant group-active:stateActive-onSurfaceVariant group-aria-selected:bg-secondaryContainer group-aria-selected:text-onSecondaryContainer"
+              class="rounded-2xl px-5 py-1 group-hover:stateHover-onSurfaceVariant
+              group-active:stateActive-onSurfaceVariant group-aria-selected:bg-secondaryContainer
+              group-aria-selected:text-onSecondaryContainer"
             >
               <alwatr-icon name="call-outline" class="block h-6 w-6"> </alwatr-icon>
             </div>
@@ -88,7 +94,9 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
             class="group flex grow flex-col items-center justify-start pt-3 aria-selected:pointer-events-none"
           >
             <div
-              class="rounded-2xl px-5 py-1 group-hover:stateHover-onSurfaceVariant group-active:stateActive-onSurfaceVariant group-aria-selected:bg-secondaryContainer group-aria-selected:text-onSecondaryContainer"
+              class="rounded-2xl px-5 py-1 group-hover:stateHover-onSurfaceVariant
+              group-active:stateActive-onSurfaceVariant group-aria-selected:bg-secondaryContainer
+              group-aria-selected:text-onSecondaryContainer"
             >
               <alwatr-icon name="create-outline" class="block h-6 w-6"> </alwatr-icon>
             </div>
@@ -97,10 +105,13 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
 
           <a
             aria-selected="false"
-            class="group flex grow flex-col items-center justify-start pt-3 hover:text-onSurface aria-selected:pointer-events-none"
+            class="group flex grow flex-col items-center justify-start pt-3
+            hover:text-onSurface aria-selected:pointer-events-none"
           >
             <div
-              class="rounded-2xl px-5 py-1 group-hover:stateHover-onSurfaceVariant group-active:stateActive-onSurfaceVariant group-aria-selected:bg-secondaryContainer group-aria-selected:text-onSecondaryContainer"
+              class="rounded-2xl px-5 py-1 group-hover:stateHover-onSurfaceVariant
+              group-active:stateActive-onSurfaceVariant group-aria-selected:bg-secondaryContainer
+              group-aria-selected:text-onSecondaryContainer"
             >
               <alwatr-icon name="mic-outline" class="block h-6 w-6"> </alwatr-icon>
             </div>
@@ -109,10 +120,13 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
 
           <a
             aria-selected="false"
-            class="group flex grow flex-col items-center justify-start pt-3 hover:text-onSurface aria-selected:pointer-events-none"
+            class="group flex grow flex-col items-center justify-start pt-3
+            hover:text-onSurface aria-selected:pointer-events-none"
           >
             <div
-              class="rounded-2xl px-5 py-1 group-hover:stateHover-onSurfaceVariant group-active:stateActive-onSurfaceVariant group-aria-selected:bg-secondaryContainer group-aria-selected:text-onSecondaryContainer"
+              class="rounded-2xl px-5 py-1 group-hover:stateHover-onSurfaceVariant
+              group-active:stateActive-onSurfaceVariant group-aria-selected:bg-secondaryContainer
+              group-aria-selected:text-onSecondaryContainer"
             >
               <alwatr-icon name="person-outline" class="block h-6 w-6"> </alwatr-icon>
             </div>
@@ -127,14 +141,19 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
     return html`
       <aside
         id="navigationDrawer"
-        class="fixed bottom-0 left-0 top-0 z-modal w-[22.5rem] translate-x-full transform-gpu overflow-clip rounded-e-2xl bg-surfaceContainerLow transition-transform duration-300 ease-in will-change-transform elevation-1 rtl:left-auto rtl:right-0 extended:translate-x-0 extended:rounded-none extended:transition-none extended:will-change-auto extended:elevation-0 [&.opened]:translate-x-0 [&.opened]:ease-out"
+        class="fixed bottom-0 left-0 top-0 z-modal w-[22.5rem] translate-x-full
+        transform-gpu overflow-clip rounded-e-2xl bg-surfaceContainerLow transition-transform
+        duration-300 ease-in will-change-transform elevation-1 rtl:left-auto rtl:right-0
+        extended:translate-x-0 extended:rounded-none extended:transition-none extended:will-change-auto
+        extended:elevation-0 [&.opened]:translate-x-0 [&.opened]:ease-out"
       >
         <nav class="flex h-full flex-col bg-surfaceContainerLow px-3 py-3 elevation-1">
           <h2 class="mx-6 py-7 text-titleSmall text-onSurfaceVariant">سربرگ</h2>
 
           <ul class="text-labelLarge text-onSurfaceVariant">
             <li
-              class="flex h-14 cursor-pointer select-none flex-nowrap items-center rounded-full bg-secondaryContainer px-3 text-onSecondaryContainer stateActive-secondaryContainer"
+              class="flex h-14 cursor-pointer select-none flex-nowrap items-center rounded-full
+              bg-secondaryContainer px-3 text-onSecondaryContainer stateActive-secondaryContainer"
             >
               <alwatr-icon name="menu-outline" class="mx-1 h-6 w-6"></alwatr-icon>
 
@@ -143,7 +162,9 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
             </li>
 
             <li
-              class="w-84 group flex h-14 cursor-pointer select-none flex-nowrap items-center rounded-full px-3 hover:bg-secondaryContainer hover:text-onSecondaryContainer hover:stateHover-onSecondaryContainer active:text-onSecondaryContainer active:stateActive-onSecondaryContainer"
+              class="w-84 group flex h-14 cursor-pointer select-none flex-nowrap items-center rounded-full
+              px-3 hover:bg-secondaryContainer hover:text-onSecondaryContainer hover:stateHover-onSecondaryContainer
+              active:text-onSecondaryContainer active:stateActive-onSecondaryContainer"
             >
               <alwatr-icon name="person-outline" class="mx-1 h-6 w-6"></alwatr-icon>
 
@@ -151,7 +172,9 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
             </li>
 
             <li
-              class="w-84 group flex h-14 cursor-pointer select-none flex-nowrap items-center rounded-full px-3 hover:bg-secondaryContainer hover:text-onSecondaryContainer hover:stateHover-onSecondaryContainer active:text-onSecondaryContainer active:stateActive-onSecondaryContainer"
+              class="w-84 group flex h-14 cursor-pointer select-none flex-nowrap items-center rounded-full px-3
+              hover:bg-secondaryContainer hover:text-onSecondaryContainer hover:stateHover-onSecondaryContainer
+              active:text-onSecondaryContainer active:stateActive-onSecondaryContainer"
             >
               <alwatr-icon name="create-outline" class="mx-1 h-6 w-6"></alwatr-icon>
 
@@ -159,7 +182,9 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
             </li>
 
             <li
-              class="w-84 group flex h-14 cursor-pointer select-none flex-nowrap items-center rounded-full px-3 hover:bg-secondaryContainer hover:text-onSecondaryContainer hover:stateHover-onSecondaryContainer active:text-onSecondaryContainer active:stateActive-onSecondaryContainer"
+              class="w-84 group flex h-14 cursor-pointer select-none flex-nowrap items-center rounded-full px-3
+              hover:bg-secondaryContainer hover:text-onSecondaryContainer hover:stateHover-onSecondaryContainer
+              active:text-onSecondaryContainer active:stateActive-onSecondaryContainer"
             >
               <alwatr-icon name="archive-outline" class="mx-1 h-6 w-6"></alwatr-icon>
 
@@ -167,7 +192,9 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
             </li>
 
             <li
-              class="w-84 group flex h-14 cursor-pointer select-none flex-nowrap items-center rounded-full px-3 hover:bg-secondaryContainer hover:text-onSecondaryContainer hover:stateHover-onSecondaryContainer active:text-onSecondaryContainer active:stateActive-onSecondaryContainer"
+              class="w-84 group flex h-14 cursor-pointer select-none flex-nowrap items-center rounded-full
+              px-3 hover:bg-secondaryContainer hover:text-onSecondaryContainer hover:stateHover-onSecondaryContainer
+              active:text-onSecondaryContainer active:stateActive-onSecondaryContainer"
             >
               <alwatr-icon name="trash-outline" class="mx-1 h-6 w-6"></alwatr-icon>
 
@@ -178,7 +205,8 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
       </aside>
       <div
         id="scrim"
-        class="pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-scrim bg-scrim opacity-0 transition-opacity will-change-[opacity] extended:hidden [&.opened]:pointer-events-auto [&.opened]:opacity-25"
+        class="pointer-events-none fixed bottom-0 left-0 right-0 top-0 z-scrim bg-scrim opacity-0
+        transition-opacity will-change-[opacity] extended:hidden [&.opened]:pointer-events-auto [&.opened]:opacity-25"
         onclick="navigationDrawer.classList.remove('opened');scrim.classList.remove('opened');"
       ></div>
     `;
@@ -187,13 +215,17 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
   protected _renderNavigationRail(): unknown {
     return html` <aside
       id="navigationRail"
-      class="fixed bottom-0 left-0 top-0 z-modal w-20 translate-x-full transform-gpu overflow-clip rounded-e-2xl bg-surfaceContainerLow transition-transform duration-300 ease-in will-change-transform elevation-1 rtl:left-auto rtl:right-0 medium:translate-x-0 medium:rounded-none medium:transition-none medium:will-change-auto medium:elevation-0 extended:hidden [&.opened]:translate-x-0 [&.opened]:ease-out"
+      class="fixed bottom-0 left-0 top-0 z-modal w-20 translate-x-full transform-gpu overflow-clip
+      rounded-e-2xl bg-surfaceContainerLow transition-transform duration-300 ease-in will-change-transform
+      elevation-1 rtl:left-auto rtl:right-0 medium:translate-x-0 medium:rounded-none medium:transition-none
+      medium:will-change-auto medium:elevation-0 extended:hidden [&.opened]:translate-x-0 [&.opened]:ease-out"
     >
       <nav class="flex h-full flex-col justify-around bg-surfaceContainerLow elevation-1">
         <ul class="flex flex-col gap-3 text-labelLarge text-onSurfaceVariant">
           <li class="w-84 group group mx-3 flex h-14 cursor-pointer select-none flex-col flex-nowrap items-center">
             <div
-              class="flex h-8 w-14 flex-col items-center justify-around rounded-2xl group-hover:bg-secondaryContainer group-hover:stateHover-onSecondaryContainer group-active:stateActive-onSecondaryContainer"
+              class="flex h-8 w-14 flex-col items-center justify-around rounded-2xl group-hover:bg-secondaryContainer
+              group-hover:stateHover-onSecondaryContainer group-active:stateActive-onSecondaryContainer"
             >
               <alwatr-icon
                 name="person-outline"
@@ -208,7 +240,8 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
 
           <li class="w-84 group group mx-3 flex h-14 cursor-pointer select-none flex-col flex-nowrap items-center">
             <div
-              class="flex h-8 w-14 flex-col items-center justify-around rounded-2xl group-hover:bg-secondaryContainer group-hover:stateHover-onSecondaryContainer group-active:stateActive-onSecondaryContainer"
+              class="flex h-8 w-14 flex-col items-center justify-around rounded-2xl group-hover:bg-secondaryContainer
+              group-hover:stateHover-onSecondaryContainer group-active:stateActive-onSecondaryContainer"
             >
               <alwatr-icon
                 name="send-outline"
@@ -223,7 +256,8 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
 
           <li class="w-84 group group mx-3 flex h-14 cursor-pointer select-none flex-col flex-nowrap items-center">
             <div
-              class="flex h-8 w-14 flex-col items-center justify-around rounded-2xl group-hover:bg-secondaryContainer group-hover:stateHover-onSecondaryContainer group-active:stateActive-onSecondaryContainer"
+              class="flex h-8 w-14 flex-col items-center justify-around rounded-2xl group-hover:bg-secondaryContainer
+              group-hover:stateHover-onSecondaryContainer group-active:stateActive-onSecondaryContainer"
             >
               <alwatr-icon
                 name="archive-outline"
@@ -238,7 +272,8 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
 
           <li class="w-84 group group mx-3 flex h-14 cursor-pointer select-none flex-col flex-nowrap items-center">
             <div
-              class="flex h-8 w-14 flex-col items-center justify-around rounded-2xl group-hover:bg-secondaryContainer group-hover:stateHover-onSecondaryContainer group-active:stateActive-onSecondaryContainer"
+              class="flex h-8 w-14 flex-col items-center justify-around rounded-2xl group-hover:bg-secondaryContainer
+              group-hover:stateHover-onSecondaryContainer group-active:stateActive-onSecondaryContainer"
             >
               <alwatr-icon
                 name="trash-outline"
@@ -272,13 +307,15 @@ export class AlwatrPwaDirective extends AlwatrDynamicDirective {
         </div>
 
         <button
-          class="inline-block cursor-pointer rounded-full p-3 text-onSurfaceVariant [&>.alwatr-icon]:h-6 [&>.alwatr-icon]:w-6"
+          class="inline-block cursor-pointer rounded-full p-3
+          text-onSurfaceVariant [&>.alwatr-icon]:h-6 [&>.alwatr-icon]:w-6"
         >
           ${alwatrIcon({svg: icons.refresh})}
         </button>
 
         <button
-          class="inline-block cursor-pointer rounded-full p-3 text-onSurfaceVariant [&>.alwatr-icon]:h-6 [&>.alwatr-icon]:w-6"
+          class="inline-block cursor-pointer rounded-full p-3
+          text-onSurfaceVariant [&>.alwatr-icon]:h-6 [&>.alwatr-icon]:w-6"
         >
           ${alwatrIcon({svg: icons.calendarNumber})}
         </button>
