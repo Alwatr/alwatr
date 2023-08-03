ff3039fa8c59d8a8262594fd550f55246dbd11a1
Mohammad Honarvar <honarvar.info@gmail.com>
2023-08-03 21:28:03 +0330

    fix(ui-kit2/icon-button): the render structure

--- ui/ui-kit2/icon-button/icon-button.ts
+++ ui/ui-kit2/icon-button/icon-button.ts
@@ -7,5 +7,4 @@ export interface IconButtonContent extends IconContent {
   disabled?: boolean;
 }
 
-export const iconButton = (content: IconButtonContent) => html`<button @click=${content.onClick} class="flex h-full max-h-full w-full max-w-full flex-col flex-nowrap items-stretch overflow-clip">
-  <div class="flex w-10 h-10 rounded-full bg-surfaceContainerHighest text-Primary items-center justify-center hover:stateHover-onSurface m-1 active:stateActive-onSurface">${icon(content)}</div></button>`;
+export const iconButton = (content: IconButtonContent) => html`<button @click=${content.onClick} class="flex w-10 h-10 rounded-full bg-surfaceContainerHighest text-Primary items-center justify-center hover:stateHover-onSurface m-1 active:stateActive-onSurface">${icon(content)}</button>`;
