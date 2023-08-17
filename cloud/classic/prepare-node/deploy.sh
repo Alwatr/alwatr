3e907df31b6fb13ceadae77e5bd14a59905122df
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-08-17 23:57:02 +0330

    fix(cloud/deploy): rename ssh-rsa

--- cloud/classic/prepare-node/deploy.sh
+++ cloud/classic/prepare-node/deploy.sh
@@ -61,8 +61,8 @@ function command_ssh() {
   echoStep "Setup ssh: add sshd_config"
   copyConfigFile ssh-banner /etc/ssh/banner
   copyConfigFile ssh-config /etc/ssh/sshd_config
-  copyConfigFile alwatr_rsa "~/.ssh/id_rsa"
-  copyConfigFile alwatr_rsa.pub "~/.ssh/id_rsa.pub"
+  copyConfigFile ssh-rsa "~/.ssh/id_rsa"
+  copyConfigFile ssh-rsa.pub "~/.ssh/id_rsa.pub"
 
   echoStep "Setup ssh: restart ssh and test"
 
