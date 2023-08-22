6e6856b093799ba730922f667f8e60300b23e518
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-08-22 19:13:12 +0330

    fix(nginx): force-domain bug

--- cloud/container/nginx/conf/entrypoint.d/30-cleanup-conf.sh
+++ cloud/container/nginx/conf/entrypoint.d/30-cleanup-conf.sh
@@ -5,7 +5,7 @@ set -eu
 # Remove some configs base on envs
 cleanup_confings() {
   if [ -z ${NGINX_FORCE_DOMAIN:-} ]; then
-    rm -fv /etc/nginx/conf.d/location.d/90-force-domain.conf
+    rm -fv /etc/nginx/conf.d/location.d/40-force-domain.conf
   fi
 }
 
