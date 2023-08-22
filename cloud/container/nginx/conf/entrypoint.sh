47594af6c5c00f60391a54849794fc378a2a14a5
Ali Mihandoost <ali.mihandoost@gmail.com>
2023-08-22 15:44:10 +0330

    chore(nginx): update entrypoints

--- cloud/container/nginx/conf/entrypoint.sh
+++ cloud/container/nginx/conf/entrypoint.sh
@@ -1,40 +1,49 @@
 #!/bin/sh
-# vim:sw=4:ts=4:et
 
 set -e
 
-if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
-    exec 3>&1
-else
-    exec 3>/dev/null
-fi
+
+entrypoint_log() {
+  if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+    echo "$@"
+  fi
+}
 
 entrypointDir=/etc/nginx/entrypoint.d/
 
-if [ "$1" = "nginx" -o "$1" = "nginx-debug" ]; then
-    if /usr/bin/find "$entrypointDir" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
-        echo >&3 "$0: $entrypointDir is not empty, will attempt to perform configuration"
+if [ "$1" = "nginx" ] || [ "$1" = "nginx-debug" ]; then
+  if /usr/bin/find "$entrypointDir" -mindepth 1 -maxdepth 1 -type f -print -quit 2>/dev/null | read v; then
+    entrypoint_log "$0: $entrypointDir is not empty, will attempt to perform configuration"
 
-        echo >&3 "$0: Looking for shell scripts in $entrypointDir"
-        find "$entrypointDir" -follow -type f -print | sort -V | while read -r f; do
-            case "$f" in
-                *.sh)
-                    if [ -x "$f" ]; then
-                        echo >&3 "$0: Launching $f";
-                        "$f"
-                    else
-                        # warn on shell scripts without exec bit
-                        echo >&3 "$0: Ignoring $f, not executable";
-                    fi
-                    ;;
-                *) echo >&3 "$0: Ignoring $f";;
-            esac
-        done
+    entrypoint_log "$0: Looking for shell scripts in $entrypointDir"
+    find "$entrypointDir" -follow -type f -print | sort -V | while read -r f; do
+      case "$f" in
+        *.envsh)
+          if [ -x "$f" ]; then
+            entrypoint_log "$0: Sourcing $f";
+            . "$f"
+          else
+            # warn on shell scripts without exec bit
+            entrypoint_log "$0: Ignoring $f, not executable";
+          fi
+          ;;
+        *.sh)
+          if [ -x "$f" ]; then
+            entrypoint_log "$0: Launching $f";
+            "$f"
+          else
+            # warn on shell scripts without exec bit
+            entrypoint_log "$0: Ignoring $f, not executable";
+          fi
+          ;;
+        *) entrypoint_log "$0: Ignoring $f";;
+      esac
+    done
 
-        echo >&3 "$0: Configuration complete; ready for start up"
-    else
-        echo >&3 "$0: No files found in $entrypointDir, skipping configuration"
-    fi
+    entrypoint_log "$0: Configuration complete; ready for start up"
+  else
+    entrypoint_log "$0: No files found in $entrypointDir, skipping configuration"
+  fi
 fi
 
 exec "$@"
