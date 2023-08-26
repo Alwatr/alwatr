1950a247727bb8aa0eb7c494a7c0e807eda48516
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-08-26 14:19:19 +0330

    feat(v2fly): automatically export vmess link

--- cloud/classic/v2fly/export-vmess-url.sh
+++ cloud/classic/v2fly/export-vmess-url.sh
@@ -7,6 +7,60 @@ thisPath="$(pwd)"
 cd $thisPath
 
 configPath="${1}"
+serverUrl="${2}"
+configName="${3}"
+outputPath="${4}"
 
-url=$(cat $configPath | base64)
-echo "vmess://${url}"
+function makeConfig() {
+  local url=${1}
+  local port=${2}
+  local protocol=${3}
+  local userId=${4}
+  local userName=${5}
+
+  cat <<EOF
+{
+  "add": "$url",
+  "aid": "0",
+  "alpn": "",
+  "fp": "",
+  "host": "",
+  "id": "$userId",
+  "net": "$protocol",
+  "path": "",
+  "port": "$port",
+  "ps": "$userName",
+  "scy": "zero",
+  "sni": "",
+  "tls": "",
+  "type": "none",
+  "v": "2"
+}
+EOF
+}
+
+function exportConfig() {
+  local index=0
+  serverPort=$(jq -r '.inbounds[0].settings.port' ${configPath})
+  local output=""
+  for user in $(jq -r '.inbounds[0].settings.users[]' ${configPath}); do
+    oldOutput=$output
+    userName="${configName}-u${index}"
+    config=$(makeConfig $serverUrl $serverPort $1 $user $userName)
+    url=$(echo $config | base64)
+    output="${oldOutput}\n${userName}\t${url}"
+    ((index++))
+  done
+
+  echo -e $output
+}
+
+
+function exportConfigToFile() {
+  output=$(exportConfig ws)
+  echo -e "ws\n${output}" >> $outputPath
+  output=$(exportConfig tcp)
+  echo -e "\n---------------------\n\ntcp\n${output}" >> $outputPath
+}
+
+exportConfigToFile
