70b0357b0accd6ed96c0cdc62099146b36ce2660
Ali Mihandoost <ali.mihandoost@gmail.com>
2024-10-28 13:35:14 +0330

    refactor(pre-handlers): Remove logger module

--- packages/pre-handlers/src/logger.ts
+++
@@ -1,5 +0,0 @@
-import {createLogger, packageTracer} from '@alwatr/nanolib';
-
-__dev_mode__: packageTracer.add(__package_name__, __package_version__);
-
-export const logger = /* #__PURE__ */  createLogger(__package_name__);
