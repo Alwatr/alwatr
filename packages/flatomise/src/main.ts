692e7ff5ddbc2efef16aaa2ecbeff90e55d63e2a
Ali Mihandoost <ali.mihandoost@gmail.com>
2025-02-18 19:29:27 +0330

    feat: enhance Flatomise interface with promise settlement tracking

--- packages/flatomise/src/main.ts
+++ packages/flatomise/src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {packageTracer} from '@alwatr/package-tracer';
 
 __dev_mode__: packageTracer.add(__package_name__, __package_version__);
@@ -6,10 +7,25 @@ __dev_mode__: packageTracer.add(__package_name__, __package_version__);
  * Flat promise that can be resolved or rejected from outside.
  */
 export interface Flatomise<T> {
+  /**
+   * The promise.
+   */
   promise: Promise<T>;
+
+  /**
+   * Resolve the promise.
+   */
   resolve: (value: T | PromiseLike<T>) => void;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+
+  /**
+   * Reject the promise.
+   */
   reject: (reason?: any) => void;
+
+  /**
+   * Whether the promise is settled (resolved or rejected).
+   */
+  settled: boolean;
 }
 
 /**
@@ -27,10 +43,13 @@ export interface Flatomise<T> {
  * ```
  */
 export function newFlatomise<T>(): Flatomise<T> {
-  const flatomise: Partial<Flatomise<T>> = {};
+  const flatomise: Partial<Flatomise<T>> = {settled: false};
   flatomise.promise = new Promise<T>((resolve, reject) => {
     flatomise.resolve = resolve;
     flatomise.reject = reject;
+  });
+  flatomise.promise.finally(() => {
+    flatomise.settled = true;
   });
   return flatomise as Flatomise<T>;
 }
