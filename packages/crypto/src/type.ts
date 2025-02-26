aa2720f6f7b128a572d8591dc7a75ac15ed6d2d6
Ali Mihandoost <ali.mihandoost@gmail.com>
2025-02-26 19:47:04 +0330

    feat(crypto): add 'ascii' and 'utf8' to CryptoEncoding type

--- packages/crypto/src/type.ts
+++ packages/crypto/src/type.ts
@@ -8,4 +8,4 @@ export type CryptoAlgorithm = 'md5' | 'sha1' | 'sha224' | 'sha256' | 'sha384' | 
  * Represents the encoding options for cryptographic operations.
  * The encoding can be one of the following: 'base64', 'base64url', 'hex', or 'binary'.
  */
-export type CryptoEncoding = 'base64' | 'base64url' | 'hex' | 'binary';
+export type CryptoEncoding = 'base64' | 'base64url' | 'hex' | 'ascii' | 'utf8' | 'binary';
