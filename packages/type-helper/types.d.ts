c4ac67a102c15a982c4b32822bd606239c32f05c
Ali Mihandoost <ali.mihandoost@gmail.com>
2025-09-06 22:43:32 +0330

    refactor: redefine all types as just global and remove exported each types
    BREAKING CHANGE: Import separate types not supported anymore, please just use global usage.

--- packages/type-helper/types.d.ts
+++ packages/type-helper/types.d.ts
@@ -1,44 +1,6 @@
-export type * from './types.d.ts';
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/consistent-indexed-object-style, @typescript-eslint/ban-types */
 
-export type * from './types.d.ts';
-
-import type {
-  Primitive as Primitive_,
-  Falsy as Falsy_,
-  Nullish as Nullish_,
-  Nullable as Nullable_,
-  Maybe as Maybe_,
-  MaybePromise as MaybePromise_,
-  SingleOrArray as SingleOrArray_,
-  NonUndefined as NonUndefined_,
-  RequiredKeys as RequiredKeys_,
-  OptionalKeys as OptionalKeys_,
-  Mutable as Mutable_,
-  Immutable as Immutable_,
-  DeepReadonly as DeepReadonly_,
-  DeepRequired as DeepRequired_,
-  DeepPartial as DeepPartial_,
-  Class as Class_,
-  OmitFirstParam as OmitFirstParam_,
-  Prop as Prop_,
-  Values as Values_,
-  ArrayItems as ArrayItems_,
-  Merge as Merge_,
-  StrictlyRequired as StrictlyRequired_,
-  HasAddEventListener as HasAddEventListener_,
-  DictionaryOpt as DictionaryOpt_,
-  DictionaryReq as DictionaryReq_,
-  JsonPrimitive as JsonPrimitive_,
-  JsonValue as JsonValue_,
-  JsonArray as JsonArray_,
-  JsonObject as JsonObject_,
-  JsonifiableObject as JsonifiableObject_,
-  Json as Json_,
-  NotJsonifiable as NotJsonifiable_,
-  FilterJsonifiableKeys as FilterJsonifiableKeys_,
-  JsonifyObject as JsonifyObject_,
-  Simplify as Simplify_
-} from './types';
+export {}; // Make this a module
 
 /**
  * Declare global types for convenient access throughout the project
@@ -46,50 +8,49 @@ import type {
 declare global {
   /**
    * Represents a primitive type in TypeScript.
-   * @typedef {string | number | bigint | boolean | symbol | null | undefined} Primitive
    */
-  type Primitive = Primitive_;
+  type Primitive = string | number | bigint | boolean | symbol | null | undefined;
 
   /**
    * Represents a type that includes all falsy values: false, '', 0, null, and undefined.
    */
-  type Falsy = Falsy_;
+  type Falsy = false | '' | 0 | null | undefined;
 
   /**
    * Represents a type that can be null or undefined.
    */
-  type Nullish = Nullish_;
+  type Nullish = null | undefined;
 
   /**
    * Represents a type that can be either a value of type T or null.
    * @template T - The type of the value.
    */
-  type Nullable<T> = Nullable_<T>;
+  type Nullable<T> = T | null;
 
   /**
    * Represents a type that can either be of type T or undefined.
    * @template T - The type parameter.
    */
-  type Maybe<T> = Maybe_<T>;
+  type Maybe<T> = T | undefined;
 
   /**
    * Represents a type that can either be a value of type T or a promise that resolves to a value of type T.
    * @template T - The type of the value or the resolved value.
    */
-  type MaybePromise<T> = MaybePromise_<T>;
+  type MaybePromise<T> = T | Promise<T>;
 
   /**
    * Represents a type that can be either a single value or an array of values.
    * @template T - The type of the value(s).
    */
-  type SingleOrArray<T> = SingleOrArray_<T>;
+  type SingleOrArray<T> = T | T[];
 
   /**
    * Type helper that removes the undefined type from a given type.
    * @template T The type to remove undefined from.
    * @returns The type without undefined.
    */
-  type NonUndefined<T> = NonUndefined_<T>;
+  type NonUndefined<T> = T extends undefined ? never : T;
 
   /**
    * Returns the keys of an object type `T` that are required (not optional).
@@ -97,7 +58,9 @@ declare global {
    * @template T - The object type.
    * @returns The keys of `T` that are required.
    */
-  type RequiredKeys<T> = RequiredKeys_<T>;
+  type RequiredKeys<T> = {
+    [K in keyof T]-?: {} extends Pick<T, K> ? never : K;
+  }[keyof T];
 
   /**
    * Returns the keys of an object type `T` that are optional.
@@ -105,24 +68,40 @@ declare global {
    * @template T - The object type.
    * @returns The keys of `T` that are optional.
    */
-  type OptionalKeys<T> = OptionalKeys_<T>;
+  type OptionalKeys<T> = {
+    [K in keyof T]-?: {} extends Pick<T, K> ? K : never;
+  }[keyof T];
 
   /**
    * Represents a type that makes all properties of an object mutable (remove readonly).
    */
-  type Mutable<T> = Mutable_<T>;
+  type Mutable<T> = {
+    -readonly [P in keyof T]: T[P];
+  };
 
   /**
    * Represents a type that makes all properties of an object immutable (readonly).
    */
-  type Immutable<T> = Immutable_<T>;
+  type Immutable<T> = {
+    readonly [P in keyof T]: T[P];
+  };
 
   /**
    * Represents a type that makes all properties of an object and its nested objects readonly.
    * @template T - The type to make readonly.
    * @returns The readonly version of the input type.
    */
-  type DeepReadonly<T> = DeepReadonly_<T>;
+  type DeepReadonly<T> = T extends ((...args: any[]) => any) | Primitive
+    ? T
+    : T extends DeepReadonlyArray_<infer U>
+      ? DeepReadonlyArray_<U>
+      : T extends DeepReadonlyObject_<infer V>
+        ? DeepReadonlyObject_<V>
+        : T;
+  type DeepReadonlyArray_<T> = readonly DeepReadonly<T>[];
+  type DeepReadonlyObject_<T> = {
+    readonly [P in keyof T]: DeepReadonly<T[P]>;
+  };
 
   /**
    * Recursively makes all properties of an object and its nested objects/array required.
@@ -131,26 +110,44 @@ declare global {
    * @param {T} value - The value to make deep required.
    * @returns {DeepRequired<T>} - The deep required type.
    */
-  type DeepRequired<T> = DeepRequired_<T>;
+  type DeepRequired<T> = T extends (...args: any[]) => any
+    ? T
+    : T extends any[]
+      ? DeepRequiredArray_<T[number]>
+      : T extends object
+        ? DeepRequiredObject_<T>
+        : T;
+  type DeepRequiredArray_<T> = DeepRequired<NonUndefined<T>>[];
+  type DeepRequiredObject_<T> = {
+    [P in keyof T]-?: DeepRequired<NonUndefined<T[P]>>;
+  };
 
   /**
    * Represents a type that makes all properties of the given type optional recursively.
    * @template T - The type to make partial.
    */
-  type DeepPartial<T> = DeepPartial_<T>;
+  type DeepPartial<T> = {[P in keyof T]?: DeepPartial_<T[P]>};
+  type DeepPartial_<T> = T extends ((...args: any[]) => any) | Primitive
+    ? T
+    : T extends (infer U)[]
+      ? DeepPartialArray_<U>
+      : T extends object
+        ? DeepPartial<T>
+        : T | undefined;
+  type DeepPartialArray_<T> = DeepPartial_<T>[];
 
   /**
    * Represents a class constructor.
    * @template T - The type of the class.
    */
-  type Class<T> = Class_<T>;
+  type Class<T> = new (...args: any[]) => T;
 
   /**
    * Removes the first parameter from a function type.
    * @template F The function type.
    * @returns A new function type without the first parameter.
    */
-  type OmitFirstParam<F> = OmitFirstParam_<F>;
+  type OmitFirstParam<F> = F extends (x: any, ...args: infer A) => infer R ? (...args: A) => R : never;
 
   /**
    * Retrieves the type of a property from an object type.
@@ -159,14 +156,14 @@ declare global {
    * @template K - The property key.
    * @returns {Prop<T, K>} - The type of the property.
    */
-  type Prop<T, K> = Prop_<T, K>;
+  type Prop<T, K> = K extends keyof T ? T[K] : never;
 
   /**
    * Retrieves the union of all values in the given object type.
    * @typeparam T - The object type.
    * @returns The union of all values in the object type.
    */
-  type Values<T> = Values_<T>;
+  type Values<T> = T[keyof T];
 
   /**
    * Extracts the type of individual items in an array.
@@ -176,7 +173,7 @@ declare global {
    * @typeParam T - The input type.
    * @returns The type of individual items in the array, or the input type itself.
    */
-  type ArrayItems<T> = ArrayItems_<T>;
+  type ArrayItems<T> = T extends (infer K)[] ? K : T;
 
   /**
    * Merges two types together by omitting keys from the first type that exist in the second type,
@@ -185,82 +182,90 @@ declare global {
    * @template N - The second type to merge.
    * @returns A new type that is the result of merging the two input types.
    */
-  type Merge<M, N> = Merge_<M, N>;
+  type Merge<M, N> = Omit<M, keyof N> & N;
 
   /**
    * Make all properties in T required and exclude undefined and null from the property type.
    */
-  type StrictlyRequired<T> = StrictlyRequired_<T>;
+  type StrictlyRequired<T> = {
+    [P in keyof T]-?: NonNullable<T[P]>;
+  };
 
   /**
    * Represents an object that has the ability to add event listeners.
    */
-  interface HasAddEventListener extends HasAddEventListener_ {}
+  export interface HasAddEventListener {
+    addEventListener: (type: string, listener: EventListenerOrEventListenerObject, options?: AddEventListenerOptions) => void;
+  }
 
   /**
    * Represents a dictionary where values can be optional (undefined).
    *
    * @template T The type of values stored in the dictionary. Defaults to `any`.
    */
-  type DictionaryOpt<T = any> = DictionaryOpt_<T>;
+  type DictionaryOpt<T = any> = {[key in string]?: T};
 
   /**
    * Represents a dictionary where all values are required (non-optional).
    *
    * @template T The type of values stored in the dictionary. Defaults to `any`.
    */
-  type DictionaryReq<T = any> = DictionaryReq_<T>;
+  type DictionaryReq<T = any> = {[key in string]: T};
 
   /**
    * Matches any valid JSON primitive value.
    */
-  type JsonPrimitive = JsonPrimitive_;
+  type JsonPrimitive = string | number | boolean | null;
 
   /**
    * Strigifyable JSON value that can be of type `string`, `number`, `boolean`, `null`, `undefined`,
    * `JSONArray`, or `JSONObject`.
    */
-  type JsonValue = JsonValue_;
+  type JsonValue = JsonPrimitive | JsonArray | JsonObject | JsonifiableObject | undefined;
 
   /**
    * Represents `Array<JSONValues>`.
    */
-  type JsonArray = JsonArray_;
+  type JsonArray = JsonValue[] | readonly JsonValue[];
 
   /**
    * Represents an `Dictionary` of `JSONValue` (Record<string, JSONValues>)
    */
-  type JsonObject = JsonObject_;
+  type JsonObject = {[Key in string]?: JsonValue};
 
   /**
    * Represents an object that can be converted to JSON value (JsonObject or an object with toJSON method).
    */
-  type JsonifiableObject = JsonifiableObject_;
+  type JsonifiableObject = JsonObject | {toJSON: () => JsonValue};
 
   /**
    * Represents a Json response content that can be of type `JSONArray` or `JSONObject`.
    */
-  type Json = Json_;
+  type Json = JsonArray | JsonObject | JsonifiableObject;
 
   /**
    * Represents a type that cannot be converted to JSON.
    * This includes functions, undefined, and symbols.
    */
-  type NotJsonifiable = NotJsonifiable_;
+  type NotJsonifiable = ((...arguments_: any[]) => any) | undefined | symbol;
 
   /**
    * Filters out the keys from an object type that have values that are not JSONifiable.
    * @template T - The object type to filter.
    * @returns The keys from the object type that have values that are JSONifiable.
    */
-  type FilterJsonifiableKeys<T extends object> = FilterJsonifiableKeys_<T>;
+  type FilterJsonifiableKeys<T extends object> = {
+    [Key in keyof T]: T[Key] extends NotJsonifiable ? never : Key;
+  }[keyof T];
 
   /**
    * Converts an object type to a JSONifiable object type.
    * @template T - The object type to be converted.
    * @returns The JSONifiable object.
    */
-  type JsonifyObject<T extends object> = JsonifyObject_<T>;
+  type JsonifyObject<T extends object> = {
+    [Key in keyof Pick<T, FilterJsonifiableKeys<T>>]: T[Key];
+  };
 
   /**
    * Convert simple type (بکش از ما بیرون).
@@ -270,5 +275,5 @@ declare global {
    * @template T - The type to be simplified.
    * @returns The simplified type.
    */
-  type Simplify<T> = Simplify_<T>;
+  type Simplify<T> = {[KeyType in keyof T]: T[KeyType]} & {};
 }
