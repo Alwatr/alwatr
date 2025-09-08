33de03e3434c2974756db673d7689ecb342086a9
Ali Mihandoost <ali.mihandoost@gmail.com>
2025-09-08 20:18:52 +0330

    chore: remove fetch-state-machine export from main.ts
    BREAKING CHANGE: Deprecate `@alwatr/fetch-state-machine`

--- packages/flux/src/main.ts
+++ packages/flux/src/main.ts
@@ -1,5 +1,4 @@
 export * from '@alwatr/context';
-export * from '@alwatr/fetch-state-machine';
 export * from '@alwatr/fsm';
 export * from '@alwatr/observable';
 export * from '@alwatr/remote-context';
