7ccf6f1a0dc0a60fefa99ceb8929ea773a20e0f6
Ali Mihandoost <ali.mihandoost@gmail.com>
2025-09-20 11:47:29 +0330

    fix: update console log message in main.ts

--- packages/playground/src/main.ts
+++ packages/playground/src/main.ts
@@ -1,7 +1,8 @@
+// Remember to remove sideEffect from nanolib package.json before test
 import {createLogger} from '@alwatr/nanolib';
 import '@alwatr/nanolib/node';
 
 /* #__PURE__ */
 createLogger('playground');
 
-console.log('salam');
+console.log('hi');
