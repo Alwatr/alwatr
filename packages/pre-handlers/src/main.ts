411708a37a1202098b747d902cd255c03cfad061
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-09-22 00:52:54 +0330

    refactor: remove unused packageTracer import and development mode tracer from main.ts

--- packages/pre-handlers/src/main.ts
+++ packages/pre-handlers/src/main.ts
@@ -1,7 +1,3 @@
-import {packageTracer} from '@alwatr/nanolib';
-
 export * from './lib/get-auth-bearer.js';
 export * from './handler/parse-body-as-json.js';
 export * from './handler/require-access-token.js';
-
-__dev_mode__: packageTracer.add(__package_name__, __package_version__);
