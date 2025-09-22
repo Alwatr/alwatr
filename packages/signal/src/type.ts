ebc542e496cfcdfe06d74abeb08a4e1c4f4ec8b6
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-09-22 17:17:54 +0330

    feat: add saveDebounceDelay option to PersistentStateSignalConfig for optimized localStorage writes

--- packages/signal/src/type.ts
+++ packages/signal/src/type.ts
@@ -317,4 +317,12 @@ export interface DebounceSignalConfig extends Omit<DebouncerConfig<never>, 'func
  *
  * @template T The type of the state it holds.
  */
-export interface PersistentStateSignalConfig<T> extends SignalConfig, LocalStorageProviderConfig<T> {}
+export interface PersistentStateSignalConfig<T> extends SignalConfig, LocalStorageProviderConfig<T> {
+  /**
+   * The debounce delay (in milliseconds) for saving changes to localStorage.
+   * This helps to reduce the frequency of write operations, which can be costly in terms of performance.
+   * 
+   * @default 500
+   */
+  saveDebounceDelay?: number;
+}
