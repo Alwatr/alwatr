b993ad8d390f8f1b6654713b828a7f3e0221b671
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2025-11-04 22:03:58 +0330

    test: uncomment fetch tests for search parameters

--- packages/fetch/src/main.test.js
+++ packages/fetch/src/main.test.js
@@ -1,18 +1,18 @@
-// import {fetch} from '@alwatr/fetch';
+import {fetch} from '@alwatr/fetch';
 
-// describe('fetch with search params', () => {
-//   it('should make a GET request to the specified URL', async () => {
-//     const response = await fetch('http://httpbin.org/get', {
-//       queryParams: {
-//         a: 'b&c=2',
-//       },
-//     });
-//     const responseJson = await response.json();
+describe('fetch with search params', () => {
+  it('should make a GET request to the specified URL', async () => {
+    const response = await fetch('http://httpbin.org/get', {
+      queryParams: {
+        a: 'b&c=2',
+      },
+    });
+    const responseJson = await response.json();
 
-//     expect(response.status).toBe(200);
-//     expect(responseJson.args.a).toBe('b&c=2');
-//   });
-// });
+    expect(response.status).toBe(200);
+    expect(responseJson.args.a).toBe('b&c=2');
+  });
+});
 
 // describe('fetch with bodyJson', () => {
 //   it('should send JSON body and receive it back', async () => {
