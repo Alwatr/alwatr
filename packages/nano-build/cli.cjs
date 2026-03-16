058c09ee0cdcf8455df70a96dd6e84e7cee369e4
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2026-03-17 00:27:15 +0330

    fix: remove debug statement from banner in default build options

--- packages/nano-build/cli.cjs
+++ packages/nano-build/cli.cjs
@@ -56,7 +56,7 @@ const defaultOptions = {
   charset: 'utf8',
   legalComments: 'linked',
   banner: {
-    js: `/** 📦 ${packageJson.name} v${packageJson.version} */\n__dev_mode__: console.debug("📦 ${packageJson.name} v${packageJson.version}");`,
+    js: `/** 📦 ${packageJson.name} v${packageJson.version} */`,
   },
   define: {
     __package_name__: `'${packageJson.name}'`,
