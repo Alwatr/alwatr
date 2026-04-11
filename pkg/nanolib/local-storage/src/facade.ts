7ff34614d59709e24358e500d4c5a3a9320f1698
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2026-04-11 21:42:41 +0330

    refactor: remove generic constraint from createLocalStorageProvider function

--- pkg/nanolib/local-storage/src/facade.ts
+++ pkg/nanolib/local-storage/src/facade.ts
@@ -23,6 +23,6 @@ import type {LocalStorageProviderConfig} from './type.js';
  * console.log(currentSettings); // { theme: 'dark', notifications: false }
  * ```
  */
-export function createLocalStorageProvider<T extends JsonValue>(config: LocalStorageProviderConfig): LocalStorageProvider<T> {
+export function createLocalStorageProvider<T>(config: LocalStorageProviderConfig): LocalStorageProvider<T> {
   return new LocalStorageProvider<T>(config);
 }
