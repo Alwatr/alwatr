9f26fe11dee60fda4b0e824d4e02762bb5dc76cf
S. Ali Mihandoost <ali.mihandoost@gmail.com>
2026-06-07 17:39:06 +0330

    feat(delay): add requestNextRender function to handle post-render calculations

---
+++ pkg/nanolib/delay/src/request_next_render.ts
@@ -0,0 +1,21 @@
+import {requestAnimationFrame} from './request-animation-frame.js';
+
+/**
+ * Suspends execution flow until the next two consecutive animation frames have occurred.
+ *
+ * Ensures that any pending DOM updates have been rendered before resuming execution.
+ * Resolves after the second animation frame, providing a safe point to perform post-render calculations.
+ *
+ * @returns A Promise that resolves after two animation frames.
+ *
+ * @example
+ * ```ts
+ * delay.nextRender(() => {
+ *   performPostRenderCalculations();
+ * });
+ */
+export const requestNextRender = (callback: FrameRequestCallback): void => {
+  requestAnimationFrame(() => {
+    requestAnimationFrame(callback);
+  });
+};
